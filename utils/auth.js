4e6492f1ee3eb5ea85a71e1dc20b960bab34e629
Jayson Hunter <github@jaysonhunter.com>
2020-10-26 20:28:51 +0000

    Update auth to cater for Adults and Childrens Google User groups

--- utils/auth.js
+++ utils/auth.js
@@ -7,7 +7,7 @@ const { GSSO_TOKEN_NAME } = process.env;
 export const redirectToHome = ({ res } = {}) => {
   if (res) {
     res.writeHead(302, {
-      Location: '/',
+      Location: '/'
     });
     res.end();
   } else {
@@ -18,7 +18,7 @@ export const redirectToHome = ({ res } = {}) => {
 export const redirectToLogin = ({ res } = {}) => {
   if (res) {
     res.writeHead(302, {
-      Location: '/login',
+      Location: '/login'
     });
     res.end();
   } else {
@@ -29,7 +29,7 @@ export const redirectToLogin = ({ res } = {}) => {
 export const redirectToAcessDenied = ({ res } = {}) => {
   if (res) {
     res.writeHead(302, {
-      Location: '/access-denied',
+      Location: '/access-denied'
     });
     res.end();
   } else {
@@ -37,41 +37,63 @@ export const redirectToAcessDenied = ({ res } = {}) => {
   }
 };
 
-export const deleteSession = (ctx) => {
+export const deleteSession = ctx => {
   removeCookies(ctx, GSSO_TOKEN_NAME, {
     path: '/',
-    domain: '.hackney.gov.uk',
+    domain: '.hackney.gov.uk'
   });
 
   redirectToLogin(ctx);
 };
 
-export const isAuthorised = (ctx) => {
-  const { HACKNEY_JWT_SECRET, AUTHORISED_ADMIN_GROUP } = process.env;
+export const isAuthorised = ctx => {
+  const {
+    HACKNEY_JWT_SECRET,
+    AUTHORISED_ADMIN_GROUP,
+    AUTHORISED_ADULT_GROUP,
+    AUTHORISED_CHILD_GROUP
+  } = process.env;
 
   let gssoUserObj = {
     isAuthorised: false,
     name: '',
     email: '',
-    isAdmin: false,
+    hasAdminPermissions: false,
+    hasAdultPermissions: false,
+    hasChildrenPermissions: false
   };
+
+  let payload = {};
 
   const token = getCookies(ctx, GSSO_TOKEN_NAME);
 
   try {
     if (token) {
       try {
-        let payload = jsonwebtoken.verify(token, HACKNEY_JWT_SECRET);
+        payload = jsonwebtoken.verify(token, HACKNEY_JWT_SECRET);
         const groups = payload.groups;
 
-        // User is authorised if in the group
+        // User is authorised if in the Admin group
         if (groups && groups.includes(AUTHORISED_ADMIN_GROUP)) {
-          gssoUserObj = {
-            isAuthorised: true,
-            name: payload.name,
-            email: payload.email,
-            isAdmin: true,
-          };
+          gssoUserObj.isAuthorised = true;
+          gssoUserObj.hasAdminPermissions = true;
+        }
+
+        // User is authorised if in the Adult group
+        if (groups && groups.includes(AUTHORISED_ADULT_GROUP)) {
+          gssoUserObj.isAuthorised = true;
+          gssoUserObj.hasAdultPermissions = true;
+        }
+
+        // User is authorised if in the Children group
+        if (groups && groups.includes(AUTHORISED_CHILD_GROUP)) {
+          gssoUserObj.isAuthorised = true;
+          gssoUserObj.hasChildrenPermissions = true;
+        }
+
+        if (gssoUserObj.isAuthorised) {
+          gssoUserObj.name = payload.name;
+          gssoUserObj.email = payload.email;
         } else {
           redirectToAcessDenied(ctx);
         }
