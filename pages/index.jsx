9e73f95aa102e8ae096f14c86615b1a662de16af
Naomi Prescod-Green <naomipgreen@gmail.com>
2020-10-27 18:59:13 +0000

    linkbutton, dashboard, tidy (#16)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,53 +1,23 @@
 import PropTypes from 'prop-types';
-import Router from 'next/router';
 import { isAuthorised, redirectToLogin } from 'utils/auth';
 import { NextSeo } from 'next-seo';
 import AdminNavBar from 'components/AdminNavBar/AdminNavBar';
+import LinkButton from 'components/LinkButton/LinkButton';
 
 const Home = ({ userDetails }) => {
   return (
     <div>
       <NextSeo title="Home" />
       <AdminNavBar adminName={userDetails.name} />
-      <button
-        href="#"
-        role="button"
-        draggable="false"
-        className="govuk-button govuk-button--start govuk-!-margin-top-3"
-        data-module="govuk-button"
-        onClick={() => Router.push('/people/search')}
-      >
-        Search for People
-        <svg
-          className="govuk-button__start-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="17.5"
-          height="19"
-          viewBox="0 0 33 40"
-          aria-hidden="true"
-          focusable="false"
-        >
-          <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
-        </svg>
-      </button>
       <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
-        <h1 className="govuk-fieldset__heading">Adult Referral From</h1>
+        <h1 className="govuk-fieldset__heading">Form Dashboard</h1>
       </legend>
-      <button
-        className="govuk-button"
-        onClick={() => Router.push('/steps/client-details')}
-      >
-        Start
-      </button>
-      <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
-        <h1 className="govuk-fieldset__heading">ASC Case Notes Recording</h1>
-      </legend>
-      <button
-        className="govuk-button"
-        onClick={() => Router.push('/steps/case-notes-recording')}
-      >
-        Start
-      </button>
+      <LinkButton label="Search for People" route="/people/search" />
+      <LinkButton label="Adult Referral From" route="/steps/client-details" />
+      <LinkButton
+        label="Case Notes Recording"
+        route="/steps/case-notes-recording"
+      />
     </div>
   );
 };
