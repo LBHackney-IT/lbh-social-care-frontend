6351c83fb1d9584984ffe5985e663e613a1d2f26
Naomi Prescod-Green <naomipgreen@gmail.com>
2020-10-27 16:17:48 +0000

    linkbutton, dashboard, tidy

---
+++ components/LinkButton/LinkButton.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import Router from 'next/router';
+
+const LinkButton = ({ label, route }) => (
+  <div>
+    <button
+      href="#"
+      role="button"
+      draggable="false"
+      className="govuk-button govuk-button--start govuk-!-margin-top-3"
+      data-module="govuk-button"
+      onClick={() => Router.push(`${route}`)}
+    >
+      {label}
+      <svg
+        className="govuk-button__start-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="17.5"
+        height="19"
+        viewBox="0 0 33 40"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
+      </svg>
+    </button>
+  </div>
+);
+
+LinkButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
+};
+
+export default LinkButton;
