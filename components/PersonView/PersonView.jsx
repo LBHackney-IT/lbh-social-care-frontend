a07fead71314b963ab20c881b7185091a6dd25a2
LBHABEBIC <ana.bebic@hackney.gov.uk>
2020-10-29 11:26:09 +0000

    fixed error message and applied govuk styleguides

--- components/PersonView/PersonView.jsx
+++ components/PersonView/PersonView.jsx
@@ -1,109 +1,46 @@
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { getResident } from 'utils/api/residents';
+import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
+import PersonDetails from './PersonDetails';
 
 const PersonView = ({ personId }) => {
   const [person, setPerson] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
 
   const getPerson = async (personId) => {
+    setLoading(false);
     try {
       const data = await getResident(personId);
       setPerson(data);
-    } catch {
-      console.log('error');
+      setError(null);
+    } catch (e) {
+      setPerson(null);
+      setError(e.response.data);
     }
   };
   useEffect(() => {
+    setLoading(true);
     getPerson(personId);
   }, [personId]);
 
   return (
     <>
-      <h1>View person</h1>
-      <h2 className="personDetails">PERSON DETAILS</h2>
-      <hr className="personDetailsHr" />
-      <dl className="govuk-summary-list--no-border   lbh-summary-list">
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <dt
-              className="govuk-summary-list__key"
-              style={{ display: 'inline-flex' }}
-            >
-              {person?.firstName} {person?.lastName}
-            </dt>
-            <dd className="govuk-summary-list__value"></dd>
-          </div>
-          <div className="govuk-grid-column-one-half">
-            <dt className="govuk-summary-list__key">Mosaic ID:</dt>
-            <dd className="govuk-summary-list__value">#{person?.mosaicId}</dd>
-          </div>
-        </div>
-        <hr />
-
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <dt className="govuk-summary-list__key">Date of birth</dt>
-            <dd className="govuk-summary-list__value">
-              {new Date(person?.dateOfBirth).toLocaleDateString('en-GB')}
-            </dd>
-          </div>
-          <div className="govuk-grid-column-one-half">
-            <dt className="govuk-summary-list__key">NHS number</dt>
-            <dd className="govuk-summary-list__value">{person?.nhsNumber}</dd>
-          </div>
-        </div>
-
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <dt className="govuk-summary-list__key">Nationality</dt>
-            <dd className="govuk-summary-list__value">{person?.nationality}</dd>
-          </div>
-        </div>
-
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <dt className="govuk-summary-list__key">Gender</dt>
-            <dd className="govuk-summary-list__value">{person?.gender}</dd>
-          </div>
-        </div>
-
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <dt className="govuk-summary-list__key">
-              <h4>Address</h4>
-            </dt>
-            <dd class="govuk-summary-list__value">
-              {person?.addressList &&
-                Object.values(person?.addressList[0]).map((addressLine) => (
-                  <p>{addressLine}</p>
-                ))}
-            </dd>
-          </div>
-
-          <div className="govuk-grid-column-one-half">
-            <dt className="govuk-summary-list__key">PhoneNumber</dt>
-            <dd className="govuk-summary-list__value">
-              {person?.phoneNumber &&
-                Object.values(person?.phoneNumber[0]).map((number) => (
-                  <p key={number}>{number}</p>
-                ))}
-            </dd>
-          </div>
-        </div>
-      </dl>
-      <style jsx>{`
-        :global(.personDetails) {
-          color: #00513f;
-        }
-        :global(.personDetailsHr) {
-          background-color: #00513f;
-          height: 2px;
-        }
-        :global() {
-          border: solid 1px black;
-        }
-      `}</style>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {error && <ErrorMessage label={error} />}
+          {person && <PersonDetails {...person} />}
+        </>
+      )}
     </>
   );
+};
+
+PersonView.propTypes = {
+  personId: PropTypes.string.isRequired,
 };
 
 export default PersonView;
