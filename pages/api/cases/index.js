fa76b7fb60655083d4d1776d9fc4493304685174
Naomi Prescod-Green <naomipgreen@gmail.com>
2020-11-02 18:36:14 +0000

    update

---
+++ pages/api/cases/index.js
@@ -0,0 +1,24 @@
+import { postResidentCase } from 'utils/server/residents';
+import { isAuthorised } from 'utils/auth';
+
+export default async (req, res) => {
+  if (!isAuthorised({ req })) {
+    return res.status(401).send('Auth cookie missing.');
+  }
+  switch (req.method) {
+    case 'POST':
+      try {
+        await postResidentCase(req.query.id, req.body);
+        res.setHeader('Content-Type', 'application/json');
+      } catch (error) {
+        console.log(error.status);
+        console.log('Cast post error:', error);
+        res.status(500).json('Unable to post cast');
+      }
+      break;
+
+    default:
+      res.status(400).json('Invalid request method');
+      console.log(res.status);
+  }
+};
