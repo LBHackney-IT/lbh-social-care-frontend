593f1bec162e59d905ddcc2d0a30d3360dec9298
Luca Lischetti <luca@sirlisko.com>
2020-11-10 13:28:07 +0100

    Bugfix/search postcode validation (#40)

--- components/Search/SearchForm.jsx
+++ components/Search/SearchForm.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 import Router from 'next/router';
 import { useForm } from 'react-hook-form';
 import isPast from 'date-fns/isPast';
 import isPostcodeValid from 'uk-postcode-validator';
 
 import { Button, TextInput, DateInput } from 'components/Form';
-import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
 import { getQueryString } from 'utils/urls';
 
 const isDetailsFormEmpty = ({
@@ -21,17 +19,19 @@ const isDetailsFormEmpty = ({
 const isMosaicFormEmpty = ({ mosaicId }) => mosaicId === '';
 
 const SearchForm = ({ onFormSubmit, query }) => {
-  const [formError, setFormError] = useState();
   const [mosaicSearchDisabled, setMosaicSearchDisabled] = useState(false);
   const [detailsSearchDisabled, setDetailsSearchDisabled] = useState(false);
-  const { register, errors, control, watch, handleSubmit } = useForm({
+  const {
+    register,
+    errors,
+    control,
+    watch,
+    handleSubmit,
+    formState: { isDirty },
+  } = useForm({
     defaultValues: query,
   });
   const onSubmit = async (formData) => {
-    setFormError(null);
-    if (isDetailsFormEmpty(formData) && isMosaicFormEmpty(formData)) {
-      return setFormError('You need to enter name or date of birth');
-    }
     onFormSubmit(formData);
     const qs = getQueryString(formData);
     Router.replace(`/people/search?${qs}`, `/people/search?${qs}`, {
@@ -58,88 +58,87 @@ const SearchForm = ({ onFormSubmit, query }) => {
   }, []);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className={cx({ 'govuk-form-group--error': Boolean(formError) })}>
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <TextInput
-              label="First name:"
-              labelSize="s"
-              name="first_name"
-              error={errors.first_name}
-              register={register}
-              disabled={detailsSearchDisabled}
-            />
-          </div>
-          <div className="govuk-grid-column-one-half">
-            <TextInput
-              label="Last name:"
-              labelSize="s"
-              name="last_name"
-              error={errors.last_name}
-              register={register}
-              disabled={detailsSearchDisabled}
-            />
-          </div>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-half">
+          <TextInput
+            label="First name:"
+            labelSize="s"
+            name="first_name"
+            error={errors.first_name}
+            register={register}
+            disabled={detailsSearchDisabled}
+          />
         </div>
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <DateInput
-              label="Date of Birth:"
-              labelSize="s"
-              name="date_of_birth"
-              error={errors.date_of_birth}
-              control={control}
-              rules={{
-                validate: {
-                  past: (value) =>
-                    value && (isPast(new Date(value)) || 'Must be a past Date'),
-                },
-              }}
-              disabled={detailsSearchDisabled}
-            />
-          </div>
-          <div className="govuk-grid-column-one-half">
-            <TextInput
-              label="Postcode:"
-              labelSize="s"
-              name="postcode"
-              hint="i.e. E8 3AS"
-              inputClassName="govuk-input--width-10"
-              error={errors.postcode}
-              register={register({
-                validate: {
-                  valid: (value) =>
-                    (value && isPostcodeValid(value)) ||
-                    'You entered an invalid postcode',
-                },
-              })}
-              disabled={detailsSearchDisabled}
-            />
-          </div>
+        <div className="govuk-grid-column-one-half">
+          <TextInput
+            label="Last name:"
+            labelSize="s"
+            name="last_name"
+            error={errors.last_name}
+            register={register}
+            disabled={detailsSearchDisabled}
+          />
         </div>
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <TextInput
-              label="Mosaic ID:"
-              labelSize="s"
-              name="mosaicId"
-              hint="e.g. 1234567890"
-              inputClassName="govuk-input--width-10"
-              inputMode="numeric"
-              error={errors.mosaicId}
-              register={register({
-                pattern: {
-                  value: /^[0-9]+$/,
-                  message: 'Mosaic ID must be a number',
-                },
-              })}
-              disabled={mosaicSearchDisabled}
-            />
-          </div>
-        </div>
-        {formError && <ErrorMessage label={formError} />}
       </div>
-      <Button label="Search" type="submit" />
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-half">
+          <DateInput
+            label="Date of Birth:"
+            labelSize="s"
+            name="date_of_birth"
+            error={errors.date_of_birth}
+            control={control}
+            rules={{
+              validate: {
+                past: (value) =>
+                  value && (isPast(new Date(value)) || 'Must be a past Date'),
+              },
+            }}
+            disabled={detailsSearchDisabled}
+          />
+        </div>
+        <div className="govuk-grid-column-one-half">
+          <TextInput
+            label="Postcode:"
+            labelSize="s"
+            name="postcode"
+            hint="i.e. E8 3AS"
+            inputClassName="govuk-input--width-10"
+            error={errors.postcode}
+            register={register({
+              required: false,
+              validate: {
+                valid: (value) =>
+                  value === '' ||
+                  (value && isPostcodeValid(value)) ||
+                  'You entered an invalid postcode',
+              },
+            })}
+            disabled={detailsSearchDisabled}
+          />
+        </div>
+      </div>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-half">
+          <TextInput
+            label="Mosaic ID:"
+            labelSize="s"
+            name="mosaicId"
+            hint="e.g. 1234567890"
+            inputClassName="govuk-input--width-10"
+            inputMode="numeric"
+            error={errors.mosaicId}
+            register={register({
+              pattern: {
+                value: /^[0-9]+$/,
+                message: 'Mosaic ID must be a number',
+              },
+            })}
+            disabled={mosaicSearchDisabled}
+          />
+        </div>
+      </div>
+      <Button label="Search" type="submit" disabled={!isDirty} />
     </form>
   );
 };
