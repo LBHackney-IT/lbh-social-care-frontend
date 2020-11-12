8e12446a4040579afea3b6e6edd447e2518d2f61
Marta Villalobos <49244143+LBHMVillalobos@users.noreply.github.com>
2020-11-12 12:18:20 +0000

    cypress integration and some tests created (#43)
    * cypress integration and some test created
    
    * remove unused files
    
    * updated gitignore
    
    Co-authored-by: Luca Lischetti <luca@sirlisko.com>

---
+++ cypress/integration/search_by_name_adult_admin.js
@@ -0,0 +1,43 @@
+describe('Adult group', () => {
+  it('Access not allowed - Search for People by name', () => {
+    cy.getCookies().should('be.empty');
+    cy.setCookie('hackneyToken', Cypress.env('TEST_KEY_ADULT_GROUP'));
+    cy.getCookie('hackneyToken').should(
+      'have.property',
+      'value',
+      Cypress.env('TEST_KEY_ADULT_GROUP')
+    );
+    cy.visit('http://dev.hackney.gov.uk:3000/');
+    cy.contains('Search for People').click();
+    cy.get('[data-testid="first_name"]').type(
+      Cypress.env('CHILDREN_RECORD_FIRST_NAME')
+    );
+    cy.get('[data-testid="last_name"]').type(
+      Cypress.env('CHILDREN_RECORD_LAST_NAME')
+    );
+    cy.get('[type="submit"]').click();
+    cy.get('.govuk-error-message').should('be.visible');
+  });
+});
+
+describe('Adult group', () => {
+  it('Access allowed - Search for People by name', () => {
+    cy.getCookies().should('be.empty');
+    cy.setCookie('hackneyToken', Cypress.env('TEST_KEY_ADULT_GROUP'));
+    cy.getCookie('hackneyToken').should(
+      'have.property',
+      'value',
+      Cypress.env('TEST_KEY_ADULT_GROUP')
+    );
+    cy.visit('http://dev.hackney.gov.uk:3000/');
+    cy.contains('Search for People').click();
+    cy.get('[data-testid="first_name"]').type(
+      Cypress.env('ADULT_RECORD_FIRST_NAME')
+    );
+    cy.get('[data-testid="last_name"]').type(
+      Cypress.env('ADULT_RECORD_LAST_NAME')
+    );
+    cy.get('[type="submit"]').click();
+    cy.contains(Cypress.env('ADULT_RECORD_FULL_NAME')).should('be.visible');
+  });
+});
