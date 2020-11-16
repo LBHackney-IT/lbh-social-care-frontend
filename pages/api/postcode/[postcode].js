cbb0ff940070ea5a5a9946814cc8c2101ddfb65e
Luca Lischetti <luca@sirlisko.com>
2020-11-16 12:34:45 +0000

    improved postcode API

--- pages/api/postcode/[postcode].js
+++ pages/api/postcode/[postcode].js
@@ -1,23 +1,30 @@
 import axios from 'axios';
+import * as HttpStatus from 'http-status-codes';
+
+import { isAuthorised } from 'utils/auth';
 
 export default async (req, res) => {
-  const {
-    query: { postcode },
-  } = req;
-  try {
-    const { data } = await axios.get(
-      `${process.env.POSTCODE_LOOKUP_URL}${postcode}`,
-      {
-        headers: {
-          'x-api-key': process.env.POSTCODE_LOOKUP_APIKEY,
-        },
+  if (!isAuthorised({ req })) {
+    return res.status(HttpStatus.UNAUTHORIZED).send('Auth cookie missing.');
+  }
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { data } = await axios.get(
+          `${process.env.POSTCODE_LOOKUP_URL}${req.query.postcode}`,
+          {
+            headers: {
+              'x-api-key': process.env.POSTCODE_LOOKUP_APIKEY,
+            },
+          }
+        );
+        res.status(data.statusCode).json(data.data);
+      } catch (e) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).end(e.message);
       }
-    );
-    res.statusCode = data.statusCode;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(data.data));
-  } catch (e) {
-    res.statusCode = 500;
-    res.end(e.message);
+      break;
+
+    default:
+      res.status(HttpStatus.BAD_REQUEST).json('Invalid request method');
   }
 };
