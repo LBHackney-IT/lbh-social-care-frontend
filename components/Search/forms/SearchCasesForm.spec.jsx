f7e66da9ede21146df56d7754ce0eb1337c6a1ef
Luca Lischetti <luca@sirlisko.com>
2020-11-30 12:22:56 +0000

    refactored and fixed SearchCasesForm unit tests

--- components/Search/forms/SearchCasesForm.spec.jsx
+++ components/Search/forms/SearchCasesForm.spec.jsx
@@ -1,51 +1,51 @@
-// import { act, fireEvent, render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 
-// jest.mock('next/router', () => ({
-//   replace: jest.fn(),
-// }));
+jest.mock('next/router', () => ({
+  replace: jest.fn(),
+}));
 
-// import SearchCasesForm from './SearchCasesForm';
+import SearchCasesForm from './SearchCasesForm';
 
-// describe(`SearchCasesForm`, () => {
-//   const props = {
-//     onFormSubmit: jest.fn(),
-//     query: {},
-//     user: {
-//       name: 'i am a user',
-//     },
-//   };
+describe(`SearchCasesForm`, () => {
+  const props = {
+    onFormSubmit: jest.fn(),
+    query: {},
+    user: {
+      name: 'i am a user',
+    },
+  };
 
-//   it('should pass to onFormSubmit the form values', async () => {
-//     const { getByRole, getByLabelText } = render(
-//       <SearchCasesForm {...props} />
-//     );
+  it('should pass to onFormSubmit the form values', async () => {
+    const { getByRole, getByLabelText } = render(
+      <SearchCasesForm {...props} />
+    );
 
-//     const firstNameInput = getByLabelText('First name:');
-//     fireEvent.change(firstNameInput, { target: { value: 'foo' } });
+    const firstNameInput = getByLabelText('First name:');
+    fireEvent.change(firstNameInput, { target: { value: 'foo' } });
 
-//     await act(async () => {
-//       fireEvent.submit(getByRole('form'));
-//     });
-//     expect(props.onFormSubmit).toHaveBeenCalledWith({
-//       first_name: 'foo',
-//       last_name: '',
-//       case_note_type: '',
-//       exact_match: false,
-//     });
-//   });
+    await act(async () => {
+      fireEvent.submit(getByRole('form'));
+    });
+    expect(props.onFormSubmit).toHaveBeenCalledWith({
+      first_name: 'foo',
+      last_name: '',
+      case_note_type: '',
+      exact_match: false,
+    });
+  });
 
-//   it('should initialise the form with the passed query', async () => {
-//     const { getByRole } = render(
-//       <SearchCasesForm {...props} query={{ first_name: 'bar' }} />
-//     );
-//     await act(async () => {
-//       fireEvent.submit(getByRole('form'));
-//     });
-//     expect(props.onFormSubmit).toHaveBeenCalledWith({
-//       first_name: 'bar',
-//       last_name: '',
-//       case_note_type: '',
-//       exact_match: false,
-//     });
-//   });
-// });
+  it('should initialise the form with the passed query', async () => {
+    const { getByRole } = render(
+      <SearchCasesForm {...props} query={{ first_name: 'bar' }} />
+    );
+    await act(async () => {
+      fireEvent.submit(getByRole('form'));
+    });
+    expect(props.onFormSubmit).toHaveBeenCalledWith({
+      first_name: 'bar',
+      last_name: '',
+      case_note_type: '',
+      exact_match: false,
+    });
+  });
+});
