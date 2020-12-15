c840793a551c86152447ac1e17ff49c57e52b206
Luca Lischetti <luca@sirlisko.com>
2020-12-15 16:27:43 +0100

    show display address (#80)

--- components/PersonView/PersonDetails.jsx
+++ components/PersonView/PersonDetails.jsx
@@ -14,9 +14,9 @@ const PersonDetails = ({
   expandView = false,
 }) => {
   const [expandDetails, setExpandDetails] = useState(false);
-  const address =
-    addressList &&
-    addressList.find((address) => address.contactAddressFlag === 'Y');
+  const address = addressList?.find(
+    ({ displayAddressFlag }) => displayAddressFlag === 'Y'
+  );
   return (
     <>
       <div>
@@ -62,12 +62,14 @@ const PersonDetails = ({
                 <dt className="govuk-summary-list__key">Gender</dt>
                 <dd className="govuk-summary-list__value">{gender}</dd>
               </div>
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Address</dt>
-                <dd className="govuk-summary-list__value">
-                  {address && <p>{address.addressLine1}</p>}
-                </dd>
-              </div>
+              {address?.addressLine1 && (
+                <div className="govuk-summary-list__row">
+                  <dt className="govuk-summary-list__key">Address</dt>
+                  <dd className="govuk-summary-list__value">
+                    <p>{address.addressLine1}</p>
+                  </dd>
+                </div>
+              )}
               <div className="govuk-summary-list__row">
                 <dt className="govuk-summary-list__key">PhoneNumber</dt>
                 <dd className="govuk-summary-list__value">
