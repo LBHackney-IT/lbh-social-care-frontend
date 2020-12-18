0943b7b35eee792243af93d499f37f152baab426
Luca Lischetti <luca@sirlisko.com>
2020-12-18 14:06:31 +0000

    fixed dateInput ref

--- components/Form/DateInput/DateInput.jsx
+++ components/Form/DateInput/DateInput.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, forwardRef, useRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { Controller } from 'react-hook-form';
@@ -11,177 +11,163 @@ const getInitialDate = (value) => {
   return { day, month, year };
 };
 
-const DateInput = ({
-  label,
-  labelSize = 'm',
-  inputRef,
-  error,
-  hint,
-  value,
-  name,
-  onChange,
-  required,
-  ...otherProps
-}) => {
-  const [date, setDate] = useState(getInitialDate(value));
-  useEffect(() => {
-    const { day, month, year } = date;
-    day !== '' &&
-      month !== '' &&
-      year !== '' &&
-      onChange(`${year}-${month}-${day}`);
-    day === '' && month === '' && year === '' && onChange();
-  }, [date]);
-  return (
-    <div
-      className={cx('govuk-form-group', {
-        'govuk-form-group--error': error,
-      })}
-    >
-      <fieldset
-        className="govuk-fieldset"
-        role="group"
-        aria-describedby={`${name}-hint`}
+const DateInput = forwardRef(
+  (
+    {
+      label,
+      labelSize = 'm',
+      error,
+      hint,
+      value,
+      name,
+      onChange,
+      required,
+      ...otherProps
+    },
+    ref
+  ) => {
+    const [date, setDate] = useState(getInitialDate(value));
+    useEffect(() => {
+      const { day, month, year } = date;
+      day !== '' &&
+        month !== '' &&
+        year !== '' &&
+        onChange(`${year}-${month}-${day}`);
+      day === '' && month === '' && year === '' && onChange();
+    }, [date]);
+    return (
+      <div
+        className={cx('govuk-form-group', {
+          'govuk-form-group--error': error,
+        })}
       >
-        <legend
-          className={`govuk-fieldset__legend govuk-fieldset__legend--${labelSize}`}
+        <fieldset
+          className="govuk-fieldset"
+          role="group"
+          aria-describedby={`${name}-hint`}
         >
-          {label} {required && <span className="govuk-required">*</span>}
-        </legend>
-        <span id={`${name}-hint`} className="govuk-hint">
-          {hint}
-        </span>
-        {error && <ErrorMessage label={error.message} />}
-        <div className="govuk-date-input" id={name}>
-          <div className="govuk-date-input__item">
-            <div className="govuk-form-group">
-              <label
-                className="govuk-label govuk-date-input__label"
-                htmlFor={`${name}-day`}
-              >
-                Day
-              </label>
-              <input
-                className={cx(
-                  'govuk-input govuk-date-input__input govuk-input--width-2',
-                  {
-                    'govuk-input--error': error,
+          <legend
+            className={`govuk-fieldset__legend govuk-fieldset__legend--${labelSize}`}
+          >
+            {label} {required && <span className="govuk-required">*</span>}
+          </legend>
+          <span id={`${name}-hint`} className="govuk-hint">
+            {hint}
+          </span>
+          {error && <ErrorMessage label={error.message} />}
+          <div className="govuk-date-input" id={name}>
+            <div className="govuk-date-input__item">
+              <div className="govuk-form-group">
+                <label
+                  className="govuk-label govuk-date-input__label"
+                  htmlFor={`${name}-day`}
+                >
+                  Day
+                </label>
+                <input
+                  className={cx(
+                    'govuk-input govuk-date-input__input govuk-input--width-2',
+                    {
+                      'govuk-input--error': error,
+                    }
+                  )}
+                  id={`${name}-day`}
+                  name={`${name}-day`}
+                  type="text"
+                  pattern="[0-9]*"
+                  inputMode="numeric"
+                  value={date.day}
+                  onChange={({ target: { value } }) =>
+                    setDate({ ...date, day: value })
                   }
-                )}
-                id={`${name}-day`}
-                name={`${name}-day`}
-                type="text"
-                pattern="[0-9]*"
-                inputMode="numeric"
-                value={date.day}
-                onChange={({ target: { value } }) =>
-                  setDate({ ...date, day: value })
-                }
-                ref={inputRef}
-                {...otherProps}
-              />
+                  ref={ref}
+                  {...otherProps}
+                />
+              </div>
+            </div>
+            <div className="govuk-date-input__item">
+              <div className="govuk-form-group">
+                <label
+                  className="govuk-label govuk-date-input__label"
+                  htmlFor={`${name}-month`}
+                >
+                  Month
+                </label>
+                <input
+                  className={cx(
+                    'govuk-input govuk-date-input__input govuk-input--width-2',
+                    {
+                      'govuk-input--error': error,
+                    }
+                  )}
+                  id={`${name}-month`}
+                  name={`${name}-month`}
+                  type="text"
+                  pattern="[0-9]*"
+                  inputMode="numeric"
+                  value={date.month}
+                  onChange={({ target: { value } }) =>
+                    setDate({ ...date, month: value })
+                  }
+                  {...otherProps}
+                />
+              </div>
+            </div>
+            <div className="govuk-date-input__item">
+              <div className="govuk-form-group">
+                <label
+                  className="govuk-label govuk-date-input__label"
+                  htmlFor={`${name}-year`}
+                >
+                  Year
+                </label>
+                <input
+                  className={cx(
+                    'govuk-input govuk-date-input__input govuk-input--width-4',
+                    {
+                      'govuk-input--error': error,
+                    }
+                  )}
+                  id={`${name}-year`}
+                  name={`${name}-year`}
+                  type="text"
+                  pattern="[0-9]*"
+                  inputMode="numeric"
+                  value={date.year}
+                  onChange={({ target: { value } }) =>
+                    setDate({ ...date, year: value })
+                  }
+                  {...otherProps}
+                />
+              </div>
             </div>
           </div>
-          <div className="govuk-date-input__item">
-            <div className="govuk-form-group">
-              <label
-                className="govuk-label govuk-date-input__label"
-                htmlFor={`${name}-month`}
-              >
-                Month
-              </label>
-              <input
-                className={cx(
-                  'govuk-input govuk-date-input__input govuk-input--width-2',
-                  {
-                    'govuk-input--error': error,
-                  }
-                )}
-                id={`${name}-month`}
-                name={`${name}-month`}
-                type="text"
-                pattern="[0-9]*"
-                inputMode="numeric"
-                value={date.month}
-                onChange={({ target: { value } }) =>
-                  setDate({ ...date, month: value })
-                }
-                {...otherProps}
-              />
-            </div>
-          </div>
-          <div className="govuk-date-input__item">
-            <div className="govuk-form-group">
-              <label
-                className="govuk-label govuk-date-input__label"
-                htmlFor={`${name}-year`}
-              >
-                Year
-              </label>
-              <input
-                className={cx(
-                  'govuk-input govuk-date-input__input govuk-input--width-4',
-                  {
-                    'govuk-input--error': error,
-                  }
-                )}
-                id={`${name}-year`}
-                name={`${name}-year`}
-                type="text"
-                pattern="[0-9]*"
-                inputMode="numeric"
-                value={date.year}
-                onChange={({ target: { value } }) =>
-                  setDate({ ...date, year: value })
-                }
-                {...otherProps}
-              />
-            </div>
-          </div>
-        </div>
-      </fieldset>
-    </div>
-  );
+        </fieldset>
+      </div>
+    );
+  }
+);
+
 };
 
-const ControlledDateInput = ({
-  control,
-  name,
-  rules,
-  label,
-  hint,
-  ...otherProps
-}) => {
-  const inputRef = useRef();
-  return (
-    <Controller
-      as={
-        <DateInput
-          inputRef={inputRef}
-          hint={hint}
-          label={label}
-          rules={rules}
-          name={name}
-          {...otherProps}
-        />
-      }
-      onChange={([value]) => value}
-      name={name}
-      rules={{
-        ...rules,
-        validate: {
-          ...rules?.validate,
-          valid: (value) =>
-            value && (isValid(new Date(value)) || 'Must be a is valid Date'),
-        },
-      }}
-      onFocus={() => inputRef.current.focus()}
-      control={control}
-    ></Controller>
-  );
-};
-
+const ControlledDateInput = ({ control, name, rules, ...otherProps }) => (
+  <Controller
+    as={<DateInput {...otherProps} />}
+    onChange={([value]) => value}
+    name={name}
+    rules={{
+      ...rules,
+      validate: {
+        ...rules?.validate,
+        valid: (value) =>
+          value && (isValid(new Date(value)) || 'Must be a is valid Date'),
+      },
+    }}
+    control={control}
     defaultValue={control.defaultValuesRef.current[name] || null}
+  />
+);
+
 ControlledDateInput.propTypes = {
   label: PropTypes.string,
   labelSize: PropTypes.oneOf(['s', 'm', 'l', 'xl']),
