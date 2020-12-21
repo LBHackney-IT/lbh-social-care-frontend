dad580a17245ccb361337df44e27dfaadf2afa58
Naomi Prescod-Green <naomipgreen@gmail.com>
2020-12-21 23:57:23 +0000

    update

--- components/Spinner/Spinner.jsx
+++ components/Spinner/Spinner.jsx
@@ -2,7 +2,7 @@ const Spinner = () => (
   <svg
     width="50"
     height="50"
-    margin="0 auto"
+    viewBox="0 0 42 42"
     xmlns="http://www.w3.org/2000/svg"
     stroke="#00703c"
     style={{ display: 'block', margin: 'auto' }}
