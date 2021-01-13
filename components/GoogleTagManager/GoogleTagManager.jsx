17e59b95a233820fdf1fbcd3fc43cde59ce7dc3d
Luca Lischetti <luca@sirlisko.com>
2021-01-13 09:50:11 +0000

    added GTM

---
+++ components/GoogleTagManager/GoogleTagManager.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as gtm from 'utils/gtm';
+
+const handleRouteChange = () => {
+  gtm.pageview();
+};
+
+const GoogleTagManager = ({ children }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return children;
+};
+
+export default GoogleTagManager;
