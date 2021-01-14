6da987eae079b75e80fd26741f39ec48f5602635
Luca Lischetti <luca@sirlisko.com>
2021-01-14 12:20:32 +0000

    added chack-auth api

---
+++ pages/api/check-auth.js
@@ -0,0 +1,28 @@
+import * as HttpStatus from 'http-status-codes';
+
+import { isAuthorised } from 'utils/auth';
+
+export default (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const auth = isAuthorised(req);
+        !auth
+          ? res.status(HttpStatus.UNAUTHORIZED).end()
+          : auth.isAuthorised
+          ? res.status(HttpStatus.OK).json(auth)
+          : res.status(HttpStatus.FORBIDDEN).end();
+      } catch (e) {
+        console.log(e);
+        res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ message: 'Unable to get the Cases' });
+      }
+      break;
+
+    default:
+      res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ message: 'Invalid request method' });
+  }
+};
