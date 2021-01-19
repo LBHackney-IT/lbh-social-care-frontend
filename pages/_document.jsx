ad6e30cd1f2e7dc5f44c89d05f243bee5d972d2a
Luca Lischetti <luca@sirlisko.com>
2021-01-19 14:04:41 +0100

    moved from google tag manger to analytics (#133)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-import { GTM_ID } from 'utils/gtm';
+import { GA_TRACKING_ID } from 'utils/gtag';
 
 export default class AppDocument extends Document {
   render() {
@@ -8,28 +8,24 @@ export default class AppDocument extends Document {
       <Html id="root" className="govuk-template lbh-template">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          {/* Google Tag Manager - Global base code */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', '${GTM_ID}');
-              `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
             }}
           />
         </Head>
         <body className="govuk-template__body lbh-template__body js-enabled">
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
           <Main />
           <NextScript />
         </body>
