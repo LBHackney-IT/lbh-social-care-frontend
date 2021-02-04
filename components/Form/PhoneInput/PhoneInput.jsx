c536aeeaeffa577f9fe40e041f4e73e6c23dc3d7
anabebs <anabebic.x@gmail.com>
2021-02-04 18:50:23 +0000

    add minLength to Phone Number Input

--- components/Form/PhoneInput/PhoneInput.jsx
+++ components/Form/PhoneInput/PhoneInput.jsx
@@ -1,7 +1,11 @@
 import { TextInput } from '..';
 
-const PhoneInput = (props) => (
-  <TextInput {...props} minLength="5" type="number"></TextInput>
+const PhoneInput = ({ rules, ...props }) => (
+  <TextInput
+    {...props}
+    rules={{ minLength: 5, ...rules }}
+    type="number"
+  ></TextInput>
 );
 
 export default PhoneInput;
