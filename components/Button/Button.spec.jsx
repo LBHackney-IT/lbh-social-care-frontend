95101158636c42a2c62896ce6f40daf511f8c471
Luca Lischetti <luca@sirlisko.com>
2021-02-08 14:45:01 +0000

    merged LinkButton in Button

--- components/Button/Button.spec.jsx
+++ components/Button/Button.spec.jsx
@@ -1,6 +1,16 @@
 import { render, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
+const mockedUseRouter = {
+  query: { foo: 'bar' },
+  push: jest.fn(),
+  pathname: 'foopath',
+};
+
+jest.mock('next/router', () => ({
+  useRouter: () => mockedUseRouter,
+}));
+
 describe('Button', () => {
   it('renders a button', () => {
     const buttonText = 'My Button';
@@ -15,13 +25,19 @@ describe('Button', () => {
     const { getByText } = render(
       <Button label={buttonText} onClick={myAction} />
     );
-    fireEvent(
-      getByText(buttonText),
-      new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-      })
-    );
+    fireEvent.click(getByText(buttonText));
     expect(myAction).toHaveBeenCalled();
+  });
+
+  it('should render properly with route', () => {
+    const props = {
+      label: 'Foo',
+      route: 'foo/bar',
+      internalQuery: '?foo',
+    };
+    const { getByText } = render(<Button {...props} />);
+    fireEvent.click(getByText('Foo'));
+    expect(mockedUseRouter.push).toHaveBeenCalled();
+    expect(mockedUseRouter.push).toHaveBeenCalledWith('foo/bar?foo');
   });
 });
