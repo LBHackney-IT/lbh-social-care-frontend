1e223c5fd5de0888171e312d8bd5861f96e78f55
Luca Lischetti <luca@sirlisko.com>
2021-03-05 15:30:24 +0000

    set default width to 10

--- components/Form/DatePicker/DatePicker.tsx
+++ components/Form/DatePicker/DatePicker.tsx
@@ -3,7 +3,7 @@ import { TextInput } from '..';
 import type { TextInputNoType as Props } from 'components/Form/types';
 
 const DateInput = (props: Props): React.ReactElement => (
-  <TextInput {...props} type="date" />
+  <TextInput width={10} {...props} type="date" />
 );
 
 export default DateInput;
