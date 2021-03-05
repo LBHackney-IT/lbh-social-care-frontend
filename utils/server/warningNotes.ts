12c280b61b2a6c0b05048d32688b6f2531ba18d5
Luca Lischetti <luca@sirlisko.com>
2021-03-05 17:46:01 +0000

    mocked getWarningNotes

---
+++ utils/server/warningNotes.ts
@@ -0,0 +1,43 @@
+import { WarningNote } from 'types';
+
+const { MOCKED_WARNING_NOTES } = process.env;
+
+export const getWarningNotes = async (
+  personId: number
+): Promise<WarningNote[] | []> => {
+  const mockedWN = !MOCKED_WARNING_NOTES
+    ? []
+    : personId % 3 === 0
+    ? []
+    : personId % 3 === 1
+    ? [
+        {
+          id: 123,
+          type: 'Risk to Adults',
+          createdDate: new Date(2020, 12, 12),
+          createdBy: 'Foo',
+          reviewsDate: new Date(2020, 12, 13),
+          reviewsBy: 'Bar',
+        },
+      ]
+    : [
+        {
+          id: 123,
+          type: 'Risk to Adults',
+          createdDate: new Date(2020, 12, 12),
+          createdBy: 'Foo',
+          reviewsDate: new Date(2020, 12, 13),
+          reviewsBy: 'Bar',
+        },
+        {
+          id: 234,
+          type: 'Risk to Staff',
+          createdDate: new Date(2020, 12, 22),
+          createdBy: 'Foo',
+        },
+      ];
+
+  return await new Promise((resolve) =>
+    setTimeout(() => resolve(mockedWN), 1000)
+  );
+};
