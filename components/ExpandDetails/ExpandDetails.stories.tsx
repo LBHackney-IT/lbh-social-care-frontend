182d2b01e15c4e6717bdf29eff407a3be4aec514
Luca Lischetti <luca@sirlisko.com>
2021-03-09 14:08:12 +0000

    added show details component

---
+++ components/ExpandDetails/ExpandDetails.stories.tsx
@@ -0,0 +1,33 @@
+import { Story } from '@storybook/react';
+
+import ExpandDetails, { Props } from './ExpandDetails';
+
+export default {
+  title: 'ExpandDetails',
+  component: ExpandDetails,
+};
+
+const Template: Story<Props> = (args) => <ExpandDetails {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Show intructions',
+  triggerLabel: 'guidance',
+  children: (
+    <div>
+      foo <strong>bar</strong>
+    </div>
+  ),
+};
+
+export const DefaultOpen = Template.bind({});
+DefaultOpen.args = {
+  label: 'Show intructions',
+  triggerLabel: 'guidance',
+  isDefaultOpen: true,
+  children: (
+    <div>
+      foo <strong>bar</strong>
+    </div>
+  ),
+};
