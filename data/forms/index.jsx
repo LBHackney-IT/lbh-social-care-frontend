6a13b6bf42231b61fe7ef69050ae9b6d7d4f8fde
Naomi Prescod-Green <naomipgreen@gmail.com>
2021-03-10 09:08:41 +0000

    updated form view (#229)

---
+++ data/forms/index.jsx
@@ -0,0 +1,4 @@
+import ASC_case_note from './asc-case-notes-recording';
+import CFS_case_note from './cfs-case-notes-recording';
+
+export { ASC_case_note, CFS_case_note };
---
+++ pages/people/[id]/records/[recordId]/index.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router';
+
+import Seo from 'components/Layout/Seo/Seo';
+import BackButton from 'components/Layout/BackButton/BackButton';
+import CaseRecap from 'components/Cases/CaseRecap/CaseRecap';
+import PersonView from 'components/PersonView/PersonView';
+
+const CaseView = (): React.ReactElement => {
+  const {
+    query: { id, recordId },
+  } = useRouter();
+  return (
+    <>
+      <Seo title={`#${recordId} record`} />
+      <BackButton />
+      <h1 className="govuk-fieldset__legend--l gov-weight-lighter">
+        Case note for
+      </h1>
+      <PersonView personId={id as string} expandView>
+        <CaseRecap personId={id as string} recordId={recordId as string} />
+      </PersonView>
+    </>
+  );
+};
+
+export default CaseView;
