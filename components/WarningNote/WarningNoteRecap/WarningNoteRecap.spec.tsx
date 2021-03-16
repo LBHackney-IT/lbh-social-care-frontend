4a6e4604e9c71b4bfe02a2a1205f3ca97e6320a8
Miles-Alford <43789512+Miles-Alford@users.noreply.github.com>
2021-03-16 12:11:37 +0000

    Warning Note Review Page: Add warning note recap (#259)
    * WIP: Add review warning note page and create warning note recap component
    
    * WIP: update WarningNoteRecap component and add tests, add correct URL to getWarningNote endpoint
    
    * added use prefix for getWarningNote
    
    * improved dates on summary
    
    * slightly updated warningNote type to make our life easier
    
    * align form cases to what expected from the APIs
    
    * fixed layout
    
    * Add tests for API calls in utils/api for warning notes
    
    Co-authored-by: Luca Lischetti <luca@sirlisko.com>

---
+++ components/WarningNote/WarningNoteRecap/WarningNoteRecap.spec.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react';
+
+import WarningNoteRecap from './WarningNoteRecap';
+import * as warningNotes from 'utils/api/warningNotes';
+import { mockedWarningNote } from '../WarningNotes.fixtures';
+
+jest.mock('components/Spinner/Spinner', () => () => 'MockedSpinner');
+
+jest.mock('utils/api/warningNotes', () => ({
+  useWarningNote: jest.fn(),
+}));
+
+describe(`WarningNoteRecap`, () => {
+  jest.spyOn(warningNotes, 'useWarningNote').mockImplementation(() => ({
+    data: mockedWarningNote.find(({ id }) => id === props.warningNoteId),
+    mutate: jest.fn(),
+    revalidate: jest.fn(),
+    isValidating: false,
+  }));
+  const props = {
+    person: {
+      dateOfBirth: '2020-11-13',
+      firstName: 'Ciasom',
+      lastName: 'Tesselate',
+      mosaicId: '44000000',
+      nhsNumber: '12345',
+      ageContext: 'A',
+      gender: 'F',
+    },
+    warningNoteId: 123,
+  };
+
+  it('should render correctly', async () => {
+    const { asFragment } = render(<WarningNoteRecap {...props} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
