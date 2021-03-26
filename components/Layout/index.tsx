30f841c05b3ccecb6833ec2d7e185cc951693aa6
Luca Lischetti <luca@sirlisko.com>
2021-03-26 16:53:37 +0000

    removed interim from the project

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -11,9 +11,9 @@ const Layout = ({ children }: Props): React.ReactElement => {
   const feedbackLink = process.env.NEXT_PUBLIC_FEEDBACK_LINK || '';
   return (
     <>
-      <Seo title="Interim Social Care Admin - Hackney Council" />
+      <Seo title="Social Care Admin - Hackney Council" />
       <SkipLink />
-      <Header serviceName="Interim Social Care" />
+      <Header serviceName="Social Care" />
       <div className="govuk-width-container">
         <PhaseBanner phase="beta" feedbackLink={feedbackLink} />
         <main className="govuk-main-wrapper" id="content" role="main">
