78fd1160b9c71bbae52c5a8d037e34aa6959ffc4
Jaye Hackett <jaye.hackett@gmail.com>
2021-03-30 16:00:59 +0100

    fix up access denied page

--- pages/access-denied.tsx
+++ pages/access-denied.tsx
@@ -1,11 +1,19 @@
 import Seo from 'components/Layout/Seo/Seo';
+import Link from 'next/link';
 
 const AccessDenied = (): React.ReactElement => (
   <>
-    <Seo title="Access Denied" />
-    <h1>Access denied</h1>
-    <p className="govuk-body">
-      Sorry, but access to that page is for administrators only.
+    <Seo title="Access denied" />
+    <h1 className="lbh-heading-h1">Access denied</h1>
+    <p className="lbh-body">
+      You don&apos;t have permission to view this page.
+    </p>
+    <p className="lbh-body">
+      You may need to{' '}
+      <Link href="/login">
+        <a className="lbh-link">sign in</a>
+      </Link>
+      .
     </p>
   </>
 );
