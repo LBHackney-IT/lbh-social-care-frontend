527474e6612b4db5d88ec144150dfd94179f3e42
Jaye Hackett <jaye.hackett@gmail.com>
2021-03-31 22:09:59 +0100

    errant div

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,25 +14,23 @@ const AdminLoginPage = ({ gssoUrl, returnUrl }: Props): React.ReactElement => (
   <main className="lbh-main-wrapper" id="main-content" role="main">
     <Seo title="Sign in" />
     <h1 className="lbh-heading-h1">Sign in</h1>
-    <div>
-      <a
-        className="govuk-button lbh-button  lbh-button--start govuk-button--start"
-        href={`${gssoUrl}${returnUrl}`}
+    <a
+      className="govuk-button lbh-button  lbh-button--start govuk-button--start"
+      href={`${gssoUrl}${returnUrl}`}
+    >
+      Sign in with Google{' '}
+      <svg
+        className="govuk-button__start-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="17.5"
+        height="19"
+        viewBox="0 0 33 40"
+        role="presentation"
+        focusable="false"
       >
-        Sign in with Google{' '}
-        <svg
-          className="govuk-button__start-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="17.5"
-          height="19"
-          viewBox="0 0 33 40"
-          role="presentation"
-          focusable="false"
-        >
-          <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
-        </svg>
-      </a>
-    </div>
+        <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
+      </svg>
+    </a>
     <p className="lbh-body">Please log in with your Hackney email account.</p>
     <p className="lbh-body">
       Speak to your manager if you have issues logging in.
