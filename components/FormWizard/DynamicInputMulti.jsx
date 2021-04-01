7598403103daa874df0286d96de8e54f92a45a0f
Jaye <joshua.hackett93@gmail.com>
2021-04-01 13:05:41 +0100

    tiny fixes

--- components/FormWizard/DynamicInputMulti.jsx
+++ components/FormWizard/DynamicInputMulti.jsx
@@ -4,6 +4,7 @@ import cx from 'classnames';
 
 import DynamicInput from 'components/FormWizard/DynamicInput';
 import DeleteIcon from 'components/Icons/TimesCircle';
+import AddIcon from 'components/Icons/Add';
 
 import styles from './DynamicInputMulti.module.scss';
 
@@ -66,24 +67,7 @@ const DynamicInputMulti = ({
           role="button"
           onClick={() => setCounter(counter + 1)}
         >
-          <svg
-            width="11"
-            height="11"
-            viewBox="0 0 11 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="4.5293"
-              y="11"
-              width="11"
-              height="1.94118"
-              transform="rotate(-90 4.5293 11)"
-              fill="#00664F"
-            />
-            <rect y="4.52942" width="11" height="1.94118" fill="#00664F" />
-          </svg>
-
+          <AddIcon />
           {isMultiTrigger}
         </button>
       </div>
---
+++ components/Icons/Add.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Add = (): React.ReactElement => (
+  <svg
+    width="11"
+    height="11"
+    viewBox="0 0 11 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="4.5293"
+      y="11"
+      width="11"
+      height="1.94118"
+      transform="rotate(-90 4.5293 11)"
+      fill="#00664F"
+    />
+    <rect y="4.52942" width="11" height="1.94118" fill="#00664F" />
+  </svg>
+);
+
+export default Add;
