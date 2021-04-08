b6a14be219dc047aa0c86d0421e6d59d294d66e5
Luca Lischetti <luca@sirlisko.com>
2021-04-08 10:20:11 +0100

    some UI improvements

--- components/Dashboard/MyData.tsx
+++ components/Dashboard/MyData.tsx
@@ -1,70 +1,72 @@
 import { useState } from 'react';
+import cx from 'classnames';
 import { useRouter } from 'next/router';
 
 import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
 import Spinner from 'components/Spinner/Spinner';
 import { useMyData } from 'utils/api/me';
-import cx from 'classnames';
 import { getUserType } from 'utils/user';
+import { Team } from 'types';
 
 import styles from './MyData.module.scss';
-import { Team } from 'types';
 
 const MyData = (): React.ReactElement => {
   const { query, replace, pathname } = useRouter();
-  const [expandView, setExpandView] = useState(Boolean(query.details));
+  const [expandView, setExpandView] = useState(Boolean(query?.details));
   const { data, error } = useMyData();
-
   if (error) {
     return <ErrorMessage />;
   }
   if (!data) {
     return <Spinner />;
   }
+  const onExapandToggle = () => {
+    setExpandView(!expandView);
+    replace(
+      `${pathname}?details=${expandView}`,
+      `${pathname}?details=${expandView}`,
+      { shallow: true, scroll: false }
+    );
+  };
   return (
     <div className={styles.container}>
       <div className={cx(styles.header, 'lbh-table-header')}>
         <h2>
           {data.firstName} {data.lastName}
         </h2>
-        <div
-          className="govuk-link"
-          style={{ color: 'white' }}
-          role="button"
-          onClick={() => {
-            setExpandView(!expandView);
-            replace(
-              `${pathname}?details=${expandView}`,
-              `${pathname}?details=${expandView}`,
-              { shallow: true, scroll: false }
-            );
-          }}
+        <button
+          className="govuk-link govuk-link--underline"
+          onClick={onExapandToggle}
         >
           {expandView ? 'Collapse' : 'Expand'} view
-        </div>
+        </button>
       </div>
       {expandView && (
         <div className={styles.body}>
-          <dl className="govuk-grid-row">
-            <div className="govuk-grid-column-one-half">
+          <div className="govuk-grid-row">
+            <div
+              className={cx('govuk-grid-column-one-half', styles.descriptions)}
+            >
               <div>Role: {data.role}</div>
               {data.teams.length > 0 && (
                 <div>Team: {data.teams.map(({ name }: Team) => name)}</div>
               )}
             </div>
-            <div className="govuk-grid-column-one-half">
-              <div>Area: {getUserType(data.auth)} Social Care</div>
+            <div
+              className={cx('govuk-grid-column-one-half', styles.descriptions)}
+            >
+              <div>Area: {getUserType(data.auth)} social care</div>
               <div>
                 Access:{' '}
                 {data.auth.hasUnrestrictedPermissions
-                  ? 'No Restriction'
-                  : ' Restricted'}{' '}
+                  ? 'No restriction'
+                  : 'Restricted'}
               </div>
               {data.auth.hasAllocationsPermissions && (
-                <div> Permission: Can Allocate</div>
+                <div>Permission: Can allocate</div>
               )}
             </div>
-          </dl>
+          </div>
         </div>
       )}
     </div>
