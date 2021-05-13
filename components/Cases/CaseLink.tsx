56ade63a5b77b7ab3b879e3a91fbf044d2dede6a
Renny Fadoju <rennyvbirds@yahoo.com>
2021-05-13 10:38:52 +0100

    Add view link for a warning note

--- components/Cases/CaseLink.tsx
+++ components/Cases/CaseLink.tsx
@@ -28,6 +28,8 @@ const getLink = (
       return `/people/${caseFormData.mosaic_id}/records/${recordId}?is_historical=${caseFormData.is_historical}`;
     case 'Historical_Visit':
       return `/people/${caseFormData.mosaic_id}/visits/${recordId}?is_historical=${caseFormData.is_historical}`;
+    case 'Warning_Note':
+      return `/people/${caseFormData.mosaic_id}/warning-notes/${recordId}`;
     default:
       return null;
   }
