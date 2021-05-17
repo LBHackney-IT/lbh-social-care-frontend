400eb0f75c23b7569b8e529cda5c4081e75478d6
JohnFarrellDev <johnfarrell@fastmail.com>
2021-05-17 11:44:02 +0100

    test person widget

--- components/PersonWidget/PersonWidget.tsx
+++ components/PersonWidget/PersonWidget.tsx
@@ -5,30 +5,29 @@ import { format } from 'date-fns';
 
 const prettyDate = (isoDateString: string): string => {
   const parsed = new Date(isoDateString);
-  return parsed ? format(parsed, 'd MMM yyyy') : '';
+  try {
+    return format(parsed, 'd MMM yyyy');
+  } catch (e) {
+    return '';
+  }
 };
 
 interface Props {
-  person: Resident | false;
+  person?: Resident;
 }
 
 const PersonWidget = ({ person }: Props): React.ReactElement => {
+  const dateOfBirth = prettyDate(person?.dateOfBirth ?? '');
+
   if (person) {
     return (
       <aside className={s.aside}>
         <h2 className={`lbh-heading-h3 ${s.title}`}>
           {person.firstName} {person.lastName}
         </h2>
-
-        <p className={`lbh-body-s ${s.paragraph}`}>Referred 12 Feb 2021</p>
-        <p className={`lbh-body-s ${s.paragraph}`}>
-          Born {prettyDate(person.dateOfBirth ?? '')}
-        </p>
-        <p className={`lbh-body-s ${s.paragraph}`}>Allocated to Namey McName</p>
-
-        <p className={`lbh-body-s ${s.important}`}>
-          <strong>1 warning</strong> <span>2 open actions</span>
-        </p>
+        {dateOfBirth && (
+          <p className={`lbh-body-s ${s.paragraph}`}>Born {dateOfBirth}</p>
+        )}
       </aside>
     );
   }
