7280b19c724a792f97155bda4f96d3bef7b3ffd4
Jaye Hackett <jaye.hackett@gmail.com>
2021-05-20 23:23:02 +0100

    datepicker defaults to current date

--- components/Form/DatePicker/DatePicker.tsx
+++ components/Form/DatePicker/DatePicker.tsx
@@ -1,9 +1,27 @@
 import { TextInput } from '..';
+import { TextInputNoType } from 'components/Form/types';
 
-import type { TextInputNoType as Props } from 'components/Form/types';
+import { format } from 'date-fns';
 
-const DateInput = (props: Props): React.ReactElement => (
-  <TextInput width={10} {...props} type="date" />
-);
+format(new Date(2014, 1, 11), 'MM/dd/yyyy');
+
+interface Props extends TextInputNoType {
+  defaultToday?: boolean;
+}
+
+const DateInput = ({ defaultToday, ...props }: Props): React.ReactElement => {
+  return (
+    <>
+      <TextInput
+        width={10}
+        {...props}
+        type="date"
+        defaultValue={
+          defaultToday ? format(new Date(), 'yyyy-MM-dd') : undefined
+        }
+      />
+    </>
+  );
+};
 
 export default DateInput;
