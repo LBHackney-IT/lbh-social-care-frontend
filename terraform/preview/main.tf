0037f87c37365cdd87937f80b1f9378330d6b118
Chris Hutchinson <chris_hutchinson@me.com>
2021-05-21 16:25:42 +0100

    Assume staging role on all branches, start TF config for Route53

---
+++ terraform/preview/main.tf
@@ -0,0 +1,32 @@
+# INSTRUCTIONS:
+# 1) ENSURE YOU POPULATE THE LOCALS
+# 2) ENSURE YOU REPLACE ALL INPUT PARAMETERS, THAT CURRENTLY STATE 'ENTER VALUE', WITH VALID VALUES
+# 3) YOUR CODE WOULD NOT COMPILE IF STEP NUMBER 2 IS NOT PERFORMED!
+# 4) ENSURE YOU CREATE A BUCKET FOR YOUR STATE FILE AND YOU ADD THE NAME BELOW - MAINTAINING THE STATE OF THE INFRASTRUCTURE YOU CREATE IS ESSENTIAL - FOR APIS, THE BUCKETS ALREADY EXIST
+# 5) THE VALUES OF THE COMMON COMPONENTS THAT YOU WILL NEED ARE PROVIDED IN THE COMMENTS
+# 6) IF ADDITIONAL RESOURCES ARE REQUIRED BY YOUR API, ADD THEM TO THIS FILE
+# 7) ENSURE THIS FILE IS PLACED WITHIN A 'terraform' FOLDER LOCATED AT THE ROOT PROJECT DIRECTORY
+
+provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 2.0"
+}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-staging-apis"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/lbh-social-care-frontend/state"
+  }
+}
+
+# resource "aws_route53_zone" "preview" {
+#   name = "social-care-service-preview.hackney.gov.uk"
+
+#   tags = {
+#     Environment = "preview"
+#   }
+# }
