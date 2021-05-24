29b7d8b1fce8f68f6d39c6cd9268697c4ffd3acf
mapleleafjack <jack.musajo@madetech.com>
2021-05-24 11:44:26 +0100

    SCT-204  Write front end views for displaying relationships

--- utils/api/relationships.spec.ts
+++ utils/api/relationships.spec.ts
@@ -8,10 +8,7 @@ describe('relationships APIs', () => {
     it('should work properly', () => {
       jest.spyOn(SWR, 'default');
       relationshipsAPI.useRelationships(123);
-      expect(SWR.default).toHaveBeenCalledWith(
-        'https://virtserver.swaggerhub.com/Hackney/social-care-case-viewer-api/1.0.0/residents/123/relationships'
-      );
-      // expect(SWR.default).toHaveBeenCalledWith('/residents/123/relationships');
+      expect(SWR.default).toHaveBeenCalledWith('/residents/123/relationships');
     });
   });
 });
--- utils/api/relationships.ts
+++ utils/api/relationships.ts
@@ -5,7 +5,4 @@ import type { RelationshipData, ErrorAPI } from 'types';
 export const useRelationships = (
   id: number
 ): SWRResponse<RelationshipData, ErrorAPI> =>
-  useSWR(
-    `https://virtserver.swaggerhub.com/Hackney/social-care-case-viewer-api/1.0.0/residents/${id}/relationships`
-  );
-// useSWR(`/api/residents/${id}/relationships`);
+  useSWR(`/api/residents/${id}/relationships`);
