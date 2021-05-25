8caadc2934e8f663fda5f660a04c3c2ea4225a82
mapleleafjack <jack.musajo@madetech.com>
2021-05-25 11:56:18 +0100

    SCT-30 - Maintenance mode test (#390)

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -5,7 +5,7 @@ const Maintenance = (): React.ReactElement => (
     <Seo title="Maintenance mode" />
     <h1>Sorry, the service is unavailable</h1>
     <p className="govuk-body">
-      You will be able to use the service from 8pm on Tuesday 24 May 2021.
+      You will be able to use the service from 8pm on Tuesday 25 May 2021.
     </p>
   </>
 );
