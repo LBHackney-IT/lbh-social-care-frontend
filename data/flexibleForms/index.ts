789a878ab56506c2d5bb84ac3171eb07986d49c3
ana <anabebic.x@gmail.com>
2021-05-26 09:54:30 +0100

    wip

--- data/flexibleForms/index.ts
+++ data/flexibleForms/index.ts
@@ -4,7 +4,7 @@ import { Form } from './forms.types';
 const formData: Form[] = [];
 
 for (let i = 0; i < allForms[0].length; i++) {
-  const form = allForms[i][0] as unknown as Form;
+  const form = allForms as unknown as Form;
 
   formData.push(form);
 }
