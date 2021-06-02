e9dd97104831b324e3048bcc93fff292fdc271d5
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-02 12:10:15 +0100

    remove job we don't need, fix bugs

--- data/flexibleForms/index.ts
+++ data/flexibleForms/index.ts
@@ -1,12 +1,14 @@
 import allForms from './forms.json';
+import review3c from './review3C.json';
 import { Form } from './forms.types';
 
 const formData: Form[] = [];
 
 for (let i = 0; i < allForms.length; i++) {
   const form = allForms[i] as unknown as Form;
-
   formData.push(form);
 }
+
+formData.push(review3c as unknown as Form);
 
 export default formData;
