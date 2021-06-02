4b8ba9a4358b89ec3b6b10ab7d4f4341e2754196
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-02 21:14:08 +0100

    new page for submissions

--- components/Cases/CaseRecap/CaseNote.tsx
+++ components/Cases/CaseRecap/CaseNote.tsx
@@ -4,8 +4,6 @@ import Summary from 'components/Summary/Summary';
 import { useCase } from 'utils/api/cases';
 import * as form from 'data/forms';
 import { FormStep } from 'components/Form/types';
-import FlexibleAnswers from 'components/FlexibleAnswers/FlexibleAnswers';
-
 interface Props {
   personId: number;
   recordId: string;
@@ -24,18 +22,6 @@ const CaseNote = ({ personId, recordId }: Props): React.ReactElement => {
   }
   if (!record || !fileData) {
     return <Spinner />;
-  }
-
-  // this needs to be fixed with a better way of distinguishing
-  if (
-    record?.caseFormData?.case_note_description &&
-    record.caseFormData.form_name === 'Case recording'
-  ) {
-    return (
-      <FlexibleAnswers
-        answers={JSON.parse(record?.caseFormData?.case_note_description)}
-      />
-    );
   }
 
   return (
