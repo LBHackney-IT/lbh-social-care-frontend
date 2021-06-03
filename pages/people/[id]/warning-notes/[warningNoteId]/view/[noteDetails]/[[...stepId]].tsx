850a3b5fad3923b977563030affc45bbbbd05c1f
Renny Omorinsola Fadoju <32823756+Renny4Real@users.noreply.github.com>
2021-06-03 11:59:03 +0100

    feat(SCT-228): Create warning note details pages for reviewed & ended warning notes (#391)
    * Update name of components in note review form step
    
    The names `reviewDiscussion` & `reviewNotes` don't match the review object.
    
    This changes the names of these components to match
    the names of respective properties from the review object.
    
    * Update the warning note type to match API response
    
    This changes the property name in the type to match
    the property name for reviews as returned by the API
    
    * Add a page that displays warning note reviews
    
    - This changes the view case link for a warning note case note
    to include whether the case note is for a created warning note or a reviewed one.
    
    - This takes the user to a page containing the summary of all
    warning note reviews submitted
    if the case note link is for a reviewed warning note entry.
    
    - If the case note link is for a created warning note,
    the page will display only
    the initial details of the warning note upon creation.
    
    * feat(SCT-229): Create warning note end details page (#409)
    
    * Display review details for a closed warning note
    
    This adds a new function that would take the existing form steps
    that we use to create a dynamic summary of each review and
    adds components that we don't ask in the form
    but want to display in the summary i.e end date, who created the review
    & the next review date without the dependency on the review decision.
    
    This modfied form step is then passed as a prop to the summary component
    and renders those pieces of information as well if they are not null.
    
    This also adds cypress tests for viewing the warning note end details page
    
    * Only display end date / next review date on the most recent review
    
    This adds a condition to check for the most recent review (which would be displayed first)
    and pass a value for the end date / next review date if it is.
    
    Since the end date and next review date are values saved on the original warning note,
    we only want to pass these values in on the most recent review rather than being repeated for each review
    on the page.
    
    * Replace curly braces in form Path with double quotes
    
    Co-authored-by: Chris Hutchinson <1573022+chrishutchinson@users.noreply.github.com>
    
    Co-authored-by: Chris Hutchinson <1573022+chrishutchinson@users.noreply.github.com>
    
    * (Refactor) : Changes based on PR comments
    
    - Uses a path parameter for the warning note view link:
    This initially created a view link using query params
    but it was suggested to change this to be a nested route
    as there are primarily only three ways a user should see
    the warning note details page depending on
    the stage of the warning note i.e
    * Warning note created
    * Warning note reviewed
    * Warning note ended
    
    - It also refactors some of the code to improve readablity and
    communicate the intention of each bit of functionality in a
    (hopefully!) clearer way.
    
    - This also adds a conditional render to the form steps type.
    Before we were creating and editing a copy
    and using that within the component to render the summary.
    It was suggested that creating and manipulating copies like this could
    get unwieldly and one would have to remember all the different "copies" of the code
    and why they exist in the niches
     which could make the code harder to maintain in the future.
    
    * Remove form name property from mocked warning note
    
    * Update case link spec
    
    This updates the test for checking the case link generated for
    viewing warning notes to check that the right path is generated
    when passed a form name
    
    Co-authored-by: Chris Hutchinson <1573022+chrishutchinson@users.noreply.github.com>

--- pages/people/[id]/warning-notes/[warningNoteId]/view/[noteDetails]/[[...stepId]].tsx
+++ pages/people/[id]/warning-notes/[warningNoteId]/view/[noteDetails]/[[...stepId]].tsx
@@ -7,17 +7,35 @@ const ViewWarningNote = (): React.ReactElement => {
   const { query } = useRouter();
   const personId = Number(query.id as string);
   const warningNoteId = Number(query.warningNoteId as string);
+  const noteDetails = query.noteDetails as string;
+
+  const getTitle = (noteDetails: string) => {
+    switch (noteDetails) {
+      case 'note-created':
+        return 'Warning Note Details';
+      case 'note-reviews':
+        return 'Warning Note Review Details';
+      case 'note-end':
+        return 'Warning Note End Details';
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
       <h1 className="govuk-fieldset__legend--l gov-weight-lighter">
-        Warning Note Details
+        {getTitle(noteDetails)}
       </h1>
       <PersonView personId={personId} expandView>
         {(person) => (
           <>
             <div className="govuk-!-margin-top-7">
-              <WarningNoteRecap person={person} warningNoteId={warningNoteId} />
+              <WarningNoteRecap
+                person={person}
+                warningNoteId={warningNoteId}
+                noteDetails={noteDetails}
+              />
             </div>
           </>
         )}
