b7b52a106dd6e8444d6d2728269e03768a6a21b6
Jaye <joshua.hackett93@gmail.com>
2021-06-08 18:10:45 +0100

    clean up api route

--- pages/api/submissions/[id]/index.ts
+++ pages/api/submissions/[id]/index.ts
@@ -1,8 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import forms from 'data/flexibleForms';
-import { getResident } from 'lib/residents';
-import { isAuthorised } from 'utils/auth';
-import { getPermissionFlag } from 'utils/user';
 import StatusCodes from 'http-status-codes';
 import { getSubmissionById, finishSubmission } from 'lib/submissions';
 
@@ -10,10 +7,6 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  let permissionFlag;
-  const user = isAuthorised(req);
-  if (user) permissionFlag = getPermissionFlag(user);
-
   const { id } = req.query;
 
   switch (req.method) {
