0956e7ae840bf57dde9a8d554ed231364e7bf1d0
Jaye <joshua.hackett93@gmail.com>
2021-06-11 13:24:59 +0100

    prefill first and last name

--- pages/people/add/[[...stepId]].tsx
+++ pages/people/add/[[...stepId]].tsx
@@ -1,4 +1,5 @@
 import { useAuth } from 'components/UserContext/UserContext';
+import { useRouter } from 'next/router';
 import FormWizard from 'components/FormWizard/FormWizard';
 import { addResident } from 'utils/api/residents';
 import CustomConfirmation from 'components/Steps/PersonConfirmation';
@@ -19,6 +20,7 @@ interface FormData {
 }
 
 const CreateNewPerson = (): React.ReactElement => {
+  const { query } = useRouter();
   const { user } = useAuth() as { user: User };
   const onFormSubmit = async (formData: FormData) => {
     const ref = await addResident({
@@ -29,13 +31,18 @@ const CreateNewPerson = (): React.ReactElement => {
     });
     return ref;
   };
+
   return (
     <FormWizard
       formPath="/people/add/"
       formSteps={formSteps}
       title="Add New Person"
       onFormSubmit={onFormSubmit}
-      defaultValues={{ user }}
+      defaultValues={{
+        user,
+        firstName: query.first_name,
+        lastName: query.last_name,
+      }}
       successMessage="Add new person confirmed"
       customConfirmation={CustomConfirmation}
       stepHeader={StepHeader}
