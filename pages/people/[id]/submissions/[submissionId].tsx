05cf8bd6440ff7b4a306e2584a664ac60797e4fb
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-14 15:38:38 +0100

    add flexible answers component (#341)
    * add flexible answers component
    
    * improve type guarding
    
    * improve type guarding
    
    * wip
    
    * render flexible answers under correct circumstances
    
    * fix bug with non-mappable data
    
    * fix type error?
    
    * Update components/FlexibleAnswers/FlexibleAnswers.stories.tsx
    
    Co-authored-by: John Farrell <johnkevinfarrell93@gmail.com>
    
    * Update tsconfig.json
    
    * refactor props and maps
    
    * fix type error
    
    * Update FlexibleAnswers.tsx
    
    * enhance flexible answers with collapsible sections
    
    * new page for submissions
    
    * grab person info
    
    * new wording on task list
    
    * fix test
    
    Co-authored-by: Jaye <joshua.hackett93@gmail.com>
    Co-authored-by: John Farrell <johnkevinfarrell93@gmail.com>

---
+++ pages/people/[id]/submissions/[submissionId].tsx
@@ -0,0 +1,66 @@
+import { GetServerSideProps } from 'next';
+import { Submission } from 'data/flexibleForms/forms.types';
+import { getSubmissionById } from 'lib/submissions';
+import { getResident } from 'lib/residents';
+import FlexibleAnswers from 'components/FlexibleAnswers/FlexibleAnswers';
+import Head from 'next/head';
+import s from 'stylesheets/Sidebar.module.scss';
+import PersonWidget from 'components/PersonWidget/PersonWidget';
+import { Resident } from 'types';
+import { isAuthorised } from 'utils/auth';
+import { getPermissionFlag } from 'utils/user';
+
+interface Props {
+  submission: Submission;
+  person: Resident;
+}
+
+const SubmissionPage = ({ submission, person }: Props): React.ReactElement => (
+  <>
+    <Head>
+      <title>{'Submission'} | Social care | Hackney Council</title>
+    </Head>
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-two-thirds">
+        <h1 className="lbh-heading-h1 govuk-!-margin-bottom-8">
+          {'Submission'}
+        </h1>
+      </div>
+    </div>
+    <div className={`govuk-grid-row ${s.outer}`}>
+      <div className="govuk-grid-column-two-thirds">
+        <FlexibleAnswers answers={submission.answers} />
+      </div>
+      <div className="govuk-grid-column-one-third">
+        <div className={s.sticky}>
+          <p className="lbh-body">This is for:</p>
+          <PersonWidget person={person} />
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const submission = await getSubmissionById(String(params?.submissionId));
+
+  let permissionFlag;
+  const user = isAuthorised(req);
+  if (user) permissionFlag = getPermissionFlag(user);
+
+  const person = await getResident(Number(params?.id), {
+    context_flag: permissionFlag,
+  });
+
+  return {
+    props: {
+      submission,
+      person,
+    },
+  };
+};
+
+export default SubmissionPage;
