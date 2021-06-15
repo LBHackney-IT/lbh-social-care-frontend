c7cb146e83a30a0aa51cfd89d1b831d040b100d7
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-15 20:18:11 +0100

    clean up submission page layout

--- pages/people/[id]/submissions/[submissionId].tsx
+++ pages/people/[id]/submissions/[submissionId].tsx
@@ -7,37 +7,47 @@ import Head from 'next/head';
 import s from 'stylesheets/Sidebar.module.scss';
 import PersonWidget from 'components/PersonWidget/PersonWidget';
 import { Resident } from 'types';
+import forms from 'data/flexibleForms';
 
 interface Props {
   submission: Submission;
   person: Resident;
 }
 
-const SubmissionPage = ({ submission, person }: Props): React.ReactElement => (
-  <>
-    <Head>
-      <title>{'Submission'} | Social care | Hackney Council</title>
-    </Head>
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-two-thirds">
-        <h1 className="lbh-heading-h1 govuk-!-margin-bottom-8">
-          {'Submission'}
-        </h1>
-      </div>
-    </div>
-    <div className={`govuk-grid-row ${s.outer}`}>
-      <div className="govuk-grid-column-two-thirds">
-        <FlexibleAnswers answers={submission.formAnswers} />
-      </div>
-      <div className="govuk-grid-column-one-third">
-        <div className={s.sticky}>
-          <p className="lbh-body">This is for:</p>
-          <PersonWidget person={person} />
+const SubmissionPage = ({ submission, person }: Props): React.ReactElement => {
+  const form = forms.find((form) => form.id === submission.formId);
+
+  return (
+    <>
+      <Head>
+        <title>
+          {form?.name} | {person.firstName} {person.lastName} | Social care |
+          Hackney Council
+        </title>
+      </Head>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          <h1 className="lbh-heading-h1 govuk-!-margin-bottom-8">
+            {form?.name}
+          </h1>
         </div>
       </div>
-    </div>
-  </>
-);
+      <div className={`govuk-grid-row ${s.outer}`}>
+        <div className="govuk-grid-column-two-thirds">
+          <FlexibleAnswers answers={submission.formAnswers} />
+        </div>
+        <div className="govuk-grid-column-one-third">
+          <div className={s.sticky}>
+            <p className="lbh-body">This is for:</p>
+            <PersonWidget person={person} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+SubmissionPage.goBackButton = true;
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const submission = await getSubmissionById(String(params?.submissionId));
