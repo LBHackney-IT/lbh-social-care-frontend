2f1d062b81903a94fd82bf8f84e2e6dc56c1a3fc
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-16 18:59:08 +0100

    simplify step form

--- components/FlexibleForms/StepForm.tsx
+++ components/FlexibleForms/StepForm.tsx
@@ -5,8 +5,6 @@ import FlexibleField from './FlexibleFields';
 import { Resident } from 'types';
 import Banner from './Banner';
 import { generateInitialValues, InitialValues } from 'lib/utils';
-import { useRouter } from 'next/router';
-import useWarnUnsavedChanges from 'hooks/useWarnUnsavedChanges';
 
 interface Props {
   fields: Field[];
@@ -23,86 +21,47 @@ interface Props {
   singleStep?: boolean;
 }
 
-const WarnUnsavedChanges = ({ warn }: { warn: boolean }) => {
-  useWarnUnsavedChanges(warn);
-  return null;
-};
-
 const StepForm = ({
   initialValues,
   fields,
   person,
   onSubmit,
-  onFinish,
   singleStep,
-}: Props): React.ReactElement => {
-  const router = useRouter();
-
-  return (
-    <Formik
-      initialValues={initialValues || generateInitialValues(fields, person)}
-      validationSchema={generateFlexibleSchema(fields)}
-      onSubmit={onSubmit}
-      validateOnMount={true}
-    >
-      {({
-        values,
-        isSubmitting,
-        setSubmitting,
-        touched,
-        errors,
-        setStatus,
-        status,
-        submitForm,
-        isValid,
-        dirty,
-      }) => (
-        <Form>
-          <WarnUnsavedChanges warn={dirty && !isSubmitting} />
-
-          {status && (
-            <Banner
-              title="There was a problem saving your answers"
-              className="lbh-page-announcement--warning"
-            >
-              <p>Please refresh the page or try again later.</p>
-              <p className="lbh-body-xs">{status}</p>
-            </Banner>
-          )}
-
-          {fields.map((field) => (
-            <FlexibleField
-              key={field.id}
-              field={field}
-              touched={touched}
-              errors={errors}
-              values={values}
-            />
-          ))}
-
-          <button
-            className="govuk-button lbh-button"
-            disabled={isSubmitting}
-            onClick={async () => {
-              await submitForm();
-              // next, finish the submission if it's the only step, or return to the task list
-              if (isValid) {
-                if (singleStep) {
-                  setSubmitting(true);
-                  onFinish(values, setStatus);
-                } else {
-                  if (!isSubmitting)
-                    router.push(`/submissions/${router.query.id}`);
-                }
-              }
-            }}
+}: Props): React.ReactElement => (
+  <Formik
+    initialValues={initialValues || generateInitialValues(fields, person)}
+    validationSchema={generateFlexibleSchema(fields)}
+    onSubmit={onSubmit}
+    validateOnMount={true}
+  >
+    {({ values, isSubmitting, touched, errors, status }) => (
+      <Form>
+        {status && (
+          <Banner
+            title="There was a problem saving your answers"
+            className="lbh-page-announcement--warning"
           >
-            {singleStep ? 'Save and finish' : 'Save and continue'}
-          </button>
-        </Form>
-      )}
-    </Formik>
-  );
-};
+            <p>Please refresh the page or try again later.</p>
+            <p className="lbh-body-xs">{status}</p>
+          </Banner>
+        )}
+
+        {fields.map((field) => (
+          <FlexibleField
+            key={field.id}
+            field={field}
+            touched={touched}
+            errors={errors}
+            values={values}
+          />
+        ))}
+
+        <button className="govuk-button lbh-button" disabled={isSubmitting}>
+          {singleStep ? 'Save and finish' : 'Save and continue'}
+        </button>
+      </Form>
+    )}
+  </Formik>
+);
 
 export default StepForm;
