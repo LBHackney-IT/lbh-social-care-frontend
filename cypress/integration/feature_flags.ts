2dd628ab5ff0353990f8f8527bf8619aebe09fac
Chris Hutchinson <chris_hutchinson@me.com>
2021-06-17 13:20:58 +0100

    Rename tests

--- cypress/integration/feature_flags.ts
+++ cypress/integration/feature_flags.ts
@@ -1,12 +1,12 @@
 import { AuthRoles } from '../support/commands';
 
 describe('feature flags', () => {
-  it('shows a happy message in the shared footer when the demo feature flag is enabled', () => {
+  it('Shows a happy message in the shared footer when the demo feature flag is active', () => {
     cy.visitAs('/?footerFlagActive=true', AuthRoles.AdultsGroup);
     cy.get('footer').contains('The Demo Feature Flag is active');
   });
 
-  it('Does not show a happy message in the shared footer when the demo feature flag is NOT enabled', () => {
+  it('Does not show a happy message in the shared footer when the demo feature flag is NOT active', () => {
     cy.visitAs('/', AuthRoles.AdultsGroup);
     cy.get('footer')
       .contains('The Demo Feature Flag is active')
