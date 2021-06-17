32a3f84458b90affd133969a27d4e296d7c5b2c5
Chris Hutchinson <chris_hutchinson@me.com>
2021-06-17 13:19:02 +0100

    Remove login from urls

--- cypress/integration/feature_flags.ts
+++ cypress/integration/feature_flags.ts
@@ -2,12 +2,12 @@ import { AuthRoles } from '../support/commands';
 
 describe('feature flags', () => {
   it('shows a happy message in the shared footer when the demo feature flag is enabled', () => {
-    cy.visitAs('/login?footerFlagActive=true', AuthRoles.AdultsGroup);
+    cy.visitAs('/?footerFlagActive=true', AuthRoles.AdultsGroup);
     cy.get('footer').contains('The Demo Feature Flag is active');
   });
 
   it('Does not show a happy message in the shared footer when the demo feature flag is NOT enabled', () => {
-    cy.visitAs('/login', AuthRoles.AdultsGroup);
+    cy.visitAs('/', AuthRoles.AdultsGroup);
     cy.get('footer')
       .contains('The Demo Feature Flag is active')
       .should('not.exist');
