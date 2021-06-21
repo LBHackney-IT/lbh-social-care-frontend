3d9d98dabc0b6577bf2798b7976da2d52b673c65
Chris Hutchinson <chris_hutchinson@me.com>
2021-06-21 14:41:26 +0100

    Add empty FeatureFlagContext around Layout to fix failing test

--- components/Layout/index.spec.tsx
+++ components/Layout/index.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import { FeatureFlagContext } from '../../lib/feature-flags/feature-flags';
 
 import Layout from './index';
 
@@ -14,9 +15,11 @@ jest.mock('./PhaseBanner/PhaseBanner', () => () => 'MockedPhaseBanner');
 describe('Layout component', () => {
   it('should render properly', async () => {
     const { asFragment } = render(
-      <Layout>
-        <p>I am the children</p>
-      </Layout>
+      <FeatureFlagContext features={{}}>
+        <Layout>
+          <p>I am the children</p>
+        </Layout>
+      </FeatureFlagContext>
     );
     expect(asFragment()).toMatchSnapshot();
   });
