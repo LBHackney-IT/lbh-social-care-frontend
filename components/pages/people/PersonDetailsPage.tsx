864c42c3799f208accc733c4a753d7c274763b6a
Chris Hutchinson <chris_hutchinson@me.com>
2021-06-22 16:03:39 +0100

    fix(SCT-659): show warning note summaries to correct users when viewing resident profiles

---
+++ components/pages/people/PersonDetailsPage.tsx
@@ -0,0 +1,42 @@
+import PersonView from 'components/PersonView/PersonView';
+import PersonDetails from 'components/PersonView/PersonDetails';
+import Cases from 'components/Cases/Cases';
+import AllocatedWorkers from 'components/AllocatedWorkers/AllocatedWorkers';
+import Relationships from 'components/Relationships/Relationships';
+import WarningNotes from 'components/WarningNote/WarningNotes';
+import Stack from 'components/Stack/Stack';
+import {
+  canManageCases,
+  canUserViewWarningNotes,
+  canViewRelationships,
+} from 'lib/permissions';
+import { useAuth } from '../../UserContext/UserContext';
+import { User } from '../../../types';
+
+export const PersonDetailsPage: React.FC<{ personId: number }> = ({
+  personId,
+}) => {
+  const { user } = useAuth() as { user: User };
+
+  return (
+    <PersonView personId={personId} showPersonDetails={false}>
+      {(person) => (
+        <Stack space={7} className="govuk-!-margin-top-7">
+          {canManageCases(user, person) ? (
+            <WarningNotes id={personId} />
+          ) : (
+            <></>
+          )}
+          <PersonDetails person={person} />
+          <AllocatedWorkers person={person} />
+          {canViewRelationships(user, person) ? (
+            <Relationships id={personId} />
+          ) : (
+            <></>
+          )}
+          <Cases id={personId} person={person} />
+        </Stack>
+      )}
+    </PersonView>
+  );
+};
