8c81457d44d980db0ba2123c48c60051927e4e74
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-22 13:49:57 +0100

    put checkboxes back the way they were

--- components/FlexibleForms/RadioField.tsx
+++ components/FlexibleForms/RadioField.tsx
@@ -5,7 +5,6 @@ import {
   FormikErrors,
   FormikTouched,
   FormikValues,
-  useFormikContext,
 } from 'formik';
 import cx from 'classnames';
 
@@ -38,88 +37,70 @@ const Field = ({
   className,
   choices,
   onChange,
-  required,
-  condition,
-}: FieldProps): React.ReactElement => {
-  const { values }: { values: FormikValues } = useFormikContext();
-
-  const validate = (val: string): string | void => {
-    if (
-      condition &&
-      values[condition.id] === condition.value &&
-      required &&
-      !val
-    ) {
-      return 'This question is required';
-    }
-  };
-
-  return (
-    <div
-      className={`govuk-form-group lbh-form-group ${
-        getIn(touched, name) && getIn(errors, name) && 'govuk-form-group--error'
-      }`}
+}: FieldProps): React.ReactElement => (
+  <div
+    className={`govuk-form-group lbh-form-group ${
+      getIn(touched, name) && getIn(errors, name) && 'govuk-form-group--error'
+    }`}
+  >
+    <fieldset
+      className="govuk-fieldset"
+      aria-describedby={hint && `${name}-hint`}
     >
-      <fieldset
-        className="govuk-fieldset"
-        aria-describedby={hint && `${name}-hint`}
-      >
-        <legend className="govuk-label lbh-label">{label}</legend>
+      <legend className="govuk-label lbh-label">{label}</legend>
 
-        {hint && (
-          <span id={`${name}-hint`} className="govuk-hint lbh-hint">
-            {hint}
-          </span>
+      {hint && (
+        <span id={`${name}-hint`} className="govuk-hint lbh-hint">
+          {hint}
+        </span>
+      )}
+
+      <ErrorMessage name={name}>
+        {(msg) => (
+          <p className="govuk-error-message lbh-error-message" role="alert">
+            <span className="govuk-visually-hidden">Error:</span>
+            {msg}
+          </p>
         )}
+      </ErrorMessage>
 
-        <ErrorMessage name={name}>
-          {(msg) => (
-            <p className="govuk-error-message lbh-error-message" role="alert">
-              <span className="govuk-visually-hidden">Error:</span>
-              {msg}
-            </p>
-          )}
-        </ErrorMessage>
-
-        <div
-          className={cx(
-            `govuk-radios lbh-radios govuk-!-margin-top-3`,
-            className
-          )}
-        >
-          {choices.map((choice) => (
-            <div className="govuk-radios__item" key={choice.value}>
-              {onChange ? (
-                <input
-                  type="radio"
-                  name={name}
-                  value={choice.value}
-                  id={`${name}-${choice.value}`}
-                  className="govuk-radios__input"
-                  onChange={onChange}
-                />
-              ) : (
-                <RawField
-                  type="radio"
-                  name={name}
-                  value={choice.value}
-                  id={`${name}-${choice.value}`}
-                  className="govuk-radios__input"
-                  validate={validate}
-                />
-              )}
-              <label
-                className="govuk-label govuk-radios__label"
-                htmlFor={`${name}-${choice.value}`}
-              >
-                {choice.label}
-              </label>
-            </div>
-          ))}
-        </div>
-      </fieldset>
-    </div>
-  );
-};
+      <div
+        className={cx(
+          `govuk-radios lbh-radios govuk-!-margin-top-3`,
+          className
+        )}
+      >
+        {choices.map((choice) => (
+          <div className="govuk-radios__item" key={choice.value}>
+            {onChange ? (
+              <input
+                type="radio"
+                name={name}
+                value={choice.value}
+                id={`${name}-${choice.value}`}
+                className="govuk-radios__input"
+                onChange={onChange}
+              />
+            ) : (
+              <RawField
+                type="radio"
+                name={name}
+                value={choice.value}
+                id={`${name}-${choice.value}`}
+                className="govuk-radios__input"
+              />
+            )}
+            <label
+              className="govuk-label govuk-radios__label"
+              htmlFor={`${name}-${choice.value}`}
+            >
+              {choice.label}
+            </label>
+          </div>
+        ))}
+      </div>
+    </fieldset>
+  </div>
+);
 
 export default Field;
