512f89ff1336b8685edafbae1da15e8fea489c36
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-23 13:45:59 +0100

    remove some messages so that the old and new unfinished submissions look okay next to each other

--- components/SaveFormData/SaveFormData.tsx
+++ components/SaveFormData/SaveFormData.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { getData, deleteData, SavedData } from 'utils/saveData';
 import SavedDataTable from './SavedDataTable';
 
-export const SavedForms = (): React.ReactElement => {
+export const SavedForms = (): React.ReactElement | null => {
   const [savedForms, setSavedForms] = useState<Record<string, SavedData>>();
   useEffect(() => {
     setSavedForms(getData());
@@ -12,22 +12,16 @@ export const SavedForms = (): React.ReactElement => {
     deleteData(path);
     setSavedForms(getData());
   };
-  if (!savedForms || Object.keys(savedForms)?.length === 0) {
-    return (
-      <p className="govuk-body">You have no incomplete forms right now.</p>
-    );
-  }
+
+  if (!savedForms || Object.keys(savedForms)?.length === 0) return null;
+
   const detailHeader = ['Person ID', 'Client Name', 'Date of birth'];
   const standardHeader = ['Form type', 'Last saved', 'Actions', ''];
   const sortData = Object.values(savedForms);
-  const formQty = sortData.length;
   const detailData = sortData.filter((item) => Boolean(item.personDetails));
   const standardData = sortData.filter((item) => !item.personDetails);
   return (
     <>
-      <p>
-        Displaying ({formQty}) unfinished {formQty > 1 ? 'forms' : 'form'}
-      </p>
       {standardData.length > 0 && (
         <SavedDataTable
           tableHeader={standardHeader}
