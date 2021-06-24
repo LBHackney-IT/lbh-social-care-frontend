e3be5c071dee2a466cbe2c86a91eb3b166a0b93e
Jaye Hackett <jaye.hackett@gmail.com>
2021-06-24 13:38:52 +0100

    fix types

--- components/SubmissionsTable/SubmissionRow.tsx
+++ components/SubmissionsTable/SubmissionRow.tsx
@@ -17,14 +17,12 @@ const SubmissionRow = ({
   openRow,
   setOpenRow,
   user,
-}: Props): React.ReactElement => {
+}: Props): React.ReactElement | null => {
   const open = openRow === submission.submissionId;
 
   const form = forms.find((form) => form.id === submission.formId);
-  
-  if(!form) {
-    return null;
-  }
+
+  if (!form) return null;
 
   const completedSteps = Object.keys(submission.formAnswers).length;
   const totalSteps = form?.steps?.length;
