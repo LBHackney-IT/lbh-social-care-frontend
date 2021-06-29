9346d3110837776f33ba5736b50f128dbf9833a0
Chris Hutchinson <chris_hutchinson@me.com>
2021-06-29 13:48:44 +0100

    Fix linting issue

--- lib/feature-flags/feature-flags.tsx
+++ lib/feature-flags/feature-flags.tsx
@@ -7,8 +7,9 @@ export type FeatureSet = {
   };
 };
 
-const FeatureFlagContext =
-  React.createContext<FeatureSet | undefined>(undefined);
+const FeatureFlagContext = React.createContext<FeatureSet | undefined>(
+  undefined
+);
 
 export const FeatureFlagProvider: React.FC<{
   features: FeatureSet;
