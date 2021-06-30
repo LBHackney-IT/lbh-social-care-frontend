5d1e2aa6b91ed6de79e22333254f29f8afe64e3c
Jaye <joshua.hackett93@gmail.com>
2021-06-30 08:49:54 +0100

    add missing case note fields

--- data/flexibleForms/childCaseNote.ts
+++ data/flexibleForms/childCaseNote.ts
@@ -11,9 +11,110 @@ const form: Form = {
       theme: 'foo',
       fields: [
         {
+          id: 'type',
+          question: 'What kind of note is this?',
+          type: 'radios',
+          required: true,
+          choices: [
+            {
+              value: 'visit',
+              label: 'Visit',
+            },
+            {
+              value: 'correspondence',
+              label: 'Correspondence',
+            },
+            {
+              value: 'something-else',
+              label: 'Something else',
+            },
+          ],
+        },
+        {
+          id: 'correspondence-type',
+          question: 'What kind of correspondence?',
+          type: 'radios',
+          required: true,
+          condition: {
+            id: 'type',
+            value: 'correspondence',
+          },
+          choices: [
+            {
+              value: 'phone-call',
+              label: 'Phone call',
+            },
+            {
+              value: 'email-letter-text-message',
+              label: 'Email, letter or text message',
+            },
+          ],
+        },
+        {
+          id: 'visit-type',
+          question: 'What kind of visit?',
+          type: 'radios',
+          required: true,
+          condition: {
+            id: 'type',
+            value: 'visit',
+          },
+          choices: [
+            {
+              value: 'home',
+              label: 'Home visit',
+            },
+            {
+              value: 'office',
+              label: 'Office visit',
+            },
+            {
+              value: 'no-reply',
+              label: 'No reply to home visit',
+            },
+          ],
+        },
+
+        {
           id: 'body',
           question: 'What happened?',
           type: 'textarea',
+        },
+
+        {
+          id: 'actions',
+          question: 'Actions',
+          type: 'repeaterGroup',
+          hint: 'eg. Dave to contact landlord',
+          itemName: 'action',
+          subfields: [
+            {
+              id: 'text',
+              question: 'What needs to be done?',
+              type: 'text',
+            },
+            {
+              id: 'assignee',
+              question: 'Assigned to',
+              type: 'text',
+              className: 'govuk-input--width-10',
+            },
+            {
+              id: 'due',
+              question: 'Due',
+              type: 'text',
+              className: 'govuk-input--width-10',
+            },
+          ],
+        },
+
+        {
+          id: 'topics',
+          question: 'Topics',
+          hint: 'Help colleagues find this note. Add all that apply.',
+          // type: 'tags',
+          type: 'text',
+          required: true,
         },
       ],
     },
