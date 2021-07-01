572cdda6f477fa270244d8998cb18f0352fab7e9
Chris Hutchinson <chris_hutchinson@me.com>
2021-07-01 17:14:17 +0100

    feat(SCT-738): add a useFeatureFlag() hook for accessing feature flags

--- lib/feature-flags/feature-flags.spec.tsx
+++ lib/feature-flags/feature-flags.spec.tsx
@@ -5,75 +5,128 @@
  *   <div>The feature is active!</div>
  * </ConditionalFeature>
  */
-
+import { renderHook } from '@testing-library/react-hooks';
 import { render, screen } from '@testing-library/react';
 
 import {
   ConditionalFeature,
   FeatureFlagProvider,
   FeatureSet,
+  useFeatureFlags,
 } from './feature-flags';
 
-describe('<ConditionalFeature />', () => {
-  it('should render nothing if an unknown feature name is provided', () => {
-    const features: FeatureSet = {};
+describe('feature flags', () => {
+  describe('<ConditionalFeature />', () => {
+    it('should render nothing if an unknown feature name is provided', () => {
+      const features: FeatureSet = {};
 
-    render(
-      <FeatureFlagProvider features={features}>
-        <ConditionalFeature name="some-unknown-feature-name">
-          <div data-testid="expectedElement">This should not be visible!</div>
-        </ConditionalFeature>
-      </FeatureFlagProvider>
-    );
+      render(
+        <FeatureFlagProvider features={features}>
+          <ConditionalFeature name="some-unknown-feature-name">
+            <div data-testid="expectedElement">This should not be visible!</div>
+          </ConditionalFeature>
+        </FeatureFlagProvider>
+      );
 
-    const children = screen.queryByTestId('expectedElement');
+      const children = screen.queryByTestId('expectedElement');
 
-    expect(children).toBeNull();
+      expect(children).toBeNull();
+    });
+
+    it('should render nothing if a known feature name is provided and that feature is inactive', () => {
+      const features: FeatureSet = {
+        'some-known-inactive-feature-name': {
+          isActive: false,
+        },
+      };
+
+      render(
+        <FeatureFlagProvider features={features}>
+          <ConditionalFeature name="some-known-inactive-feature-name">
+            <div data-testid="expectedElement">This should be visible!</div>
+          </ConditionalFeature>
+        </FeatureFlagProvider>
+      );
+
+      const children = screen.queryByTestId('expectedElement');
+
+      expect(children).toBeNull();
+    });
+
+    it('should render the children if a known feature name is provided and that feature is active', () => {
+      const features: FeatureSet = {
+        'some-known-active-feature-name': {
+          isActive: true,
+        },
+      };
+
+      render(
+        <FeatureFlagProvider features={features}>
+          <ConditionalFeature name="some-known-active-feature-name">
+            <div data-testid="expectedElement">This should be visible!</div>
+          </ConditionalFeature>
+        </FeatureFlagProvider>
+      );
+
+      screen.getByTestId('expectedElement');
+    });
+
+    it('should throw an exception if feature flag context is not provided', () => {
+      expect(() => {
+        render(<ConditionalFeature name="some-known-active-feature-name" />);
+      }).toThrow(
+        'A <FeatureFlagProvider /> must be provided as a parent of this component'
+      );
+    });
   });
 
-  it('should render nothing if a known feature name is provided and that feature is inactive', () => {
-    const features: FeatureSet = {
-      'some-known-inactive-feature-name': {
-        isActive: false,
-      },
+  describe('useFeatureFlags() hook', () => {
+    const getContextWrapper = (features: FeatureSet) => {
+      const ContextWrapper: React.FC = ({ children }) => (
+        <FeatureFlagProvider features={features}>
+          {children}
+        </FeatureFlagProvider>
+      );
+
+      return ContextWrapper;
     };
 
-    render(
-      <FeatureFlagProvider features={features}>
-        <ConditionalFeature name="some-known-inactive-feature-name">
-          <div data-testid="expectedElement">This should be visible!</div>
-        </ConditionalFeature>
-      </FeatureFlagProvider>
-    );
+    it('should return a method to check if a named hook is active and return true if it is', () => {
+      const { result } = renderHook(() => useFeatureFlags(), {
+        wrapper: getContextWrapper({
+          'test-feature': {
+            isActive: true,
+          },
+        }),
+      });
 
-    const children = screen.queryByTestId('expectedElement');
+      expect(result.current.isFeatureActive('test-feature')).toBe(true);
+    });
 
-    expect(children).toBeNull();
-  });
+    it("should return a method to check if a named hook is active and return false if it isn't", () => {
+      const { result } = renderHook(() => useFeatureFlags(), {
+        wrapper: getContextWrapper({
+          'test-feature': {
+            isActive: false,
+          },
+        }),
+      });
 
-  it('should render the children if a known feature name is provided and that feature is active', () => {
-    const features: FeatureSet = {
-      'some-known-active-feature-name': {
-        isActive: true,
-      },
-    };
+      expect(result.current.isFeatureActive('test-feature')).toBe(false);
+    });
 
-    render(
-      <FeatureFlagProvider features={features}>
-        <ConditionalFeature name="some-known-active-feature-name">
-          <div data-testid="expectedElement">This should be visible!</div>
-        </ConditionalFeature>
-      </FeatureFlagProvider>
-    );
+    it('should return a method to check if a named hook is active and return false if it is not defined', () => {
+      const { result } = renderHook(() => useFeatureFlags(), {
+        wrapper: getContextWrapper({
+          'test-feature': {
+            isActive: true,
+          },
+        }),
+      });
 
-    screen.getByTestId('expectedElement');
-  });
-
-  it('should throw an exception if feature flag context is not provided', () => {
-    expect(() => {
-      render(<ConditionalFeature name="some-known-active-feature-name" />);
-    }).toThrow(
-      'A <FeatureFlagProvider /> must be provided as a parent of this component'
-    );
+      expect(result.current.isFeatureActive('non-existant-feature')).toBe(
+        false
+      );
+    });
   });
 });
