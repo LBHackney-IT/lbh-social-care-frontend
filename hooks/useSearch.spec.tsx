0423cb05c9fe9ddd97f87b4787e88202fca707fb
Jaye <joshua.hackett93@gmail.com>
2021-07-06 09:50:49 +0100

    add test

--- hooks/useSearch.spec.tsx
+++ hooks/useSearch.spec.tsx
@@ -12,26 +12,29 @@ const documents = [
   },
 ];
 
-const MockComponent = ({ query }: { query: string }): React.ReactElement => {
-  const results = useSearch(query, documents, ['id', 'name']);
+const MockComponent = ({
+  query,
+  minChar,
+}: {
+  query: string;
+  minChar?: number;
+}): React.ReactElement => {
+  const results = useSearch(query, documents, ['id', 'name'], minChar);
   return <>{JSON.stringify(results)}</>;
 };
 
 describe('useSearch', () => {
   it('returns matching results', () => {
     render(<MockComponent query={'foo'} />);
-    expect(
-      screen.getByText(
-        JSON.stringify({
-          id: 'foo',
-          name: 'namey mcname',
-        }),
-        { exact: false }
-      )
-    );
+    expect(screen.getByText(JSON.stringify(documents[0]), { exact: false }));
   });
 
-  it('returns matching results', () => {
+  it('supports a custom character count', () => {
+    render(<MockComponent query={'foo'} minChar={500} />);
+    expect(screen.getByText(JSON.stringify(documents), { exact: false }));
+  });
+
+  it("does nothing if minimum character count isn't met", () => {
     render(<MockComponent query={'f'} />);
     expect(screen.getByText(JSON.stringify(documents), { exact: false }));
   });
