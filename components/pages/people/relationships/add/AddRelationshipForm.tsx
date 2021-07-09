db2629a301c4c220e9be77e1805513ef97e89c36
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2021-07-09 09:38:51 +0100

    refactor(SCT-471): update add relationship form to use checkboxes (#532)
    * Update summary to handle options as a function for checkbox
    
    * Update addRelationships to transform form data for API request
    
    This enables us to add test coverage for it instead of doing it
    in the onFormSubmit function in the AddRelationshipForm component.
    
    * Refactor add relationship form to use checkboxes
    
    For the additional options. Also, sort the relationship types
    by alphabetical order and add test coverage.
    
    * Update summary to handle different types for checkboxes

--- components/pages/people/relationships/add/AddRelationshipForm.tsx
+++ components/pages/people/relationships/add/AddRelationshipForm.tsx
@@ -1,34 +1,21 @@
-import { useRouter } from 'next/router';
-
 import Seo from 'components/Layout/Seo/Seo';
 import PersonView from 'components/PersonView/PersonView';
 import FormWizard from 'components/FormWizard/FormWizard';
 import formSteps from 'data/forms/add-relationship';
 import { addRelationships } from 'utils/api/relationships';
+import PersonLinkConfirmation from 'components/Steps/PersonLinkConfirmation';
 
-const AddRelationshipForm = (): React.ReactElement => {
-  const { query } = useRouter();
-
-  const personId = query.id as string;
-  const secondPersonId = query.secondPersonId as string;
-
+const AddRelationshipForm: React.FC<{
+  personId: number;
+  secondPersonId: number;
+}> = ({ personId, secondPersonId }) => {
   interface FormData {
     type: string;
-    ofUnbornChild?: string;
-    isMainCarer?: string;
+    additionalOptions?: string[];
     [key: string]: unknown;
   }
 
   const onFormSubmit = async (formData: FormData) => {
-    if (formData.type === 'Parent' && formData.ofUnbornChild == 'Yes') {
-      formData.type = 'parentOfUnbornChild';
-    }
-    if (formData.type === 'Sibling' && formData.ofUnbornChild == 'Yes') {
-      formData.type = 'siblingOfUnbornChild';
-    }
-
-    delete formData.ofUnbornChild;
-
     const { data, error } = await addRelationships({
       ...formData,
       personId: Number(personId),
@@ -43,14 +30,17 @@ const AddRelationshipForm = (): React.ReactElement => {
   return (
     <>
       <Seo title={`Person Details - #${personId}`} />
-      <PersonView personId={Number(personId as string)} expandView>
-        <FormWizard
-          formPath={`/people/${personId}/relationships/add/${secondPersonId}/`}
-          formSteps={formSteps}
-          title="Add relationship"
-          onFormSubmit={onFormSubmit}
-          successMessage="Relationship has been added"
-        />
+      <PersonView personId={Number(personId)} expandView>
+        {() => (
+          <FormWizard
+            formPath={`/people/${personId}/relationships/add/${secondPersonId}/`}
+            formSteps={formSteps}
+            title="Add new relationship"
+            onFormSubmit={onFormSubmit}
+            successMessage="Relationship has been added"
+            customConfirmation={PersonLinkConfirmation}
+          />
+        )}
       </PersonView>
     </>
   );
--- pages/people/[id]/relationships/add/[secondPersonId]/[[...stepId]].tsx
+++ pages/people/[id]/relationships/add/[secondPersonId]/[[...stepId]].tsx
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router';
+
 import AddRelationshipForm from 'components/pages/people/relationships/add/AddRelationshipForm';
 
 const AddRelationshipPage: React.FC = () => {
-  return <AddRelationshipForm />;
+  const { query } = useRouter();
+  const personId = Number(query.id as string);
+  const secondPersonId = Number(query.secondPersonId);
+
+  return (
+    <AddRelationshipForm personId={personId} secondPersonId={secondPersonId} />
+  );
 };
 
 export default AddRelationshipPage;
