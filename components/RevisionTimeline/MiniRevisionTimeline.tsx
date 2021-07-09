d3b729cd86635c8b509a805d27a1ad7bf22cc108
Jaye Hackett <jaye.hackett@gmail.com>
2021-07-09 12:52:20 +0100

    add mini timeline to dashboard

--- components/RevisionTimeline/MiniRevisionTimeline.tsx
+++ components/RevisionTimeline/MiniRevisionTimeline.tsx
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import { Submission } from 'data/flexibleForms/forms.types';
+import s from './MiniRevisionTimeline.module.scss';
 
 interface Props {
   submission: Submission;
@@ -10,13 +11,13 @@ const RevisionTimeline = ({ submission }: Props): React.ReactElement | null => {
   const revisions = Array.from(submission.editHistory).reverse().slice(0, 3);
 
   return (
-    <ol className="lbh-timeline">
+    <ol className={`lbh-timeline ${s.timeline}`}>
       {revisions.map((revision, i) => (
         <li
-          className={`lbh-timeline__event lbh-timeline__event--minor`}
+          className={`lbh-timeline__event lbh-timeline__event--minor ${s.event}`}
           key={i}
         >
-          <h3 className="lbh-body-s">Edited by {revision.worker.email}</h3>
+          <h3 className="lbh-body-xs">Edited by {revision.worker.email}</h3>
 
           <p className="lbh-body-xs">
             {format(new Date(revision.editTime), 'dd MMM yyyy K.mm aaa')}
---
+++ components/RevisionTimeline/MiniSubmissionTimeline.spec.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from '@testing-library/react';
+import { mockSubmission } from 'factories/submissions';
+import { mockedWorker } from 'factories/workers';
+import MiniRevisionTimeline from './MiniRevisionTimeline';
+
+describe('MiniRevisionTimeline', () => {
+  it('correctly renders edits and creation', () => {
+    render(
+      <MiniRevisionTimeline
+        submission={{
+          ...mockSubmission,
+          editHistory: [
+            {
+              worker: mockedWorker,
+              editTime: '2021-06-23T12:00:00.000Z',
+            },
+            {
+              worker: mockedWorker,
+              editTime: '2021-07-28T11:00:00.000Z',
+            },
+          ],
+        }}
+      />
+    );
+
+    expect(
+      screen.queryAllByText('Edited by foo.bar@hackney.gov.uk').length
+    ).toBe(2);
+    expect(screen.getByText('28 Jul 2021', { exact: false }));
+    expect(screen.getByText('23 Jun 2021', { exact: false }));
+  });
+
+  it('only shows up to three events', () => {
+    render(
+      <MiniRevisionTimeline
+        submission={{
+          ...mockSubmission,
+          editHistory: [
+            {
+              worker: mockedWorker,
+              editTime: '2021-06-23T12:00:00.000Z',
+            },
+            {
+              worker: mockedWorker,
+              editTime: '2021-07-28T11:00:00.000Z',
+            },
+            {
+              worker: mockedWorker,
+              editTime: '2021-07-28T11:00:00.000Z',
+            },
+            {
+              worker: mockedWorker,
+              editTime: '2021-07-28T11:00:00.000Z',
+            },
+          ],
+        }}
+      />
+    );
+
+    expect(screen.getAllByRole('listitem').length).toBe(3);
+  });
+});
