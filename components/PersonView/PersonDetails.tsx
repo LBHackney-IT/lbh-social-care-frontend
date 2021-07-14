e3246f5f90fdb3aa72b5e4785d6a5a0f7347f17a
Jaye Hackett <jaye.hackett@gmail.com>
2021-07-14 17:01:38 +0100

    chore: update person details (#568)
    * update person details
    
    * remove snapshots
    
    * e2e fixes
    
    * only show health section if there's something to show
    
    * don't include person details on the form wizard
    
    * use person widget rather than person details
    
    Co-authored-by: Jaye <joshua.hackett93@gmail.com>
    Co-authored-by: John Farrell <johnfarrell@fastmail.com>

--- components/PersonView/PersonDetails.tsx
+++ components/PersonView/PersonDetails.tsx
@@ -1,24 +1,19 @@
-import { useState } from 'react';
 import { getEthnicityName } from 'utils/person';
 import { Resident, User } from 'types';
 import { useAuth } from 'components/UserContext/UserContext';
 import { canUserEditPerson } from 'lib/permissions';
 import Link from 'next/link';
+import s from './PersonDetails.module.scss';
 
 interface Props {
   person: Resident;
   expandView?: boolean;
 }
 
-const PersonDetails = ({
-  person,
-  expandView = false,
-}: Props): React.ReactElement => {
+const PersonDetails = ({ person }: Props): React.ReactElement => {
   const {
     otherNames,
     contextFlag,
-    firstName,
-    lastName,
     id,
     dateOfBirth,
     nhsNumber,
@@ -34,162 +29,152 @@ const PersonDetails = ({
     preferredMethodOfContact,
   } = person;
 
-  const [expandDetails, setExpandDetails] = useState(false);
   const { user } = useAuth() as { user: User };
 
   return (
-    <div>
-      <div className="lbh-table-header">
-        <h2 className="govuk-fieldset__legend--m govuk-custom-text-color">
-          {expandView ? `${firstName} ${lastName}` : 'PERSON DETAILS'}
-        </h2>
-        {expandView ? (
-          <button
-            className="govuk-link"
-            onClick={(e) => {
-              e.preventDefault();
-              setExpandDetails(!expandDetails);
-            }}
-          >
-            {expandDetails ? 'Hide' : 'Show'} details
-          </button>
-        ) : (
-          canUserEditPerson(user, person) && (
+    <>
+      <section className="govuk-!-margin-bottom-8">
+        <div className={s.heading}>
+          <h2>Personal details</h2>
+          {canUserEditPerson(user, person) && (
             <Link href={`/people/${id}/edit`}>
-              <a className="lbh-link">Edit details</a>
+              <a className="lbh-link lbh-link--no-visited-state">
+                Edit details
+              </a>
             </Link>
-          )
-        )}
-      </div>
-      <hr className="govuk-divider" />
-      {(!expandView || expandDetails) && (
-        <>
-          <dl className="govuk-summary-list">
-            {otherNames && otherNames?.length > 0 && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Other Names</dt>
-                <dd className="govuk-summary-list__value">
-                  <ul className="govuk-list">
-                    {otherNames.map(({ firstName, lastName }) => (
-                      <li key={`${firstName} ${lastName}`}>
-                        {firstName} {lastName}
-                      </li>
-                    ))}
-                  </ul>
-                </dd>
-              </div>
-            )}
+          )}
+        </div>
+
+        <dl className="govuk-summary-list lbh-summary-list">
+          {otherNames && otherNames?.length > 0 && (
             <div className="govuk-summary-list__row">
-              <dt className="govuk-summary-list__key">Person ID</dt>
-              <dd className="govuk-summary-list__value">#{id}</dd>
+              <dt className="govuk-summary-list__key">Other names</dt>
+              <dd className="govuk-summary-list__value">
+                <ul className="govuk-list">
+                  {otherNames.map(({ firstName, lastName }) => (
+                    <li key={`${firstName} ${lastName}`}>
+                      {firstName} {lastName}
+                    </li>
+                  ))}
+                </ul>
+              </dd>
             </div>
-            {gender && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Gender</dt>
-                <dd className="govuk-summary-list__value">{gender}</dd>
-              </div>
-            )}
-            {dateOfBirth && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Date of birth</dt>
-                <dd className="govuk-summary-list__value">
-                  {new Date(dateOfBirth).toLocaleDateString('en-GB')}
-                </dd>
-              </div>
-            )}
-            {dateOfDeath && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Date Of Death</dt>
-                <dd className="govuk-summary-list__value">
-                  {new Date(dateOfDeath).toLocaleDateString('en-GB')}
-                </dd>
-              </div>
-            )}
-            {ethnicity && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Ethnicity</dt>
-                <dd className="govuk-summary-list__value">
-                  {getEthnicityName(ethnicity)}
-                </dd>
-              </div>
-            )}
-            {firstLanguage && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">First Language </dt>
-                <dd className="govuk-summary-list__value">{firstLanguage}</dd>
-              </div>
-            )}
-            {religion && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Religion </dt>
-                <dd className="govuk-summary-list__value">{religion}</dd>
-              </div>
-            )}
-            {sexualOrientation && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Sexual orientation</dt>
-                <dd className="govuk-summary-list__value">
-                  {sexualOrientation}
-                </dd>
-              </div>
-            )}
-            {nhsNumber && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">NHS number</dt>
-                <dd className="govuk-summary-list__value">{nhsNumber}</dd>
-              </div>
-            )}
-            {address && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Address</dt>
-                <dd className="govuk-summary-list__value">
-                  <div>{address.address}</div>
-                  <div>{address.postcode}</div>
-                </dd>
-              </div>
-            )}
-            {phoneNumbers.length > 0 && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Phone Number</dt>
-                <dd className="govuk-summary-list__value">
-                  <ul className="govuk-list">
-                    {phoneNumbers.map(({ number, type }) => (
-                      <li key={number}>
-                        {number} - {type}
-                      </li>
-                    ))}
-                  </ul>
-                </dd>
-              </div>
-            )}
-            {emailAddress && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Email</dt>
-                <dd className="govuk-summary-list__value">{emailAddress}</dd>
-              </div>
-            )}
-            {preferredMethodOfContact && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">
-                  Preferred Method Of Contact
-                </dt>
-                <dd className="govuk-summary-list__value">
-                  {preferredMethodOfContact}
-                </dd>
-              </div>
-            )}
-            {contextFlag && (
-              <div className="govuk-summary-list__row">
-                <dt className="govuk-summary-list__key">Person type</dt>
-                <dd className="govuk-summary-list__value">
-                  {contextFlag === 'C' ? 'CFS' : 'ASC'}
-                </dd>
-              </div>
-            )}
+          )}
+          <div className="govuk-summary-list__row">
+            <dt className="govuk-summary-list__key">Social care ID</dt>
+            <dd className="govuk-summary-list__value">#{id}</dd>
+          </div>
+          {gender && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Gender</dt>
+              <dd className="govuk-summary-list__value">{gender}</dd>
+            </div>
+          )}
+          {dateOfBirth && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Date of birth</dt>
+              <dd className="govuk-summary-list__value">
+                {new Date(dateOfBirth).toLocaleDateString('en-GB')}
+              </dd>
+            </div>
+          )}
+          {dateOfDeath && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Date of death</dt>
+              <dd className="govuk-summary-list__value">
+                {new Date(dateOfDeath).toLocaleDateString('en-GB')}
+              </dd>
+            </div>
+          )}
+          {ethnicity && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Ethnicity</dt>
+              <dd className="govuk-summary-list__value">
+                {getEthnicityName(ethnicity)}
+              </dd>
+            </div>
+          )}
+          {firstLanguage && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">First language </dt>
+              <dd className="govuk-summary-list__value">{firstLanguage}</dd>
+            </div>
+          )}
+          {religion && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Religion </dt>
+              <dd className="govuk-summary-list__value">{religion}</dd>
+            </div>
+          )}
+          {sexualOrientation && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Sexual orientation</dt>
+              <dd className="govuk-summary-list__value">{sexualOrientation}</dd>
+            </div>
+          )}
+          {address && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Address</dt>
+              <dd className="govuk-summary-list__value">
+                {address.address}
+                <br />
+                {address.postcode}
+              </dd>
+            </div>
+          )}
+          {phoneNumbers.length > 0 && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Phone number</dt>
+              <dd className="govuk-summary-list__value">
+                <ul className="lbh-list">
+                  {phoneNumbers.map(({ number, type }) => (
+                    <li key={number}>
+                      {number} - {type}
+                    </li>
+                  ))}
+                </ul>
+              </dd>
+            </div>
+          )}
+          {emailAddress && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Email</dt>
+              <dd className="govuk-summary-list__value">{emailAddress}</dd>
+            </div>
+          )}
+          {preferredMethodOfContact && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">
+                Preferred method of contact
+              </dt>
+              <dd className="govuk-summary-list__value">
+                {preferredMethodOfContact}
+              </dd>
+            </div>
+          )}
+          {contextFlag && (
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">Service area</dt>
+              <dd className="govuk-summary-list__value">
+                {contextFlag === 'C' ? 'CFS' : 'ASC'}
+              </dd>
+            </div>
+          )}
+        </dl>
+      </section>
+
+      {nhsNumber && (
+        <section>
+          <h2 className={s.heading}>Medical and health</h2>
+          <dl className="govuk-summary-list lbh-summary-list">
+            <div className="govuk-summary-list__row">
+              <dt className="govuk-summary-list__key">NHS number</dt>
+              <dd className="govuk-summary-list__value">{nhsNumber}</dd>
+            </div>
           </dl>
-        </>
+        </section>
       )}
-    </div>
+    </>
   );
 };
 
