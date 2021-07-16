6ec0f8175a09cad69ad8b3d43f1df86b15559d1b
Jaye <joshua.hackett93@gmail.com>
2021-07-16 14:09:50 +0100

    improve error handling on step id api route

--- pages/api/submissions/[id]/steps/[stepId].tsx
+++ pages/api/submissions/[id]/steps/[stepId].tsx
@@ -2,43 +2,49 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import forms from 'data/flexibleForms';
 import { isAuthorised } from 'utils/auth';
 import { getSubmissionById, patchSubmissionForStep } from 'lib/submissions';
-import statusCodes from 'http-status-codes';
+import statusCodes, { StatusCodes } from 'http-status-codes';
+import { Submission } from 'data/flexibleForms/forms.types';
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  const { id, stepId } = req.query;
+  try {
+    const { id, stepId } = req.query;
 
-  const submission = await getSubmissionById(String(id));
-  const form = forms.find((form) => form.id === submission.formId);
-  const step = form?.steps.find((step) => step.id === stepId);
+    let submission: Submission;
+    submission = await getSubmissionById(String(id));
+    const form = forms.find((form) => form.id === submission.formId);
+    const step = form?.steps.find((step) => step.id === stepId);
 
-  if (!step)
-    return res.status(statusCodes.NOT_FOUND).json({
-      error: 'Step not found',
-    });
+    if (!step)
+      return res.status(statusCodes.NOT_FOUND).json({
+        error: 'Step not found',
+      });
 
-  if (req.method === 'PATCH') {
-    const values = req.body;
-    const user = isAuthorised(req);
+    if (req.method === 'PATCH') {
+      const values = req.body;
+      const user = isAuthorised(req);
 
-    patchSubmissionForStep(
-      String(id),
-      String(stepId),
-      String(user?.email),
-      values
-    );
+      submission = await patchSubmissionForStep(
+        String(id),
+        String(stepId),
+        String(user?.email),
+        values
+      );
 
-    return res.json(submission);
-  } else {
-    return res.json({
-      ...submission,
-      // include the answers for this step only
-      stepAnswers: submission.formAnswers[stepId.toString()],
-      form,
-      step,
-    });
+      return res.json(submission);
+    } else {
+      return res.json({
+        ...submission,
+        // include the answers for this step only
+        stepAnswers: submission.formAnswers[stepId.toString()],
+        form,
+        step,
+      });
+    }
+  } catch (e) {
+    res.status(e.response.status).json(e.toJSON());
   }
 };
 
