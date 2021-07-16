831145619d1c477451dc693979ee8aa59abd413a
Jaye Hackett <jaye.hackett@gmail.com>
2021-07-16 11:23:26 +0100

    feat(sct-855): overhaul dashboard nav (#574)
    * wip
    
    * redesign my data
    
    * remove duplicate nav link
    
    * refactor tabs

--- components/Dashboard/DashboardWrapper.tsx
+++ components/Dashboard/DashboardWrapper.tsx
@@ -1,35 +1,71 @@
-import Stack from 'components/Stack/Stack';
-import Tabs from 'components/Tabs/Tabs';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import s from './DashboardWrapper.module.scss';
 import MyData from './MyData';
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactChild;
+}
+
+const NavLink = ({ href, children }: NavLinkProps) => {
+  const router = useRouter();
+  return (
+    <li>
+      <Link href={href}>
+        <a
+          className={`lbh-link lbh-link--no-visited-state ${
+            router.asPath === href ? s.navLinkActive : s.navLink
+          }`}
+        >
+          {children}
+        </a>
+      </Link>
+    </li>
+  );
+};
+
+const navigation: { text: string; href: string }[] = [
+  {
+    text: `Allocations`,
+    href: `/`,
+  },
+  {
+    text: `Submissions`,
+    href: `/forms-in-progress`,
+  },
+  // { text: 'Records', href: '/my-records' },
+];
 
 interface Props {
   children: React.ReactChild;
 }
 
 const DashboardWrapper = ({ children }: Props): React.ReactElement => (
-  <Stack space={7}>
-    <h1 className="lbh-heading-l">My work space</h1>
-    <MyData />
-    <Tabs
-      title="Dashboard"
-      tabs={[
-        {
-          url: '/',
-          text: 'Clients allocated',
-        },
-        {
-          url: '/forms-in-progress',
-          text: 'Unfinished submissions',
-        },
-        {
-          url: '/my-records',
-          text: "Records I've added",
-        },
-      ]}
-    >
-      {children}
-    </Tabs>
-  </Stack>
+  <>
+    <Head>
+      <title>Dashboard | Social care | Hackney Council</title>
+    </Head>
+
+    <div className={`govuk-grid-row ${s.outer}`}>
+      <div className="govuk-grid-column-one-quarter">
+        <div className={s.sticky}>
+          <nav>
+            <ul className="lbh-list">
+              {navigation.map((link) => (
+                <NavLink href={link.href} key={link.href}>
+                  {link.text}
+                </NavLink>
+              ))}
+            </ul>
+          </nav>
+          <MyData />
+        </div>
+      </div>
+      <div className="govuk-grid-column-three-quarters">{children}</div>
+    </div>
+  </>
 );
 
 export default DashboardWrapper;
--- components/Dashboard/MyData.spec.tsx
+++ components/Dashboard/MyData.spec.tsx
@@ -1,30 +1,13 @@
-import { fireEvent, render } from '@testing-library/react';
-
+import { render } from '@testing-library/react';
 import { mockedUser } from 'factories/users';
 import * as meAPI from 'utils/api/me';
 import MyData from './MyData';
 import { mockedAllocations } from 'factories/allocatedWorkers';
 import { mockedWorker } from 'factories/workers';
-import {
-  errorFactory,
-  mockedAPIerror,
-  mockedAPIservererror,
-} from 'factories/APIerrors';
-
-const mockedUseRouter = {
-  query: { foo: 'bar' },
-  replace: jest.fn(),
-  pathname: 'foopath',
-};
-
-jest.mock('next/router', () => ({
-  useRouter: () => mockedUseRouter,
-}));
-
-jest.mock('components/Spinner/Spinner', () => () => 'MockedSpinner');
+import { mockedAPIerror, mockedAPIservererror } from 'factories/APIerrors';
 
 describe(`MyData`, () => {
-  it('should render an error if the API returns a 500', async () => {
+  it('should render nothing if the API returns a 500', async () => {
     jest.spyOn(meAPI, 'useMyData').mockImplementation(() => ({
       data: undefined,
       error: mockedAPIservererror,
@@ -32,8 +15,7 @@ describe(`MyData`, () => {
       mutate: jest.fn(),
       isValidating: false,
     }));
-    const { asFragment, getByText } = render(<MyData />);
-    getByText('An unknown error has occurred.');
+    const { asFragment } = render(<MyData />);
     expect(asFragment()).toMatchSnapshot();
   });
 
@@ -46,10 +28,10 @@ describe(`MyData`, () => {
       mutate: jest.fn(),
       isValidating: false,
     }));
-    const { asFragment, queryByText } = render(<MyData />);
-    expect(queryByText('User not found in the system')).toBeNull();
+    const { asFragment } = render(<MyData />);
     expect(asFragment()).toMatchSnapshot();
   });
+
   it("should render a worker's data only if a worker is available", async () => {
     jest.spyOn(meAPI, 'useMyData').mockImplementation(() => ({
       data: {
@@ -61,17 +43,7 @@ describe(`MyData`, () => {
       mutate: jest.fn(),
       isValidating: false,
     }));
-    const { asFragment, getByRole } = render(<MyData />);
+    const { asFragment } = render(<MyData />);
     expect(asFragment()).toMatchSnapshot();
-    fireEvent.click(getByRole('button'));
-    expect(asFragment()).toMatchSnapshot();
-    expect(mockedUseRouter.replace).toHaveBeenCalledWith(
-      'foopath?details=true',
-      'foopath?details=true',
-      {
-        scroll: false,
-        shallow: true,
-      }
-    );
   });
 });
