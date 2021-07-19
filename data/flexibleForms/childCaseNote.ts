10a738b5b7a8fbe492089b3636f49a2dbe313905
Jaye <joshua.hackett93@gmail.com>
2021-07-19 15:11:52 +0100

    add missing tag

--- data/flexibleForms/childCaseNote.ts
+++ data/flexibleForms/childCaseNote.ts
@@ -1,5 +1,4 @@
 import { Form } from './forms.types';
-import tags from 'data/childCaseNoteTags';
 import { format } from 'date-fns';
 
 const form: Form = {
@@ -138,7 +137,53 @@ const form: Form = {
             id: 'Type',
             value: 'Something else',
           },
-          choices: tags,
+          choices: [
+            { label: 'Unit meeting note', value: 'Unit meeting note' },
+            { label: 'Allocation record', value: 'Allocation record' },
+            { label: 'Case audit', value: 'Case audit' },
+            { label: 'Clinical input', value: 'Clinical input' },
+            {
+              label: 'Consultation with service manager',
+              value: 'Consultation with service manager',
+            },
+            {
+              label: 'Consultation with head of service',
+              value: 'Consultation with head of service',
+            },
+            {
+              label: 'Consultation with independent reviewing officer',
+              value: 'Consultation with independent reviewing officer',
+            },
+            {
+              label: 'Independent reviewing officer oversight',
+              value: 'Independent reviewing officer oversight',
+            },
+            { label: 'Management oversight', value: 'Management oversight' },
+            { label: 'MARAC', value: 'MARAC' },
+            { label: 'MAPPA', value: 'MAPPA' },
+            { label: 'PMU', value: 'PMU' },
+            {
+              label: 'Extra-familial risk panel',
+              value: 'Extra-familial risk panel',
+            },
+            {
+              label: 'Escalation with independent chair',
+              value: 'Escalation with independent chair',
+            },
+            {
+              label: 'Escalation with service manager',
+              value: 'Escalation with service manager',
+            },
+            { label: 'Record of meeting', value: 'Record of meeting' },
+            {
+              label: 'Safe and together consultation',
+              value: 'Safe and together consultation',
+            },
+            {
+              label: 'Out of hours',
+              value: 'Out of hours',
+            },
+          ],
         },
 
         {
