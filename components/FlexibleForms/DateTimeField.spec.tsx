b65b7d8bc7068ee46ab09b77a204c29ecc57eeef
Jaye Hackett <jaye.hackett@gmail.com>
2021-07-21 15:13:40 +0100

    cleanup

--- components/FlexibleForms/DateTimeField.spec.tsx
+++ components/FlexibleForms/DateTimeField.spec.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react';
 
 const mockSubmit = jest.fn();
 
-describe('TextField', () => {
+describe('DateTimeField', () => {
   it('renders two text inputs with the right types', () => {
     render(
       <Formik
