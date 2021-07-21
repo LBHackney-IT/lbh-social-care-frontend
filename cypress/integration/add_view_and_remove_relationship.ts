680a7b49ddcb241eb59e4133d6cca626544d5cce
Wen Ting Wang <wenting@madetech.com>
2021-07-21 09:28:18 +0100

    feat(SCT-481): add Cypress test for relationships
    This goes through the flow of adding a relationship, viewing it and
    then removing it.
    
    In the test, we had to intercept the request for creating a relationship
    because the cookie used by Cypress is associated with a worker that does
    not exist. A worker with `e2e.tests.adult@hackney.gov.uk` has been added
    so that a valid email address for `createdBy` could be used.

---
+++ cypress/integration/add_view_and_remove_relationship.ts
@@ -0,0 +1,70 @@
+import { AuthRoles } from '../support/commands';
+
+describe('Adding, viewing and remove a relationship', () => {
+  beforeEach(() => {
+    // This is required as the email address stored in the cookie is not an
+    // existing worker.
+    cy.intercept('POST', '/api/relationships', (req) => {
+      req.body.createdBy = 'e2e.tests.adult@hackney.gov.uk';
+    });
+  });
+
+  it('allows a user to add, view and remove a relationship', () => {
+    cy.visitAs(
+      `/people/${Cypress.env('ADULT_RECORD_PERSON_ID')}/relationships`,
+      AuthRoles.AdultsGroup
+    );
+
+    cy.contains('Add a new relationship').click();
+
+    // Search for related person
+    cy.get('input[name=first_name]').type(
+      Cypress.env('CHILDREN_RECORD_FIRST_NAME')
+    );
+
+    cy.contains('button', 'Search').click();
+
+    cy.get(`input[value=${Cypress.env('CHILDREN_RECORD_PERSON_ID')}]`).check();
+    cy.contains('Choose relationship').click();
+
+    // Define the relationship to related person
+    cy.get('select').select('Child');
+    cy.get('input[name=details]').type('Some context of relationship');
+
+    cy.contains('button', 'Continue').click();
+
+    // Review and confirm relationship
+    cy.contains('Child').should('be.visible');
+    cy.contains('Some context of relationship').should('be.visible');
+
+    cy.contains('button', 'Submit').click();
+
+    cy.contains('Relationship has been added').should('be.visible');
+
+    // View added relationship of person
+    cy.contains('button', 'View person').click();
+
+    cy.contains('a', 'Relationships (1)').click();
+
+    cy.contains('Children').should('be.visible');
+    cy.contains(Cypress.env('CHILDREN_RECORD_FIRST_NAME')).should('be.visible');
+    cy.contains(Cypress.env('CHILDREN_RECORD_LAST_NAME')).should('be.visible');
+    cy.contains('Some context of relationship').should('be.visible');
+
+    // Remove added relationship of person
+    cy.contains(
+      'a',
+      `Remove relationship with ${Cypress.env(
+        'CHILDREN_RECORD_FIRST_NAME'
+      )} ${Cypress.env('CHILDREN_RECORD_LAST_NAME')}`
+    ).click();
+
+    cy.contains('Are you sure you want to remove this relationship?').should(
+      'be.visible'
+    );
+
+    cy.contains('button', 'Yes, remove').click();
+
+    cy.contains('No relationships found for this person').should('be.visible');
+  });
+});
