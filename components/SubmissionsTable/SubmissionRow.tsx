04973357ecc3fce2bb3bb19a1f8462ad4ffbc614
Jaye Hackett <jaye.hackett@gmail.com>
2021-07-22 17:37:14 +0100

    visual tweaks

--- components/SubmissionsTable/SubmissionRow.tsx
+++ components/SubmissionsTable/SubmissionRow.tsx
@@ -26,6 +26,7 @@ const SubmissionRow = ({
 
   const completedSteps = Object.keys(submission.formAnswers).length;
   const totalSteps = form?.steps?.length;
+  const completion = Math.round((completedSteps / Number(totalSteps)) * 100);
 
   const lastEdited =
     submission.editHistory[submission.editHistory.length - 1]?.editTime;
@@ -38,108 +39,44 @@ const SubmissionRow = ({
     <>
       <li className={s.row} aria-expanded={open}>
         <div className={s.person}>
-          <h3 className={s.name}>
+          <h3 className={s.name}>{form?.name || 'Unknown form'}</h3>
+          <p className={`lbh-body-xs govuk-!-margin-top-1 ${s.meta}`}>
+            For{' '}
             <Link href={`/people/${submission.residents[0].id}`}>
-              <a className="lbh-link lbh-link--no-visited-state">
+              <a className="lbh-link lbh-link--muted ">
                 {submission.residents[0].firstName}{' '}
                 {submission.residents[0].lastName}
               </a>
-            </Link>
-          </h3>
-          <p className={`lbh-body-xs govuk-!-margin-top-1`}>
-            #{submission.residents[0].id}
+            </Link>{' '}
+            ·{' '}
+            <button
+              className="lbh-link lbh-link--muted"
+              onClick={() => setOpenRow(submission.submissionId)}
+            >
+              Details
+            </button>
           </p>
         </div>
 
-        <dl className={`lbh-body-s ${s.meta}`}>
-          <dt>Form</dt>{' '}
-          <dd>
-            <Link href={`/submissions/${submission.submissionId}`}>
-              <a className="lbh-link">{form?.name || submission.formId}</a>
-            </Link>
-          </dd>
-          <br />
-          <dt>Last edited</dt>{' '}
-          <dd>{format(new Date(submission.createdAt), 'dd MMM yyyy')}</dd>
+        <dl className={`lbh-body-s ${s.stats}`}>
+          <div>
+            <dd>{completion}%</dd>
+            <dt>completed</dt>
+          </div>
+          <div>
+            <dd>{format(new Date(submission.createdAt), 'd MMM yyyy')}</dd>
+            <dt>last edited</dt>
+          </div>
         </dl>
 
-        <button
-          className={s.expanderButton}
-          onClick={() => setOpenRow(open ? false : submission.submissionId)}
-        >
-          <svg width="15" height="15" viewBox="0 0 13 13" fill="none">
-            <rect y="5.41675" width="13" height="2.16667" fill="#0B0C0C" />
-            {!open && (
-              <rect
-                x="5.41663"
-                y="13"
-                width="13"
-                height="2.16667"
-                transform="rotate(-90 5.41663 13)"
-                fill="#0B0C0C"
-              />
-            )}
-          </svg>
+        <Link href={`/submissions/${submission.submissionId}`}>
+          <a className="govuk-button lbh-button">Resume</a>
+        </Link>
 
-          <span className="govuk-visually-hidden">
-            {open ? 'Hide details' : 'Expand details'}
-          </span>
-        </button>
-      </li>
-
-      {open && (
-        <div className={`${s.detailsPanel}`}>
-          <dl>
-            <div>
-              <dt className="lbh-body-s">Last edited</dt>
-              <dd>{format(new Date(lastEdited), 'dd MMM yyyy K.mm aaa')}</dd>
-            </div>
-
-            <div>
-              <dt className="lbh-body-s">Started</dt>
-              <dd>
-                {format(new Date(submission.createdAt), 'dd MMM yyyy K.mm aaa')}
-              </dd>
-            </div>
-
-            <div>
-              <dt className="lbh-body-s">Progress</dt>
-              <dd>
-                {completedSteps === totalSteps ? (
-                  'Ready to finish'
-                ) : (
-                  <>
-                    {completedSteps || '0'} of {totalSteps} sections (
-                    {Math.round((completedSteps / Number(totalSteps)) * 100)}
-                    %)
-                  </>
-                )}
-              </dd>
-            </div>
-
-            {editors?.length > 0 && (
-              <div>
-                <dt className="lbh-body-s">Edited by</dt>
-                <dd>
-                  <ul className="lbh-list govuk-!-margin-top-0">
-                    {editors.map((editor) => (
-                      <li key={editor}>{editor}</li>
-                    ))}
-                  </ul>
-                </dd>
-              </div>
-            )}
-
-            <div className={s.timeline}>
-              <dt className="lbh-body-s">Recent revisions</dt>
-              <dd>
-                <MiniRevisionTimeline submission={submission} />
-              </dd>
-            </div>
-          </dl>
-          <DiscardDialog submissionId={submission.submissionId} />
+        <div className={s.meter}>
+          <div style={{ width: `${completion}%` }}></div>
         </div>
-      )}
+      </li>
     </>
   );
 };
