5c10e7e5d0b696492819e814d40b0a1828431f72
ana <anabebic.x@gmail.com>
2021-08-11 13:01:41 +0100

    add tests

---
+++ lib/casestatus.spec.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import * as caseStatusAPI from './casestatus';
+import {
+  mockedCaseStatusFactory,
+  mockedPersonCaseStatusFactory,
+} from 'factories/caseStatus';
+import { mockedResident } from 'factories/residents';
+
+const ENDPOINT_API = process.env.ENDPOINT_API;
+const AWS_KEY = process.env.AWS_KEY;
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock('axios');
+
+describe('case status APIs', () => {
+  describe('GetCaseStatusByResident', () => {
+    it("calls the service API's case status endpoint", async () => {
+      const caseStatus = mockedCaseStatusFactory.build();
+      mockedAxios.get.mockResolvedValue({
+        data: caseStatus,
+      });
+
+      const data = await caseStatusAPI.getCaseStatusByPersonId(123);
+
+      expect(mockedAxios.get).toHaveBeenCalled();
+      expect(mockedAxios.get.mock.calls[0][0]).toEqual(
+        `https://virtserver.swaggerhub.com/Hackney/social-care-case-viewer-api/1.0.0/residents/123/casestatuses`
+        // `${ENDPOINT_API}/residents/123/casestatuses`
+      );
+      expect(mockedAxios.get.mock.calls[0][1]?.headers).toEqual({
+        'x-api-key': AWS_KEY,
+      });
+      expect(data).toEqual(caseStatus);
+    });
+  });
+});
