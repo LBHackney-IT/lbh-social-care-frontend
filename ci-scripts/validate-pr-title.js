869ba0d9a7180bca7a21361d8653cafd78ef16d6
Chris Hutchinson <1573022+chrishutchinson@users.noreply.github.com>
2021-08-16 17:40:27 +0100

    feat: adds CI script for validating a PR title conforms to conventional commits (#638)
    * feat: adds CI script for validating a PR title conforms to conventional commits
    
    * Swap requirement for job

---
+++ ci-scripts/validate-pr-title.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+const fetch = require('cross-fetch');
+
+const fetchPullRequestTitle = ({
+  pullRequestUrl,
+  projectUsername,
+  projectRepoName,
+  gitHubToken,
+}) => {
+  const prNumber = pullRequestUrl.split('/').reverse()[0];
+
+  return fetch(
+    `https://api.github.com/repos/${projectUsername}/${projectRepoName}/pulls/${prNumber}`,
+    {
+      headers: {
+        Authorization: `token ${gitHubToken}`,
+      },
+    }
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+
+      return res.json();
+    })
+    .then(({ title }) => title);
+};
+
+const validatePrTitle = async ({
+  pullRequestUrl,
+  projectUsername,
+  projectRepoName,
+  gitHubToken,
+}) => {
+  if (!pullRequestUrl || !projectUsername || !projectRepoName || !gitHubToken) {
+    throw new Error('Please provide the required config');
+  }
+
+  const title = await fetchPullRequestTitle({
+    pullRequestUrl,
+    projectUsername,
+    projectRepoName,
+    gitHubToken,
+  });
+
+  console.log(`Verifying PR title: "${title}"`);
+
+  const regex =
+    /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|test)\(?(.*?)\)?: (.*?)$/g;
+
+  const result = regex.exec(title);
+
+  if (!result) {
+    throw new Error(
+      `The PR type provided is not valid, must be one of ${[
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'test',
+      ].join(', ')}`
+    );
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  const [_, __, ___, description] = result;
+
+  if (!description) {
+    throw new Error('No description provided');
+  }
+};
+
+if (require.main === module) {
+  const {
+    CIRCLE_PROJECT_USERNAME,
+    CIRCLE_PROJECT_REPONAME,
+    GITHUB_TOKEN,
+    CIRCLE_PULL_REQUEST,
+  } = process.env;
+
+  validatePrTitle({
+    gitHubToken: GITHUB_TOKEN,
+    pullRequestUrl: CIRCLE_PULL_REQUEST,
+    projectRepoName: CIRCLE_PROJECT_REPONAME,
+    projectUsername: CIRCLE_PROJECT_USERNAME,
+  })
+    .then(() => {
+      console.log('Done!');
+      process.exit();
+    })
+    .catch((e) => {
+      console.error('Failed to parse PR title.', e.message);
+      process.exit(1);
+    });
+}
+
+module.exports = {
+  validatePrTitle,
+};
