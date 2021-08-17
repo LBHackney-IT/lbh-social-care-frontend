dcf959973bf0f9791cc12a9707dcd36a171d2cad
ana <anabebic.x@gmail.com>
2021-08-17 09:34:05 +0100

    renaming

--- lib/casestatus.spec.ts
+++ lib/casestatus.spec.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import * as caseStatusAPI from './casestatus';
+import * as caseStatusAPI from '.caseStatus';
 import { mockedCaseStatusFactory } from 'factories/caseStatus';
 
 const ENDPOINT_API = process.env.ENDPOINT_API;
