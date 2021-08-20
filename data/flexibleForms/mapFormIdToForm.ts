3634ddf0cc24d790703f7ef20eb360fc971fa25b
JohnFarrellDev <johnfarrell@fastmail.com>
2021-08-20 14:35:15 +0100

    update maps so contain all form details

--- data/flexibleForms/mapFormIdToForm.ts
+++ data/flexibleForms/mapFormIdToForm.ts
@@ -5,4 +5,9 @@ export const mapFormIdToForm: Record<string, Form> = {
   'adult-case-note': formAsObjects.adultCaseNote,
   'child-case-note': formAsObjects.childCaseNote,
   foo: formAsObjects.foo,
+  'review-3c': formAsObjects.review3c,
+  'FACE overview assessment': formAsObjects.faceOverview,
+  'safeguarding-adult-concern-form': formAsObjects.sgAdultConcern,
+  'Safeguarding adult manager decision on concern':
+    formAsObjects.sgAdultManagerDecisionConcern,
 };
--- data/flexibleForms/mapFormIdsToDisplayName.ts
+++ data/flexibleForms/mapFormIdsToDisplayName.ts
@@ -1,4 +1,10 @@
 export const mapIdToType: Record<string, string> = {
   'adult-case-note': 'Case note',
   'child-case-note': 'Case note',
+  foo: 'Sandbox form',
+  'review-3c': 'Review of Care and Support Plan (3C)',
+  'FACE overview assessment': 'FACE overview assessment',
+  'safeguarding-adult-concern-form': 'Safeguarding Adult Concern Form',
+  'Safeguarding adult manager decision on concern':
+    'Safeguarding adult manager decision on concern',
 };
