db4512cf4ac27ff680990f569017e1b1ad984d31
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2021-08-20 15:44:31 +0100

    handled error and isValidating

--- components/RevisionTimeline/MiniRevisionTimeline.tsx
+++ components/RevisionTimeline/MiniRevisionTimeline.tsx
@@ -1,3 +1,4 @@
+import Spinner from 'components/Spinner/Spinner';
 import { InProgressSubmission } from 'data/flexibleForms/forms.types';
 import { format } from 'date-fns';
 import { useSubmission } from 'utils/api/submissions';
@@ -20,7 +21,12 @@ const RevisionTimeline = ({
   const revisions = Array.from(submission?.editHistory ?? [])
     .reverse()
     .slice(0, 3);
-
+  if (error) {
+    return <p>Error fetching this sumbission edit history</p>;
+  }
+  if (isValidating) {
+    return <Spinner />;
+  }
   return (
     <ol className={`lbh-timeline ${s.timeline}`}>
       {revisions.map((revision, i) => (
