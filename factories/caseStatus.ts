3928def0bf6eaa82d3c27f7453f0f5029ae18bc8
mapleleafjack <jack.musajo@madetech.com>
2021-08-23 14:58:12 +0100

    feat(SCT-1111): Case status response structure change

--- factories/caseStatus.ts
+++ factories/caseStatus.ts
@@ -1,5 +1,10 @@
 import { Factory } from 'fishery';
-import { CaseStatus, PersonCaseStatus } from 'types';
+import {
+  CaseStatus,
+  PersonCaseStatus,
+  FormOption,
+  CaseStatusFields,
+} from 'types';
 
 export const mockedPersonCaseStatusFactory = Factory.define<PersonCaseStatus>(
   ({ sequence }) => ({
@@ -12,9 +17,20 @@ export const mockedCaseStatusFactory = Factory.define<CaseStatus>(
   ({ sequence }) => ({
     id: sequence,
     type: 'foo',
-    subType: 'bar',
-    StartDate: '2021-1-1',
-    EndDate: '2021-1-1',
-    Notes: 'a lot to note',
+    fields: [mockedStatusField.build()],
+    startDate: '2021-01-01T02:00:00Z',
+    endDate: '2021-12-01T02:00:00Z',
+    notes: 'a lot to note',
   })
 );
+
+const mockedStatusField = Factory.define<CaseStatusFields>(() => ({
+  name: 'placementReason',
+  description: 'What is the primary reason for placement? (Primary need code)',
+  selectedOption: mockedFieldsFactory.build(),
+}));
+
+const mockedFieldsFactory = Factory.define<FormOption>(() => ({
+  name: 'N0',
+  description: 'Not stated',
+}));
