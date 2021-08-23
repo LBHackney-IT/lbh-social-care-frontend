73ae1d6d1193c27474917bb226d684810a14adcf
mapleleafjack <jack.musajo@madetech.com>
2021-08-23 11:58:55 +0100

    Removed swagger commented line

--- lib/caseStatus.ts
+++ lib/caseStatus.ts
@@ -7,10 +7,11 @@ const headers = { 'x-api-key': AWS_KEY };
 export const getCaseStatusByPersonId = async (
   personId: number
 ): Promise<PersonCaseStatus> => {
-  const { data }: { data: PersonCaseStatus; error: Error } =
-    // await axios.get(`https://virtserver.swaggerhub.com/Hackney/social-care-case-viewer-api/1.0.0/residents/123/casestatuses`, {
-    await axios.get(`${ENDPOINT_API}/residents/${personId}/casestatuses`, {
+  const { data }: { data: PersonCaseStatus; error: Error } = await axios.get(
+    `${ENDPOINT_API}/residents/${personId}/casestatuses`,
+    {
       headers,
-    });
+    }
+  );
   return data;
 };
