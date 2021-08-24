eedc3e8589f94911441d9a75d8d35de929673af7
mapleleafjack <jack.musajo@madetech.com>
2021-08-24 15:53:40 +0100

    Grouping types together

--- components/CaseStatus/CaseStatusView.tsx
+++ components/CaseStatus/CaseStatusView.tsx
@@ -21,22 +21,31 @@ const CaseStatusView = ({ person }: Props): React.ReactElement => {
 
   return (
     <>
-      {caseStatusData && (
-        <section className="govuk-!-margin-top-0">
-          {caseStatusData.caseStatuses.map((status) => (
-            <span
-              className="govuk-tag lbh-tag govuk-!-margin-right-1 govuk-!-margin-top-2"
-              key={status.id}
-            >
-              {getTypeString(status.type as keyof typeof valueMapping)} -{' '}
-              {status.fields.map((val) => val.selectedOption.name).join(',')}
-            </span>
-          ))}
-        </section>
+      {groupByType(caseStatusData.caseStatuses, (data: any) => data.type).map(
+        (status) => (
+          <span
+            className="govuk-tag lbh-tag govuk-!-margin-right-1 govuk-!-margin-top-2"
+            key={status}
+          >
+            {getTypeString(status)}
+          </span>
+        )
       )}
     </>
   );
 };
+
+function groupByType(list: any, keyGetter: any) {
+  const map = [];
+  list.forEach((item: any) => {
+    const key = keyGetter(item);
+    const collection = map.find((elm) => elm === key);
+    if (!collection) {
+      map.push(key);
+    }
+  });
+  return map;
+}
 
 const getTypeString = (type: keyof typeof valueMapping): any => {
   return valueMapping[type];
