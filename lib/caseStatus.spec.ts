0364e7f066d13049f8ad5329496980a9110ac205
mapleleafjack <jack.musajo@madetech.com>
2021-08-25 11:08:48 +0100

    Tidy up test

--- lib/caseStatus.spec.ts
+++ lib/caseStatus.spec.ts
@@ -21,13 +21,10 @@ describe('case status APIs', () => {
 
       const data = await caseStatusAPI.getCaseStatusByPersonId(123);
 
-      expect(mockedAxios.get).toHaveBeenCalled();
-      expect(mockedAxios.get.mock.calls[0][0]).toEqual(
-        `${ENDPOINT_API}/residents/123/casestatuses`
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        `${ENDPOINT_API}/residents/123/casestatuses`,
+        { headers: { 'x-api-key': AWS_KEY } }
       );
-      expect(mockedAxios.get.mock.calls[0][1]?.headers).toEqual({
-        'x-api-key': AWS_KEY,
-      });
       expect(data).toEqual(caseStatus);
     });
   });
@@ -44,14 +41,10 @@ describe('case form values APIs', () => {
 
       const data = await caseStatusAPI.GetFormValues('CIN');
 
-      expect(mockedAxios.get).toHaveBeenCalled();
-      expect(mockedAxios.get.mock.calls[0][0]).toEqual(
-        `${ENDPOINT_API}/case-status/form-options/CIN`
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        `${ENDPOINT_API}/case-status/form-options/CIN`,
+        { headers: { 'x-api-key': AWS_KEY } }
       );
-      expect(mockedAxios.get.mock.calls[0][1]?.headers).toEqual({
-        'x-api-key': AWS_KEY,
-      });
-
       expect(data).toEqual(mockedData);
     });
   });
