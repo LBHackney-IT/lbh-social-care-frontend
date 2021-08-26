e81e0525fe5af58bfcd02d5d97221f525337a169
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2021-08-26 10:07:39 +0100

    further styling and additional value mapping items

--- components/CaseStatus/CaseStatusDetails.tsx
+++ components/CaseStatus/CaseStatusDetails.tsx
@@ -22,13 +22,20 @@ const CaseStatusDetails = ({ person }: Props): React.ReactElement => {
   return (
     <>
       <div className={styles.container}>
+        
+        <h2>Statuses</h2>
+
         {caseStatusData.caseStatuses.map((status) => (
+         status.fields.map((item) => (
+          
           <div key={status.id} className={styles.caseStatusDesign}>
             <div className={styles.caseStatusDetails}>
+              
               <dl>
+                <div className={styles.align}>
                 {status.type && (
                   <>
-                    <dt className={styles.type}>Type</dt>
+                    <dt className={styles.typeFontSize}>Type</dt>
                     <dd>
                       {getTypeString(status.type as keyof typeof valueMapping)}{' '}
                       -{' '}
@@ -36,44 +43,47 @@ const CaseStatusDetails = ({ person }: Props): React.ReactElement => {
                   </>
                 )}
 
-                {status.subType && (
+                {status.startDate && (
                   <>
-                    <dt>Sub Type</dt>
-                    <dd>{status.subType}</dd>
+                    <dt className={styles.date}>Start</dt>
+                    <dd>
+                      {status.startDate
+                        ? new Date(status.startDate).toLocaleDateString('en-GB'): new Date(status.endDate).toLocaleDateString('en-GB')}{' '}-{' '}
+                    </dd>
                   </>
-                )}
+                )}  
 
                 {status.endDate && (
                   <>
-                    <dt>End</dt>
+                    <dt className={styles.date}>End</dt>
                     <dd>
                       {new Date(status.endDate).toLocaleDateString('en-GB')}{' '}
                     </dd>
                   </>
                 )}
+                </div>
 
-                {status.startDate && (
+                {item.selectedOption.name && (
                   <>
-                    <dt>Start</dt>
-                    <dd>
-                      {status.startDate
-                        ? new Date(status.startDate).toLocaleDateString('en-GB')
-                        : new Date(status.endDate).toLocaleDateString(
-                            'en-GB'
-                          )}{' '}
-                    </dd>
+                    <dt className={styles.selectedTitles}>Category of need</dt>
+                    <dd>{getTypeString(item.selectedOption.name as keyof typeof valueMapping)}</dd>
                   </>
                 )}
 
                 {status.notes && (
                   <>
-                    <dt className={styles.notes}>Notes</dt>
+                    <dt className={styles.selectedTitles}> Notes </dt>
                     <dd>{status.notes}</dd>
                   </>
                 )}
+
               </dl>
+              
             </div>
-          </div>
+
+        </div>
+           ))
+
         ))}
       </div>
     </>
@@ -85,6 +95,16 @@ const getTypeString = (type: keyof typeof valueMapping): any => {
 };
 const valueMapping = {
   CIN: 'Child in need',
+  N1: 'N1 - Abuse or neglect',
+  N2: 'N2- Child\'s diability',
+  N3: 'N3 - Parental disability or illness',
+  N4: 'N4 - Family in acute stress',
+  N5: 'N5 - Family dysfunction',
+  N6: 'N6 - Socially unacceptable behaviour',
+  N7: 'N7 - Low income',
+  N8: 'N8 - Absent Parenting',
+  N9: 'N9 - Cases other than children in need',
+  NO: 'N0 - Not stated'
 };
 
 export default CaseStatusDetails;
