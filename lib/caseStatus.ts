cdb197c4e6f6e6db891644ce9c9f04366fea85b1
mapleleafjack <jack.musajo@madetech.com>
2021-09-01 11:09:47 +0100

    Added API call

--- lib/caseStatus.ts
+++ lib/caseStatus.ts
@@ -3,11 +3,24 @@ import type { PersonCaseStatus } from 'types';
 const ENDPOINT_API = process.env.ENDPOINT_API;
 const AWS_KEY = process.env.AWS_KEY;
 const headers = { 'x-api-key': AWS_KEY };
+
 export const getCaseStatusByPersonId = async (
   personId: number
 ): Promise<PersonCaseStatus> => {
   const { data }: { data: PersonCaseStatus } = await axios.get(
     `${ENDPOINT_API}/residents/${personId}/casestatuses`,
+    {
+      headers,
+    }
+  );
+  return data;
+};
+
+export const GetFormValues = async (
+  type: string
+): Promise<PersonCaseStatus> => {
+  const { data }: { data: PersonCaseStatus } = await axios.get(
+    `${ENDPOINT_API}/case-status/form-options/${type}`,
     {
       headers,
     }
