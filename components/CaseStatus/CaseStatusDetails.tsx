ec2498ca38c60d95a2b3ff1777b1995a3a5343a5
mapleleafjack <jack.musajo@madetech.com>
2021-09-07 13:47:14 +0100

    Added support for case status without fields

--- components/CaseStatus/CaseStatusDetails.tsx
+++ components/CaseStatus/CaseStatusDetails.tsx
@@ -25,74 +25,76 @@ const CaseStatusDetails = ({ person }: Props): React.ReactElement => {
       <div className={styles.container}>
         <h2>Case statuses</h2>
 
-        {caseStatusData.caseStatuses.map((status) =>
-          status.fields.map((item) => (
-            <div key={status.id} className={styles.caseStatusDesign}>
-              <div className={styles.caseStatusDetails}>
-                <dl>
-                  <div className={styles.align}>
-                    {status.type && (
-                      <>
-                        <dt className="govuk-!-margin-right-2">
-                          <div className={styles.typeStyling}>
-                            {getTypeString(
-                              status.type as keyof typeof valueMapping
-                            )}{' '}
-                          </div>
-                        </dt>
-                      </>
-                    )}
-
-                    {status.startDate && (
-                      <>
-                        <dt className={styles.date}>Start</dt>
-                        <dd className={styles.date}>
-                          {new Date(status.startDate).toLocaleDateString(
-                            'en-GB',
-                            { day: '2-digit', month: 'short', year: 'numeric' }
-                          )}
-                        </dd>
-                      </>
-                    )}
-
-                    {status.endDate && (
-                      <>
-                        <dt className={styles.date}> - End</dt>
-                        <dd className={styles.date}>
-                          {new Date(status.endDate).toLocaleDateString(
-                            'en-GB',
-                            { day: '2-digit', month: 'short', year: 'numeric' }
+        {caseStatusData.caseStatuses.map((status) => (
+          <div key={status.id} className={styles.caseStatusDesign}>
+            <div className={styles.caseStatusDetails}>
+              <dl>
+                <div className={styles.align}>
+                  {status.type && (
+                    <>
+                      <dt className="govuk-!-margin-right-2">
+                        <div className={styles.typeStyling}>
+                          {getTypeString(
+                            status.type as keyof typeof valueMapping
                           )}{' '}
-                        </dd>
-                      </>
-                    )}
-                  </div>
+                        </div>
+                      </dt>
+                    </>
+                  )}
 
-                  {item.selectedOption &&
-                    item.selectedOption.name &&
-                    item.selectedOption.description && (
+                  {status.startDate && (
+                    <>
+                      <dt className={styles.date}>Start</dt>
+                      <dd className={styles.date}>
+                        {new Date(status.startDate).toLocaleDateString(
+                          'en-GB',
+                          { day: '2-digit', month: 'short', year: 'numeric' }
+                        )}
+                      </dd>
+                    </>
+                  )}
+
+                  {status.endDate && (
+                    <>
+                      <dt className={styles.date}> - End</dt>
+                      <dd className={styles.date}>
+                        {new Date(status.endDate).toLocaleDateString('en-GB', {
+                          day: '2-digit',
+                          month: 'short',
+                          year: 'numeric',
+                        })}{' '}
+                      </dd>
+                    </>
+                  )}
+                </div>
+
+                {status.fields.map(
+                  (field) =>
+                    field.selectedOption &&
+                    field.selectedOption.name &&
+                    field.selectedOption.description && (
                       <>
                         <dt className={styles.selectedTitles}>
                           Category of need
                         </dt>
                         <dd>
-                          {item.selectedOption.name} -{' '}
-                          {item.selectedOption.description}
+                          {field.selectedOption.name} -{' '}
+                          {field.selectedOption.description}
                         </dd>
                       </>
-                    )}
+                    )
+                )}
 
-                  {status.notes && (
-                    <>
-                      <dt className={styles.selectedTitles}> Notes </dt>
-                      <dd>{status.notes}</dd>
-                    </>
-                  )}
-                </dl>
-              </div>
+                {status.notes && (
+                  <>
+                    <dt className={styles.selectedTitles}> Notes </dt>
+                    <dd>{status.notes}</dd>
+                  </>
+                )}
+              </dl>
             </div>
-          ))
-        )}
+          </div>
+        ))}
       </div>
     </>
   );
