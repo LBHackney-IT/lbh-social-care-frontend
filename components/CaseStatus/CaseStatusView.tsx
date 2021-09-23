00d830a83115d26d1a0a4c496f519ce137ea29b5
JohnFarrellDev <johnfarrell@fastmail.com>
2021-09-23 12:16:33 +0100

    add start date to case status tip component

--- components/CaseStatus/CaseStatusView.tsx
+++ components/CaseStatus/CaseStatusView.tsx
@@ -1,7 +1,10 @@
 import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
 import { useCaseStatuses } from 'utils/api/caseStatus';
-import { Resident, CaseStatus, CaseStatusMapping } from 'types';
+import { Resident, CaseStatusMapping } from 'types';
 import Tip from 'components/Tip/Tip';
+import format from 'date-fns/format';
+
+const valueMapping = new CaseStatusMapping();
 
 interface Props {
   person: Resident;
@@ -22,14 +25,17 @@ const CaseStatusView = ({ person }: Props): React.ReactElement => {
 
   return (
     <>
-      {groupByType(caseStatusData.caseStatuses).map((status) => (
+      {caseStatusData.caseStatuses.map((caseStatus) => (
         <Tip
-          key={status}
+          key={caseStatus.id}
           interactive={true}
           content={
             <>
-              <p className="lbh-body-xs">{valueMapping[status]}</p>
-              <p className="lbh-body-xs">{}</p>
+              <p className="lbh-body-xs">{valueMapping[caseStatus.type]}</p>
+              <p className="lbh-body-xs">{`Start date: ${format(
+                new Date(caseStatus.startDate),
+                'dd MMM yyyy'
+              )}`}</p>
               <a
                 href={`http://dev.hackney.gov.uk:3000/people/${person.id}/details`}
               >
@@ -39,10 +45,10 @@ const CaseStatusView = ({ person }: Props): React.ReactElement => {
           }
         >
           <span
-            key={status}
+            key={caseStatus.id}
             className="govuk-tag lbh-tag lbh-tag--yellow govuk-!-margin-right-1 govuk-!-margin-top-2"
           >
-            {valueMapping[status]}
+            {valueMapping[caseStatus.type]}
           </span>
         </Tip>
       ))}
@@ -89,13 +95,5 @@ const CaseStatusView = ({ person }: Props): React.ReactElement => {
     </>
   );
 };
-
-const valueMapping = new CaseStatusMapping();
-
-function groupByType(
-  allCasesStatues: CaseStatus[]
-): (keyof typeof valueMapping)[] {
-  return Array.from(new Set(allCasesStatues.map((el) => el.type)));
-}
 
 export default CaseStatusView;
