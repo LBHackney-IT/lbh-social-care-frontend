d03496f4580633c6250c0eddb94a1b8067f746da
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-09-27 14:32:05 +0100

    fix: add a content security policy
    * compatibility with nextjs 'unsafe-inline' for styles and scripts
    * compatibility with google tag manager and GA
    * lock-down of usage in frames
    * allow loading fonts from google fonts cdn

--- next.config.js
+++ next.config.js
@@ -20,6 +20,19 @@ module.exports = {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self' hackney.gov.uk *.hackney.gov.uk;" +
+              "object-src 'none';" +
+              "connect-src 'self' www.google-analytics.com;" +
+              "script-src-elem 'self' 'unsafe-inline' www.googletagmanager.com;" +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com;" +
+              "style-src 'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com;" +
+              "font-src 'self' fonts.googleapis.com fonts.gstatic.com;" +
+              "frame-ancestors 'self';" +
+              "form-action 'self';",
+          },
         ],
       },
     ];
