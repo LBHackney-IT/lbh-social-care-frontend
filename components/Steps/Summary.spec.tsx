90db660387f66fa5ea918a80ac97272ec9ce5971
Jaye <joshua.hackett93@gmail.com>
2021-09-28 16:33:04 +0100

    summary step accepts redirect param

--- components/Steps/Summary.spec.tsx
+++ components/Steps/Summary.spec.tsx
@@ -1,7 +1,18 @@
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import Summary from './Summary';
-
 import { FormStep } from 'components/Form/types';
+import 'next/router';
+
+const mockReplace = jest.fn();
+
+const query: { redirectUrl?: string } = {};
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    query,
+    replace: mockReplace,
+  }),
+}));
 
 describe('Summary component', () => {
   const props = {
@@ -25,6 +36,7 @@ describe('Summary component', () => {
     onFormSubmit: jest.fn(),
     formPath: 'foo/bar-foo',
   };
+
   it('should render properly', async () => {
     const { asFragment, getByRole, getByText } = render(<Summary {...props} />);
     const submitBtn = getByRole('button', { name: 'Submit' });
@@ -32,5 +44,25 @@ describe('Summary component', () => {
     expect(getByText('Foo')).toBeInTheDocument();
     fireEvent.click(submitBtn);
     await waitFor(() => expect(props.onFormSubmit).toHaveBeenCalled());
+  });
+
+  it("should go to confirmation page if redirect url isn't given", async () => {
+    render(<Summary {...props} />);
+    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    await waitFor(() =>
+      expect(mockReplace).toBeCalledWith('foo/bar-fooconfirmation')
+    );
+  });
+
+  it('should redirect to the redirect url if one is given', async () => {
+    query.redirectUrl = 'http://example.com';
+
+    render(<Summary {...props} />);
+    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    await waitFor(() =>
+      expect(mockReplace).toBeCalledWith('http://example.com')
+    );
   });
 });
