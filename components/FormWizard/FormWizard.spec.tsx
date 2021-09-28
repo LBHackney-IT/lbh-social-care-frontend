9f9fc6187661f24c9a5a398e9ec20967d7587a6d
Jaye <joshua.hackett93@gmail.com>
2021-09-28 16:33:30 +0100

    form wizard passes redirect url along

--- components/FormWizard/FormWizard.spec.tsx
+++ components/FormWizard/FormWizard.spec.tsx
@@ -1,6 +1,18 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-
+import Router from 'next/router';
 import FormWizard from './FormWizard';
+import { useBeforeunload } from 'react-beforeunload';
+
+const query: {
+  stepId: string[];
+  redirectUrl?: string;
+} = {
+  stepId: ['1'],
+};
+
+jest.mock('react-beforeunload', () => ({
+  useBeforeunload: jest.fn(),
+}));
 
 jest.mock('next/router', () => ({
   __esModule: true,
@@ -11,9 +23,7 @@ jest.mock('next/router', () => ({
     push: jest.fn(),
   },
   useRouter: () => ({
-    query: {
-      stepId: ['1'],
-    },
+    query,
     asPath: 'path',
     push: jest.fn(),
   }),
@@ -44,10 +54,10 @@ const formSteps = [
   },
 ];
 
+const handleProgressStep = jest.fn();
+
 describe('<FormWizard />', () => {
   it("should call the 'onStepProgress' callback prop with the current form data when the step is progressed", async () => {
-    const handleProgressStep = jest.fn();
-
     render(
       <FormWizard
         formPath="/form/path"
@@ -69,5 +79,62 @@ describe('<FormWizard />', () => {
     expect(handleProgressStep).toHaveBeenCalledWith({
       'title-one': 'This is my value',
     });
+  });
+
+  it('passes the redirect parameter to the next step, if it exists', async () => {
+    query.redirectUrl = 'foooooo';
+
+    render(
+      <FormWizard
+        formPath="/form/path"
+        title="Form title"
+        onProgressStep={handleProgressStep}
+        formSteps={formSteps}
+      />
+    );
+
+    fireEvent.click(screen.getByRole('button', { name: 'Continue' }));
+
+    await waitFor(() =>
+      expect(Router.push).toBeCalledWith({
+        pathname: '/form/pathstep-two',
+        query: { redirectUrl: 'foooooo' },
+      })
+    );
+  });
+
+  it('copes if there is no redirect url', async () => {
+    query.redirectUrl = undefined;
+
+    render(
+      <FormWizard
+        formPath="/form/path"
+        title="Form title"
+        onProgressStep={handleProgressStep}
+        formSteps={formSteps}
+      />
+    );
+
+    fireEvent.click(screen.getByRole('button', { name: 'Continue' }));
+
+    await waitFor(() =>
+      expect(Router.push).toBeCalledWith({
+        pathname: '/form/pathstep-two',
+        query: {},
+      })
+    );
+  });
+
+  it('uses the beforeunload hook', () => {
+    render(
+      <FormWizard
+        formPath="/form/path"
+        title="Form title"
+        onProgressStep={handleProgressStep}
+        formSteps={formSteps}
+      />
+    );
+
+    expect(useBeforeunload).toBeCalledTimes(1);
   });
 });
