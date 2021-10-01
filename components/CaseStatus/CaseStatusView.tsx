6cb464a82af4593900fb3ca0b8fcec4489aa7798
Jerome <64031314+1JW1@users.noreply.github.com>
2021-10-01 15:52:02 +0100

    feat(SCT-1195): add panel on flag hover (#701)
    * Amending values from pascal to camelase
    
    * Added CaseStatusDetails component
    
    * created styling file to be amended at a later date
    
    * case status component stylesheet
    
    * case status component creation
    
    * brought stying from scss through to component
    
    * updated scss design to reflect recent changes
    
    * rearranged data items to match figma
    
    * put casenote module at bottom for possible removal
    
    * bold applied to type and notes
    
    * additional style matching to figma
    
    * further styling and additional value mapping items
    
    * aligned type and dates onto one line
    
    * updated title and added collapsable functionality
    
    * changed background colour to match new figma mock
    
    * removed lbh component and cleaned code for 'items'
    
    * adding margin between elements
    
    * margin amendments for caseStatusDetails class
    
    * Added tests + tidied up
    
    * Adjusted style
    
    * Added support for case status without fields
    
    * Ordered tests and added keys
    
    * Explicit types
    
    * added feature flag
    
    * ExpandDetails now supports react elements as label
    
    * Example of a collapsible element
    
    * ExpandDetails now can follow the new design
    
    * Example
    
    * Applied to case status
    
    * More improvements
    
    * additional styling to match figma
    
    * Fixing types and tests
    
    * align design to figma
    
    * updated tests
    
    * align changes to main
    
    * created component for pop up
    
    * added tip pop up component
    
    * attempting to figure out startDate pull through
    
    * add start date to case status tip component
    
    * position type and date together
    
    * attempt to align items horizontally
    
    * moved the flex container div outside of map
    
    * allign case status items and match styling closer to designs
    
    * remove inline styling
    
    * fixed hover issues
    
    * removed test no longer needed
    
    * removed .orig files
    
    Co-authored-by: Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
    Co-authored-by: mapleleafjack <jack.musajo@madetech.com>
    Co-authored-by: JohnFarrellDev <johnfarrell@fastmail.com>

--- components/CaseStatus/CaseStatusView.tsx
+++ components/CaseStatus/CaseStatusView.tsx
@@ -1,6 +1,9 @@
 import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
 import { useCaseStatuses } from 'utils/api/caseStatus';
-import { Resident, CaseStatus, CaseStatusMapping } from 'types';
+import { Resident, CaseStatusMapping } from 'types';
+import Tip from 'components/Tip/Tip';
+import { format } from 'date-fns';
+import Styles from 'components/CaseStatus/CaseStatusView.module.scss';
 
 interface Props {
   person: Resident;
@@ -15,28 +18,46 @@ const CaseStatusView = ({ person }: Props): React.ReactElement => {
     );
   }
 
-  if (!caseStatuses) {
+  if (!caseStatuses || caseStatuses?.length === 0) {
     return <></>;
   }
 
   return (
     <>
-      {groupByType(caseStatuses).map((status) => (
-        <span
-          className="govuk-tag lbh-tag lbh-tag--yellow govuk-!-margin-right-1 govuk-!-margin-top-2"
-          key={status}
-        >
-          {CaseStatusMapping[status]}
-        </span>
-      ))}
+      <div className={Styles.align}>
+        {caseStatuses.map((status) => (
+          <Tip
+            key={status.id}
+            interactive={true}
+            delay={100}
+            content={
+              <>
+                <p className={`lbh-body-xs ${Styles.popupTitle}`}>
+                  {CaseStatusMapping[status.type]}
+                </p>
+                <p className="lbh-body-xs">{`Start date: ${format(
+                  new Date(status.startDate),
+                  'dd MMM yyyy'
+                )}`}</p>
+                <a
+                  href={`http://dev.hackney.gov.uk:3000/people/${person.id}/details`}
+                >
+                  View details
+                </a>
+              </>
+            }
+          >
+            <span
+              className="govuk-tag lbh-tag lbh-tag--yellow govuk-!-margin-right-1 govuk-!-margin-top-2"
+              key={status.id}
+            >
+              {CaseStatusMapping[status.type]}
+            </span>
+          </Tip>
+        ))}
+      </div>
     </>
   );
 };
-
-function groupByType(
-  allCasesStatues: CaseStatus[]
-): (keyof typeof CaseStatusMapping)[] {
-  return Array.from(new Set(allCasesStatues.map((el) => el.type)));
-}
 
 export default CaseStatusView;
