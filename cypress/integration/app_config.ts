fa3c0f4534ab781385d25dcb79db660cf8799585
Chris Hutchinson <chris_hutchinson@me.com>
2021-10-18 13:35:04 +0100

    Cypress test to validate workflows URL after browser navigation

---
+++ cypress/integration/app_config.ts
@@ -0,0 +1,23 @@
+import { AuthRoles } from '../support/commands';
+
+describe('app config', () => {
+  it('should show the correct value for the workflows app even after a page navigation event has occurred on the client side', () => {
+    cy.visitAs(`/`, AuthRoles.AdminDevGroup);
+
+    cy.get('a').contains('Work in progress').click();
+
+    cy.location('pathname', { timeout: 60000 }).should(
+      'include',
+      '/work-in-progress'
+    );
+
+    cy.get('a').contains('Allocations').click();
+
+    cy.location('pathname', { timeout: 60000 }).should('eq', '/');
+
+    cy.get('a')
+      .contains('Workflows')
+      .invoke('attr', 'href')
+      .should('eq', 'https://www.hackney.gov.uk');
+  });
+});
