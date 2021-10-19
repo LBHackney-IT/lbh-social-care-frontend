6f0dbc60fbf28ab3e0bb7ac0b16da14a97c39174
Ruksana Shaukat Jali <ruksanashaukat@gmail.com>
2021-10-19 10:20:50 +0100

    Import gov components
    Utilising radio and selector components in screening.tsx

--- pages/screening.tsx
+++ pages/screening.tsx
@@ -6,10 +6,9 @@ const ScreeningDecision = (): React.ReactElement => (
     <h1>Make screening decision</h1>
     <section className="govuk-!-margin-bottom-8">
       <div className={s.heading}>
-        <h3>John Smith</h3>
+        <h2>John Smith</h2>
         <div>
           <span className="govuk-!-margin-right-3">
-            {' '}
             recieved at 10:00 6 Jun
           </span>
           <span className="govuk-tag lbh-tag lbh-tag--green">4 hours left</span>
@@ -21,7 +20,57 @@ const ScreeningDecision = (): React.ReactElement => (
       Write the rationale for your screening decision into the google document.
     </p>
     <h2>Select screening decision</h2>
+    <div className="govuk-form-group lbh-form-group">
+      <select className="govuk-select lbh-select" id="select-1" name="select-1">
+        <option value="1">Decision 1</option>
+      </select>
+    </div>
     <h2>Is this contact urgent?</h2>
+    <div className="govuk-form-group lbh-form-group">
+      <fieldset className="govuk-fieldset">
+        <div
+          className="govuk-radios lbh-radios govuk-radios--conditional"
+          data-module="govuk-radios"
+        >
+          <div className="govuk-radios__item">
+            <input
+              className="govuk-radios__input"
+              id="no-input"
+              name="no-input"
+              type="radio"
+              value="text"
+            />
+            <label
+              className="govuk-label govuk-radios__label"
+              htmlFor="no-input"
+            >
+              No
+            </label>
+          </div>
+          <div className="govuk-radios__item">
+            <input
+              className="govuk-radios__input"
+              id="yes-input"
+              name="yes-input"
+              type="radio"
+              value="phone"
+              checked
+            />
+            <label
+              className="govuk-label govuk-radios__label"
+              htmlFor="yes-input"
+            >
+              Yes
+            </label>
+          </div>
+          <div className="govuk-radios__conditional" id="hint-email">
+            <label className="govuk-label" htmlFor="hint">
+              Please email your MASH manager about the urgent case.
+            </label>
+          </div>
+        </div>
+      </fieldset>
+    </div>
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <Button label="Submit" type="submit" />
       <p
