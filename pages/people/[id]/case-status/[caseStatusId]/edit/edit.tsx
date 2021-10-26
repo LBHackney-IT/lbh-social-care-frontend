f198c8ff3ac6ad790b3ed58ba8dcca301262f012
Tony <tony@madtech.com>
2021-10-26 09:55:18 +0100

    SCT-1375 Add scheduled case status Announcement for END case status

--- pages/people/[id]/case-status/[caseStatusId]/edit/edit.tsx
+++ pages/people/[id]/case-status/[caseStatusId]/edit/edit.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import PersonView from 'components/PersonView/PersonView';
 import EditCaseStatusForm from 'components/CaseStatus/EditCaseStatusForm/EditCaseStatusForm';
+import AnnouncementMessage from 'components/AnnouncementMessage/AnnouncementMessage';
 
 const EditCaseStatus = (): React.ReactElement => {
   const router = useRouter();
   const personId = Number(router.query.id as string);
   const type = String(router.query.type as string);
   const caseStatusId = Number(router.query.caseStatusId as string);
-
   let action = String(router.query.action as string);
+  const isScheduledCaseStatus = Number(router.query.isScheduledCaseStatus);
 
   let prefilledFields;
   if (router.query.prefilledFields) {
@@ -20,8 +21,20 @@ const EditCaseStatus = (): React.ReactElement => {
     action.charAt(0).toUpperCase();
   }
 
+  let announcement;
+
+  if (isScheduledCaseStatus) {
+    announcement = (
+      <AnnouncementMessage
+        title="An update has already been scheduled for this status"
+        content="Any changes you make here will overwrite the scheduled update"
+      />
+    );
+  } else announcement = null;
+
   return (
     <>
+      {announcement}
       <h1 className="govuk-fieldset__legend--l gov-weight-lighter">
         {capitalize(action)} a case status
       </h1>
