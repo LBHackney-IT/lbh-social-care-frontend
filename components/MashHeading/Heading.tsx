75ffb7068b4e76465dc1f3c8bf5dc15dedea4ba2
Ruksana Shaukat Jali <ruksanashaukat@gmail.com>
2021-11-04 15:27:58 +0000

    Placed datetime conversion into the MASH heading component
    MASH heading now converts the ISO datetime format into the correct format.

--- components/MashHeading/Heading.tsx
+++ components/MashHeading/Heading.tsx
@@ -1,4 +1,5 @@
 import s from './MashHeading.module.scss';
+import { format } from 'date-fns';
 
 interface Props {
   clientname: string;
@@ -15,7 +16,10 @@ const MASHheading = ({
     <div className={s.banner}>
       <div className={s.heading}>{clientname}</div>
       <div>
-        <span className="govuk-!-margin-right-3"> received at {datetime}</span>
+        <span className="govuk-!-margin-right-3">
+          {' '}
+          received at {format(new Date(datetime), 'HH:00 dd MMM')}
+        </span>
         <span className="govuk-tag lbh-tag lbh-tag--green">
           {timeleft} left
         </span>
