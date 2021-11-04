013b208c3b0beb944f89bc3713580da6c0f20ce1
JohnFarrellDev <johnfarrell@fastmail.com>
2021-11-04 10:06:10 +0000

    remove important usage from CSS

--- components/MashHeading/Heading.tsx
+++ components/MashHeading/Heading.tsx
@@ -11,22 +11,17 @@ const MASHheading = ({
   timeleft,
   datetime,
 }: Props): React.ReactElement => (
-  <>
-    <section className="govuk-!-margin-bottom-8">
-      <div className={s.banner}>
-        <div className={s.heading}>{clientname}</div>
-        <div>
-          <span className="govuk-!-margin-right-3">
-            {' '}
-            received at {datetime}
-          </span>
-          <span className="govuk-tag lbh-tag lbh-tag--green">
-            {timeleft} left
-          </span>
-        </div>
+  <section className="govuk-!-margin-bottom-8">
+    <div className={s.banner}>
+      <div className={s.heading}>{clientname}</div>
+      <div>
+        <span className="govuk-!-margin-right-3"> received at {datetime}</span>
+        <span className="govuk-tag lbh-tag lbh-tag--green">
+          {timeleft} left
+        </span>
       </div>
-    </section>
-  </>
+    </div>
+  </section>
 );
 
 export default MASHheading;
