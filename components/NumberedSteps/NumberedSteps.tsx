11a817c45df25af3c1bc20e85b01c7d88ce03441
JohnFarrellDev <johnfarrell@fastmail.com>
2021-11-04 10:04:18 +0000

    remove unrequired react fragment

--- components/NumberedSteps/NumberedSteps.tsx
+++ components/NumberedSteps/NumberedSteps.tsx
@@ -5,15 +5,13 @@ interface Props {
 }
 
 const NumberedSteps = ({ nodes }: Props): React.ReactElement => (
-  <>
-    <ol className={s.steps}>
-      {nodes.map((node, i) => (
-        <li key={i} className={s.steps__event}>
-          {node}
-        </li>
-      ))}
-    </ol>
-  </>
+  <ol className={s.steps}>
+    {nodes.map((node, i) => (
+      <li key={i} className={s.steps__event}>
+        {node}
+      </li>
+    ))}
+  </ol>
 );
 
 export default NumberedSteps;
