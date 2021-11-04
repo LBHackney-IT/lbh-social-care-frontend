8b90749f9e8ce240118df52a30737e4c9ce2d78d
Ruksana Shaukat Jali <ruksanashaukat@gmail.com>
2021-11-04 18:17:52 +0000

    feat(SCT-1435): MASH date time format (#743)
    * Editing the time formatting for the dashboard tabs
    
    Changing the datetime format from ISO to 'HH:00 DD YY MMMM'
    
    * Placed datetime conversion into the MASH heading component
    
    MASH heading now converts the ISO datetime format into the correct format.
    
    * Adjust date time css on MASH cards and add text to action buttons
    
    Adjust date time css on MASH cards  so they match the Figma designs closer and added the correct text to the action buttons e.g. 'Make decision'.

--- components/MashCards/ContactCard.tsx
+++ components/MashCards/ContactCard.tsx
@@ -1,6 +1,7 @@
 import s from './MashCard.module.scss';
 import Link from 'next/link';
 import { MashReferral } from 'types';
+import { format } from 'date-fns';
 
 interface Props {
   mashReferral: MashReferral;
@@ -11,14 +12,14 @@ const ContactCard = ({ mashReferral }: Props): React.ReactElement => {
     <>
       <li className={s.row}>
         <div>
-          <p className="lbh-body-m govuk-!-margin-bottom-3">
+          <p className={`lbh-body-s govuk-!-margin-bottom-3 ${s.datetime}`}>
             <span className="govuk-tag lbh-tag lbh-tag--green">
               4 hours left
             </span>{' '}
-            {mashReferral.createdAt}
+            submitted {format(new Date(mashReferral.createdAt), 'HH:00 dd MMM')}
             <span className={`lbh-body-l lbh-!-font-weight-bold  ${s.action}`}>
               <Link href={`mash-referral/${mashReferral.id}/contact-decision`}>
-                Action
+                Work on
               </Link>
             </span>
           </p>
