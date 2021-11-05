220f6a0b441e2cd39877cbeeec79249634a31674
Tony <tony@madetech.com>
2021-11-05 14:55:45 +0000

    Add check for pastCaseStatusStartDate being undefined

--- pages/people/[id]/case-status/[caseStatusId]/edit/edit.tsx
+++ pages/people/[id]/case-status/[caseStatusId]/edit/edit.tsx
@@ -34,12 +34,14 @@ const EditCaseStatus = (): React.ReactElement => {
 
   return (
     <>
-      {pastCaseStatusStartDate && action == 'edit' && (
-        <AnnouncementMessage
-          title="Changes made here may affect previous status items"
-          content="Changing the start date of the current case status will also change the end date of the previous case status"
-        />
-      )}
+      {pastCaseStatusStartDate &&
+        pastCaseStatusStartDate !== 'undefined' &&
+        action == 'edit' && (
+          <AnnouncementMessage
+            title="Changes made here may affect previous status items"
+            content="Changing the start date of the current case status will also change the end date of the previous case status"
+          />
+        )}
       {isScheduledCaseStatus && action == 'end' && (
         <AnnouncementMessage
           title="An update has already been scheduled for this status"
