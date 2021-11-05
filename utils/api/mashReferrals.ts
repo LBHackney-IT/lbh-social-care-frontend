089306b8c3aa40eb5356ded1311782c11ae5920e
John Farrell <johnfarrell@fastmail.com>
2021-11-05 14:27:35 +0000

    feat(SCT-1420): Action page for initial decision (#745)
    * move initial decision form into state
    
    * on screening decision only show email hint if contact is urgent
    
    * functionality for calling api to apply form answers to initial decision
    
    * functionality for calling api to apply form answers to initial decision, pass through layers to hide api key
    
    * initial decision form to mange state, on submit send to api, reroute user to team assignments page with confirmation banner
    
    * move referral stage into an enum
    
    * filtering to use enum
    
    * for factory update stage to use referralstage enum

--- utils/api/mashReferrals.ts
+++ utils/api/mashReferrals.ts
@@ -10,10 +10,29 @@ export const submitScreeningDecision = async (
   const response = await axios.patch<MashReferral>(
     `/api/mash-referral/${referralId}/`,
     {
-      referralId,
       decision,
       requiresUrgentContact,
       updateType: 'SCREENING-DECISION',
+      workerEmail: workerEmail,
+    }
+  );
+  return response?.data;
+};
+
+export const submitInitialDecision = async (
+  referralId: string,
+  workerEmail: string,
+  decision: string,
+  referralCategory: string,
+  requiresUrgentContact: boolean
+): Promise<MashReferral> => {
+  const response = await axios.patch<MashReferral>(
+    `/api/mash-referral/${referralId}/`,
+    {
+      decision,
+      requiresUrgentContact,
+      referralCategory,
+      updateType: 'INITIAL-DECISION',
       workerEmail: workerEmail,
     }
   );
