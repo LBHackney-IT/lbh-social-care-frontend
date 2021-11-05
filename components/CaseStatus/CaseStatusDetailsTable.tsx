e9ea78c734b21d18c66c81fabc0d0f001f5a5656
Jack Musajo <jack.musajo@madetech.com>
2021-11-05 20:02:55 +0000

    feat(SCT-1433): Case status start/end date UI (#738)
    * Added check for parameter in LAC
    
    * Tests
    
    * Added test
    
    * better tests
    
    * Not skipping tests
    
    * Lots of stuff
    
    * All tests passing + correct view for everything
    
    * UI fix
    
    * Fixed tests, date set
    
    * tidy up
    
    * Fixed regexp for multiple OS
    
    * Forgot a case
    
    * Removed prints
    
    * Typo in prop name
    
    * Refactor and date change
    
    * Better check
    
    * Removed unused import
    
    * added comments
    
    * Fixed tests
    
    * SCT-1433 Update tests for FE UI
    
    * SCT-1433 Update episode reason logic
    
    Co-authored-by: Tony <tony@madtech.com>
    Co-authored-by: Tony <tony@madetech.com>
    Co-authored-by: tony griffin <54268925+tony-griffin@users.noreply.github.com>
    Co-authored-by: Rachel Hutchinson <32848419+rachel-hutchinson@users.noreply.github.com>

--- components/CaseStatus/CaseStatusDetailsTable.tsx
+++ components/CaseStatus/CaseStatusDetailsTable.tsx
@@ -9,6 +9,8 @@ interface Props {
   styleType?: string;
   status: CaseStatus;
   answers?: CaseStatusAnswerDisplay;
+  scheduledStatus?: CaseStatusAnswerDisplay[];
+  currentStatus?: CaseStatusAnswerDisplay[];
 }
 
 const CaseStatusDetailsTable = ({
@@ -16,10 +18,151 @@ const CaseStatusDetailsTable = ({
   styleType,
   status,
   answers,
+  scheduledStatus,
+  currentStatus,
 }: Props): React.ReactElement => {
   if (!status) {
     return <></>;
   }
+
+  const htmlElements = [];
+
+  if (status.type.toUpperCase() == 'CP' || status.type.toUpperCase() == 'CIN') {
+    const start_date = status.startDate;
+    const end_date = status.endDate;
+
+    start_date &&
+      !end_date &&
+      htmlElements.push(
+        <>
+          <dt className="govuk-summary-list__key ">Start date</dt>
+          <dd className="govuk-summary-list__value" data-testid="start_date">
+            {new Date(start_date).toLocaleDateString('en-GB', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}
+          </dd>
+        </>
+      );
+
+    start_date &&
+      end_date &&
+      htmlElements.push(
+        <>
+          <dt className="govuk-summary-list__key">Dates</dt>
+          <dd
+            className="govuk-summary-list__value"
+            data-testid="start_end_date"
+          >
+            {new Date(start_date).toLocaleDateString('en-GB', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}{' '}
+            -{' '}
+            {new Date(end_date).toLocaleDateString('en-GB', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}
+          </dd>
+        </>
+      );
+  } else if (status.type.toUpperCase() == 'LAC') {
+    let start_date = status.startDate;
+    let end_date = status.endDate;
+
+    //current active answers, when there's no scheduled status.
+    //the start date comes from the answers included
+    if (!scheduledStatus && answers?.startDate && !answers.endDate) {
+      start_date = answers.startDate;
+    }
+
+    //current active closed status, when there's no scheduled status.
+    //the end date comes from the status
+    if (answers?.startDate && !answers.endDate && status.endDate) {
+      end_date = status.endDate;
+    }
+
+    //past inactive status.
+    //start and end date comes from the answers
+    if (answers?.startDate && answers.endDate) {
+      start_date = answers.startDate;
+      end_date = answers.endDate;
+    }
+
+    //current active answers, when there's a scheduled status.
+    //start date comes from the answers
+    //end date comes from the scheduled status start date
+    if (
+      scheduledStatus &&
+      answers &&
+      status?.startDate &&
+      scheduledStatus[0].startDate
+    ) {
+      start_date = answers.startDate;
+      end_date = scheduledStatus[0].startDate;
+    }
+
+    //current active closed status with answers, when there's no scheduled status.
+    //end date comes from the main status
+    if (
+      answers &&
+      answers.status.length > 2 &&
+      answers?.startDate &&
+      !answers.endDate &&
+      status.endDate
+    ) {
+      end_date = status.endDate;
+    }
+
+    //last past status.
+    //end date is the current status start date
+    if (currentStatus && !end_date && !answers?.endDate && !status.endDate) {
+      end_date = currentStatus[0].startDate;
+    }
+
+    start_date &&
+      !end_date &&
+      htmlElements.push(
+        <>
+          <dt className="govuk-summary-list__key ">Start date</dt>
+          <dd className="govuk-summary-list__value" data-testid="start_date">
+            {new Date(start_date).toLocaleDateString('en-GB', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}
+          </dd>
+        </>
+      );
+
+    start_date &&
+      end_date &&
+      htmlElements.push(
+        <>
+          <dt className="govuk-summary-list__key">Dates</dt>
+          <dd
+            className="govuk-summary-list__value"
+            data-testid="start_end_date"
+          >
+            {new Date(start_date).toLocaleDateString('en-GB', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}{' '}
+            -{' '}
+            {new Date(end_date).toLocaleDateString('en-GB', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}
+          </dd>
+        </>
+      );
+  }
+
   return (
     <>
       {tableName && (
@@ -28,55 +171,18 @@ const CaseStatusDetailsTable = ({
         </h2>
       )}
       <dl
+        key={`table_${styleType}`}
         className={`govuk-summary-list lbh-summary-list ${styleType}`}
         data-testid="case_status_details_table"
       >
-        {status.startDate && !answers && (
-          <div className="govuk-summary-list__row">
-            <dt className="govuk-summary-list__key ">Start date</dt>
-            <dd className="govuk-summary-list__value" data-testid="start_date">
-              {new Date(status.startDate).toLocaleDateString('en-GB', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric',
-              })}
-            </dd>
-          </div>
-        )}
-
-        {answers?.startDate && !answers.endDate && (
-          <div className="govuk-summary-list__row">
-            <dt className="govuk-summary-list__key ">Start date</dt>
-            <dd className="govuk-summary-list__value" data-testid="start_date">
-              {new Date(answers.startDate).toLocaleDateString('en-GB', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric',
-              })}
-            </dd>
-          </div>
-        )}
-        {answers?.startDate && answers.endDate && (
-          <div className="govuk-summary-list__row">
-            <dt className="govuk-summary-list__key">Dates</dt>
-            <dd
-              className="govuk-summary-list__value"
-              data-testid="start_end_date"
-            >
-              {new Date(answers.startDate).toLocaleDateString('en-GB', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric',
-              })}{' '}
-              -{' '}
-              {new Date(answers.endDate).toLocaleDateString('en-GB', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric',
-              })}
-            </dd>
-          </div>
-        )}
+        {htmlElements.map((elm) => {
+          return (
+            <div className="govuk-summary-list__row" key={`table_${styleType}`}>
+              {' '}
+              {elm}
+            </div>
+          );
+        })}
 
         {answers &&
           answers?.status.length > 0 &&
