39e9fb542e389cdf6c64ad79fc670aac0e3f7cc8
Ruksana Shaukat Jali <ruksanashaukat@gmail.com>
2021-11-09 16:59:21 +0000

    Test for MashHeading
    Pushing the test for the 'MashHeading/Heading.tsx'

---
+++ components/MashHeading/Heading.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Heading from './Heading';
+import { mockedMashReferral } from 'factories/mashReferral';
+import { format } from 'date-fns';
+
+describe('Heading', () => {
+  it('renders a title', () => {
+    render(
+      <Heading
+        clientname={mockedMashReferral.clients[0]}
+        timeleft="4 hours"
+        datetime={mockedMashReferral.createdAt}
+      />
+    );
+    expect(screen.getByText('4 hours'));
+    expect(screen.getByText(mockedMashReferral.clients[0] as string));
+    expect(
+      screen.getByText(
+        `received at ${format(
+          new Date(mockedMashReferral.createdAt),
+          'HH:00 dd MMM'
+        )}`
+      )
+    );
+  });
+});
