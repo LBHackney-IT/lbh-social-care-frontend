24dbbbc02336578dbe3c5323737075b427d4b8ad
Ruksana Shaukat Jali <ruksanashaukat@gmail.com>
2021-11-12 10:26:11 +0000

    Making time thresholds dynamic
    Using 'mashReferral.createdAt' rather than the dummy time left.

--- components/MashTags/MashTags.tsx
+++ components/MashTags/MashTags.tsx
@@ -2,30 +2,24 @@ import moment from 'moment';
 
 interface Props {
   filter: string;
+  createdAt: string;
 }
 
-const MashTags = ({ filter }: Props): React.ReactElement => {
-  const timeRecieved = '2021-11-11T10:50:17.319Z';
+const MashTags = ({ filter, createdAt }: Props): React.ReactElement => {
   const currenttime = new Date().toISOString();
   const hoursPassed = moment(currenttime, 'YYYY-MM-DD[T]HH:mm:ss. SSS[Z]').diff(
-    moment(timeRecieved, 'YYYY-MM-DD[T]HH:mm:ss. SSS[Z]'),
+    moment(createdAt, 'YYYY-MM-DD[T]HH:mm:ss. SSS[Z]'),
     'hours'
   );
   const minsPassed = moment(currenttime, 'YYYY-MM-DD[T]HH:mm:ss. SSS[Z]').diff(
-    moment(timeRecieved, 'YYYY-MM-DD[T]HH:mm:ss. SSS[Z]'),
+    moment(createdAt, 'YYYY-MM-DD[T]HH:mm:ss. SSS[Z]'),
     'minutes'
   );
   if (filter === 'contact') {
-    if (hoursPassed > 1)
+    if (hoursPassed > 1 || hoursPassed == 1)
       return (
         <div className="govuk-tag lbh-tag lbh-tag--green">
-          {hoursPassed} hours ago
-        </div>
-      );
-    if (hoursPassed == 1)
-      return (
-        <div className="govuk-tag lbh-tag lbh-tag--green">
-          {hoursPassed} hour ago
+          {hoursPassed} {hoursPassed == 1 ? 'hour' : 'hours'} ago
         </div>
       );
     if (hoursPassed < 1 && hoursPassed > 0)
