18eb6ac64cc68bf9e9df5e9d6394b09f289a888a
jaye <hello@jayehackett.com>
2021-11-22 11:27:58 +0000

    fix test

--- components/NewPersonView/Layout.spec.tsx
+++ components/NewPersonView/Layout.spec.tsx
@@ -246,7 +246,7 @@ describe('Layout', () => {
       expect(screen.queryByText('Start workflow')).toBeVisible();
       expect(screen.queryByText('Start workflow')).toHaveAttribute(
         'href',
-        'http://example.com/workflows/new?social_care_id=123456789'
+        'http://example.com/residents/123456789'
       );
     });
 
