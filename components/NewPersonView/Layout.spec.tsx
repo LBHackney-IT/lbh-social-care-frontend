fdb6b00721cdcaf0d6d6f9fb04ca84cc8fada03d
Jaye Hackett <jaye.hackett@gmail.com>
2021-11-22 15:50:55 +0000

    chore: update resident's workflow link (#772)
    * update resident's workflow link
    
    * fix test
    
    * fix test
    
    * fix test again aaaaaa
    
    Co-authored-by: jaye <hello@jayehackett.com>

--- components/NewPersonView/Layout.spec.tsx
+++ components/NewPersonView/Layout.spec.tsx
@@ -310,7 +310,7 @@ describe('Layout', () => {
       expect(screen.queryByText('Workflows')).toBeVisible();
       expect(screen.queryByText('Workflows')).toHaveAttribute(
         'href',
-        'http://example.com?social_care_id=123456789&show_historic=true'
+        'http://example.com/residents/123456789'
       );
     });
   });
