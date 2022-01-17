7220da1ff5987f394750b7c3ae958c800488e394
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2022-01-17 15:47:32 +0000

    WorkerSelect tests

--- components/WorkerSelect/WorkerSelect.spec.tsx
+++ components/WorkerSelect/WorkerSelect.spec.tsx
@@ -1,21 +1,22 @@
 import WorkerSelect from './WorkerSelect';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('workerSelect', () => {
-  it('should render properly', () => {
-    const mockedWorker = {
-      _meta: {
-        location: {
-          api: 'fakeapi',
-          frontend: 'fakefrontend',
-        },
-        domain: 'worker',
+  const mockedWorker = {
+    _meta: {
+      location: {
+        api: 'fakeapi',
+        frontend: 'fakefrontend',
       },
-      id: 1,
-      firstName: 'Fake',
-      lastName: 'Worker',
-      email: 'fakeworker@foo.com',
-    };
+      domain: 'worker',
+    },
+    id: 1,
+    firstName: 'Fake',
+    lastName: 'Worker',
+    email: 'fakeworker@foo.com',
+  };
+
+  it('should render properly', () => {
     const { asFragment } = render(
       <WorkerSelect
         label="Matching workers"
@@ -25,5 +26,51 @@ describe('workerSelect', () => {
       />
     );
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('handles when no workers are found', () => {
+    render(
+      <WorkerSelect
+        label="Matching workers"
+        workers={[]}
+        idToAdd={0}
+        setIdToAdd={jest.fn()}
+      />
+    );
+
+    expect(screen.getByText('No results'));
+  });
+
+  it('should call the setIdToAdd callback with the id of the selected worker', () => {
+    const mockSetIdToAdd = jest.fn();
+
+    const { container } = render(
+      <WorkerSelect
+        label="Matching workers"
+        workers={[mockedWorker]}
+        idToAdd={0}
+        setIdToAdd={mockSetIdToAdd}
+      />
+    );
+
+    const input = container.querySelector('input');
+    input && fireEvent.click(input);
+
+    expect(mockSetIdToAdd).toBeCalledWith(mockedWorker.id);
+  });
+
+  it("correctly formats a Worker's details", () => {
+    render(
+      <WorkerSelect
+        label="Matching workers"
+        workers={[mockedWorker]}
+        idToAdd={mockedWorker.id}
+        setIdToAdd={jest.fn()}
+      />
+    );
+    expect(
+      screen.getByText(`${mockedWorker.firstName} ${mockedWorker.lastName}`)
+    );
+    expect(screen.getAllByRole('radio').length).toBe(1);
   });
 });
