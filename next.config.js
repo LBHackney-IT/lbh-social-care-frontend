783a08e316e7e9a4e5731ed273edd0675ddc3696
tony griffin <54268925+tony-griffin@users.noreply.github.com>
2022-01-19 16:54:53 +0000

    feat(SCT-1279): add lambda layer & sentry setup branch (#812)
    * Sentry - Initial set up & error page
    
    * SCT-1279 Add initial content security policy & sentry connection
    
    * Install @sentry/cli
    
    * Update yarn.lock file
    
    * SCT-1279: Add config for CircleCI to notify Sentry of releases
    
    * SCT-1279: Add config for CircleCI to notify Sentry of releases
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Remove Sentry orb and extra line breaks
    
    * SCT-1279 Add page to test for errors to be sent to Sentry
    
    Page includes buttons that trigger errors on the FE and also the server.
    Also a link that triggers an error when getServerSideProps is used.
    
    * SCT-1279 Add lambda layer code to serverless.yml for sentry set up
    
    Add lambda layer code to serverless.yml for sentry set up
    
    * Add dryRun option to the next.config.js file
    
    * Add note to next.config.js
    
    * Update yarn lock file
    
    * Run Lint fix
    
    * Add yarn command to create layer directory
    
    * Add executable permissions for bash script
    
    * Added Sentry-dsn to AWS parameter store and serverless file
    
    * Environmental variables
    
    * Reducing number of variables
    
    * SCT-1279 PR suggestion changes
    
    * SCT-1279 Add sentry release to deploy jobs & source map creation
    
    * Remove sentry release code - test
    
    * set dryRun to true to test release to sentry
    
    * Add CIRCLE_SHA1 as a build env var for most recent git commit sha
    
    * Add brackets to circle_sha1
    
    * Add print statement for sentry release git sha
    
    * Add echo to sentry_release command to set git sha on release
    
    * Fix valiadtion error
    
    * SCT-1279 Update Yarn command by setting SENTRY_RELEASE var
    
    Also added this sentry_release var to the serverless.yml file
    
    * SCT-1279 Add temporary e2e tests for sentry
    
    * Remove temp e2e test
    
    * SCT-1279 Fix lint errors
    
    Co-authored-by: tony-griffin <tony@madetech.com>
    Co-authored-by: rachel-hutchinson <rachel.hutchinson@madetech.com>
    Co-authored-by: mapleleafjack <jack.musajo@madetech.com>

--- next.config.js
+++ next.config.js
@@ -1,27 +1,54 @@
-module.exports = {
-  distDir: 'build/_next',
-  target: 'server',
-  poweredByHeader: false,
+const { withSentryConfig } = require('@sentry/nextjs');
 
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-        ],
-      },
-    ];
+module.exports = withSentryConfig(
+  {
+    distDir: 'build/_next',
+    target: 'server',
+    poweredByHeader: false,
+
+    async headers() {
+      const headers = [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'SAMEORIGIN',
+            },
+            {
+              key: 'X-XSS-Protection',
+              value: '1; mode=block',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+          ],
+        },
+      ];
+
+      if (process.env.NODE_ENV === 'production') {
+        headers[0].headers.push({
+          key: 'Content-Security-Policy',
+          value:
+            "connect-src 'self' o183917.ingest.sentry.io; " +
+            "default-src 'self' fonts.googleapis.com fonts.gstatic.com www.googletagmanager.com; " +
+            "frame-ancestors 'self'; " +
+            "form-action 'self';",
+        });
+      }
+
+      return headers;
+    },
   },
-};
+  {
+    silent: true,
+    org: 'london-borough-of-hackney',
+    project: 'social-care-main-app',
+    dryRun: !(
+      process.env.CI ||
+      process.env.NEXT_PUBLIC_SENTRY_TARGET === 'production' ||
+      process.env.NEXT_PUBLIC_SENTRY_TARGET === 'dev'
+    ),
+  }
+);
