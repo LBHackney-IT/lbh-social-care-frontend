1c9510e2d54c0961d5c98f249efae86b78f373f3
Tony <tony@madetech.com>
2022-01-20 13:04:26 +0000

    remove execuatbele step from bash script

--- scripts/prepareNodeModules.sh
+++ scripts/prepareNodeModules.sh
@@ -6,7 +6,4 @@ mkdir -p layer/nodejs/node_modules
 echo "Install production dependencies in layer directory"
 yarn install --production --modules-folder ./layer/nodejs/node_modules
 
-echo "Change permissions"
-chmod +x layer/nodejs/node_modules
-
 echo "Done creating nodejs/node_modules layer"
