7967dae36107fb00f8e8a67386cf1a9c28193f2d
Tony <tony@madetech.com>
2022-01-20 16:06:06 +0000

    Revert "feat(SCT-1279): add lambda layer & sentry setup branch (#812)"
    This reverts commit 783a08e316e7e9a4e5731ed273edd0675ddc3696.

--- next.config.js
+++ next.config.js
@@ -1,64 +1,27 @@
-const { withSentryConfig } = require('@sentry/nextjs');
+module.exports = {
+  distDir: 'build/_next',
+  target: 'server',
+  poweredByHeader: false,
 
-module.exports = withSentryConfig(
-  {
-    distDir: 'build/_next',
-    target: 'server',
-    poweredByHeader: false,
-
-    async redirects() {
-      return [
-        {
-          source: '/team-assignments',
-          destination: '/',
-          permanent: true,
-        },
-      ];
-    },
-
-    async headers() {
-      const headers = [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Frame-Options',
-              value: 'SAMEORIGIN',
-            },
-            {
-              key: 'X-XSS-Protection',
-              value: '1; mode=block',
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-          ],
-        },
-      ];
-
-      if (process.env.NODE_ENV === 'production') {
-        headers[0].headers.push({
-          key: 'Content-Security-Policy',
-          value:
-            "connect-src 'self' o183917.ingest.sentry.io; " +
-            "default-src 'self' fonts.googleapis.com fonts.gstatic.com www.googletagmanager.com; " +
-            "frame-ancestors 'self'; " +
-            "form-action 'self';",
-        });
-      }
-
-      return headers;
-    },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
   },
-  {
-    silent: true,
-    org: 'london-borough-of-hackney',
-    project: 'social-care-main-app',
-    dryRun: !(
-      process.env.CI ||
-      process.env.NEXT_PUBLIC_SENTRY_TARGET === 'production' ||
-      process.env.NEXT_PUBLIC_SENTRY_TARGET === 'dev'
-    ),
-  }
-);
+};
