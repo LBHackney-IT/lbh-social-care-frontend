4c9e7bed27aa23cc0268659102b59f4d3ab3ce42
Rachel Hutchinson <32848419+rachel-hutchinson@users.noreply.github.com>
2022-01-24 14:32:52 +0000

    feat(SCT-1279): Setup Sentry (#827)
    * Setup sentry config
    
    * Fix formatting and add variables to serverless
    
    * Rename server config file
    
    * chmod to make bash script executable
    
    Co-authored-by: mapleleafjack <jack.musajo@madetech.com>

---
+++ scripts/prepareNodeModules.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Create layer directory for node modules"
+mkdir -p layer/nodejs/node_modules
+
+echo "Install production dependencies in layer directory"
+yarn install --production --modules-folder ./layer/nodejs/node_modules
+
+echo "Done creating nodejs/node_modules layer"
