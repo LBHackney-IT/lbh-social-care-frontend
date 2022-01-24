e4b68878aed9e9d5e12f444ba2dadadd9791ab55
Jack Musajo <jack.musajo@madetech.com>
2022-01-24 16:34:11 +0000

    chore: Revert sentry (#829)

--- scripts/prepareNodeModules.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Create layer directory for node modules"
-mkdir -p layer/nodejs/node_modules
-
-echo "Install production dependencies in layer directory"
-yarn install --production --modules-folder ./layer/nodejs/node_modules
-
-echo "Done creating nodejs/node_modules layer"
