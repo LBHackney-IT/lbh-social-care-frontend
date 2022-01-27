8035536e2deed195664a59fabe2911ae8a5df0f5
jaye <hello@jayehackett.com>
2022-01-27 17:39:43 +0000

    tree wip

--- pages/residents/[id]/workflows.tsx
+++ pages/residents/[id]/workflows.tsx
@@ -5,26 +5,49 @@ import { GetServerSideProps } from 'next';
 import { Resident } from 'types';
 import { isAuthorised } from 'utils/auth';
 import Link from 'next/link';
+import { Workflow } from 'components/ResidentPage/types';
+import { useMemo } from 'react';
 
 interface Props {
   resident: Resident;
 }
 
+interface WorkflowWithChildren extends Workflow {
+  children?: WorkflowWithChildren[];
+}
+
+const convertWorkflowsToTree = (
+  workflows: Workflow[]
+): WorkflowWithChildren[] => {
+  const topLevel = workflows.filter((w) => !w.workflowId);
+  return topLevel.map((w) => {
+    return {
+      ...w,
+      children: workflows.filter((x) => x.workflowId === w.id),
+    };
+  });
+};
+
 const WorkflowsPage = ({ resident }: Props): React.ReactElement => {
   const { data } = useWorkflows(resident.id, 1000);
 
   const workflows = data?.workflows;
+  const tree = useMemo(
+    () => convertWorkflowsToTree(workflows || []),
+    [workflows]
+  );
 
   return (
     <Layout resident={resident}>
       <ul className="lbh-list">
-        {workflows?.map((w) => (
+        {tree?.map((w) => (
           <li key={w.id}>
             <Link
               href={`${process.env.NEXT_PUBLIC_CORE_PATHWAY_APP_URL}/workflows/${w.id}`}
             >
               {w?.form?.name || w.formId}
             </Link>
+            — {w.workflowId}
           </li>
         ))}
       </ul>
