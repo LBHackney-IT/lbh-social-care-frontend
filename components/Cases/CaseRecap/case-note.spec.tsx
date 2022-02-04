b29e8036fdcb9dd8278745af2d6b135146e9ced3
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-02-04 11:01:21 +0000

    working test

--- components/Cases/CaseRecap/case-note.spec.tsx
+++ components/Cases/CaseRecap/case-note.spec.tsx
@@ -3,10 +3,10 @@ import { render } from '@testing-library/react';
 import { residentFactory } from 'factories/residents';
 import { mockedWorker } from 'factories/workers';
 import { SubmissionState } from 'data/flexibleForms/forms.types';
-import { useRouter } from 'next/router';
+import { NextRouter, useRouter } from 'next/router';
 
 // jest.mock('next/router');
-// const useRouterMock = useRouter as jest.MockedFunction<typeof useRouter>;
+const useRouterMock = useRouter as jest.MockedFunction<typeof useRouter>;
 // const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 let eventName;
@@ -28,29 +28,53 @@ let routeChangeHandler;
 //   };
 // });
 
-jest.mock('next/router', () => ({
-  useRouter() {
-    return {
-      basePath: '/',
-      pathname: '/',
-      route: '/',
-      query: {},
-      asPath: '/',
-      push: jest.fn(() => Promise.resolve(true)),
-      replace: jest.fn(() => Promise.resolve(true)),
-      reload: jest.fn(() => Promise.resolve(true)),
-      prefetch: jest.fn(() => Promise.resolve()),
-      back: jest.fn(() => Promise.resolve(true)),
-      beforePopState: jest.fn(() => Promise.resolve(true)),
-      isFallback: false,
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-        emit: jest.fn(),
-      },
-    };
+jest.mock('next/router');
+// const useRouterMock = () => ({
+//   useRouter() {
+//     return {
+//       basePath: '/',
+//       pathname: '/',
+//       route: '/',
+//       query: {},
+//       asPath: '/',
+//       push: jest.fn(() => Promise.resolve(true)),
+//       replace: jest.fn(() => Promise.resolve(true)),
+//       reload: jest.fn(() => Promise.resolve(true)),
+//       prefetch: jest.fn(() => Promise.resolve()),
+//       back: jest.fn(() => Promise.resolve(true)),
+//       beforePopState: jest.fn(() => Promise.resolve(true)),
+//       isFallback: false,
+//       events: {
+//         on: jest.fn(),
+//         off: jest.fn(),
+//         emit: jest.fn(),
+//       },
+//     };
+//   },
+// });
+
+const mockRouter: NextRouter = {
+  basePath: '/',
+  pathname: '/',
+  route: '/',
+  query: {},
+  asPath: '/',
+  push: jest.fn(() => Promise.resolve(true)),
+  replace: jest.fn(() => Promise.resolve(true)),
+  reload: jest.fn(() => Promise.resolve(true)),
+  prefetch: jest.fn(() => Promise.resolve()),
+  back: jest.fn(() => Promise.resolve(true)),
+  beforePopState: jest.fn(() => Promise.resolve(true)),
+  isFallback: false,
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
   },
-}));
+  isReady: true,
+  isPreview: false,
+  isLocaleDomain: true,
+};
 
 // const spyOnReplace = jest.spyOn(useRouter, 'replace');
 
@@ -89,9 +113,10 @@ describe('Case note page', () => {
     // (useRouter().replace as jest.Mock).mockRejectedValue(new Error());
     // const router = useRouter();
     // router.query = { submissionId: '' };
+    useRouterMock.mockReturnValue(mockRouter);
 
     const { getByText } = render(<CaseNote {...mockedNewSubmission} />);
 
-    expect(useRouter.useRouter).toHaveBeenCalled();
+    expect(useRouterMock).toHaveBeenCalled();
   });
 });
