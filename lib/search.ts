4e8958017becaf31d19b1a755d55d1088a64867b
mapleleafjack <jack.musajo@madetech.com>
2022-03-08 13:38:14 +0000

    Search now uses new API call + sanitised data

--- lib/search.ts
+++ lib/search.ts
@@ -1,20 +1,41 @@
 import axios from 'axios';
-import type { ResidentsAPI } from 'types';
+import type { ResidentsAPI, LegacyResident } from 'types';
 
 const ENDPOINT_API = process.env.ENDPOINT_API;
 const AWS_KEY = process.env.AWS_KEY;
 const headers = { 'x-api-key': AWS_KEY };
 
+interface ResidentBE extends LegacyResident {
+  addressList: Array<{
+    displayAddressFlag: 'Y' | 'N';
+    addressLine1: string;
+    postCode: string;
+    uprn: string;
+  }>;
+}
+
+const sanitiseResidentData = (residents: ResidentBE[]): LegacyResident[] =>
+  residents?.map(({ addressList, ...resident }: ResidentBE): LegacyResident => {
+    const address = addressList?.find(
+      ({ displayAddressFlag }) => displayAddressFlag === 'Y'
+    );
+    return {
+      ...resident,
+      address: address && {
+        address: address.addressLine1,
+        postcode: address.postCode,
+        uprn: address.uprn,
+      },
+    };
+  });
+
 export const searchPerson = async (
   params: Record<string, unknown>,
   contextFlag?: string
-): Promise<ResidentsAPI | []> => {
-  const { data }: { data: ResidentsAPI } = await axios.get(
-    `${ENDPOINT_API}/search/person`,
-    {
-      headers,
-      params: { ...params, contextFlag },
-    }
-  );
-  return data;
+): Promise<ResidentsAPI> => {
+  const { data } = await axios.get(`${ENDPOINT_API}/search/person`, {
+    headers,
+    params: { ...params, contextFlag },
+  });
+  return { ...data, residents: sanitiseResidentData(data.residents) };
 };
