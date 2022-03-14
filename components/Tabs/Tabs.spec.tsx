46a3c609df545647e4716bd52b789476245523ba
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-03-14 11:02:20 +0000

    Additional tests

--- components/Tabs/Tabs.spec.tsx
+++ components/Tabs/Tabs.spec.tsx
@@ -41,4 +41,39 @@ describe(`Tabs`, () => {
       expect(updatedQuery.mosaic_id).toBe(undefined);
     }
   });
+  it('should remove name if a name is present and the url is /cases', () => {
+    const query = { person_id: 123, name: 'tom' } as unknown as ParsedUrlQuery;
+    const updatedQuery = updateQuery(query, '/cases');
+    if (updatedQuery) {
+      expect(updatedQuery.mosaic_id).toBe(123);
+      expect(updatedQuery.name).toBe(undefined);
+    }
+  });
+  it('should remove name, date of birth and postcode if they are present and the url is /cases', () => {
+    const query = {
+      person_id: 123,
+      name: 'tom',
+      date_of_birth: '22-01-2022',
+      postcode: 'N1 1QA',
+    } as unknown as ParsedUrlQuery;
+    const updatedQuery = updateQuery(query, '/cases');
+    if (updatedQuery) {
+      expect(updatedQuery.mosaic_id).toBe(123);
+      expect(updatedQuery.name).toBe(undefined);
+    }
+  });
+  it('should not remove name, date of birth and postcode if they are present and the url is /search', () => {
+    const query = {
+      person_id: 123,
+      name: 'tom',
+      date_of_birth: '22-01-2022',
+      postcode: 'N1 1QA',
+    } as unknown as ParsedUrlQuery;
+    const updatedQuery = updateQuery(query, '/search');
+    if (updatedQuery) {
+      expect(updatedQuery.name).toBe('tom');
+      expect(updatedQuery.date_of_birth).toBe('22-01-2022');
+      expect(updatedQuery.postcode).toBe('N1 1QA');
+    }
+  });
 });
