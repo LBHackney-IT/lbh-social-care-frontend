b3f89910c1feb0f5213c9c7cd121211c33ddde71
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-03-14 09:16:08 +0000

    Passing person_id

--- components/Tabs/Tabs.tsx
+++ components/Tabs/Tabs.tsx
@@ -15,12 +15,22 @@ export interface Props {
   children: React.ReactChild;
 }
 
-const updateQuery = (query: ParsedUrlQuery) => {
-  if (query && url == '/search') {
-    return query.replace('moasic_id', 'person_id');
-  }
-  if (query && url == '/cases') {
-    return query.replace('person_id', 'moasic_id');
+const updateQuery = (query: ParsedUrlQuery, url: string) => {
+  console.log('url & query', url, query);
+  if (query) {
+    const updatedQuery = query;
+    if (url == '/cases') {
+      if (query.person_id && !query.mosaic_id) {
+        console.log('ding');
+        updatedQuery.mosaic_id = query.person_id;
+        console.log('updated query', updatedQuery);
+      }
+      if (!query.person_id && query.mosaic_id) {
+        console.log('dong');
+        delete updatedQuery.mosaic_id;
+      }
+    }
+    return updatedQuery;
   } else {
     return undefined;
   }
@@ -42,7 +52,7 @@ const Tabs = ({ title, tabs, children }: Props): React.ReactElement => {
             })}
           >
             <Link
-              href={{ pathname: url, query: updateQuery(query) }}
+              href={{ pathname: url, query: updateQuery(query, url) }}
               scroll={false}
             >
               <a className={`lbh-link lbh-link--no-visited-state ${s.link}`}>
