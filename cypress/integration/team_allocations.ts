39ada0bf3b7c4665a58b6dea250bd3d97ccf61c7
mapleleafjack <jack.musajo@madetech.com>
2022-03-21 13:37:45 +0000

    Added cypress test

--- cypress/integration/team_allocations.ts
+++ cypress/integration/team_allocations.ts
@@ -14,5 +14,20 @@ describe('Worker / team allocation', () => {
       cy.contains('Choose a priority rating').should('exist');
       cy.contains('This is for').should('exist');
     });
+
+    it('Loads correcty the "Allocate a worker', () => {
+      cy.visitAs(
+        `/residents/${Cypress.env(
+          'ADULT_RECORD_PERSON_ID'
+        )}/allocations/${Cypress.env('ALLOCATION_ID')}/allocateworker`,
+        AuthRoles.AdminDevGroup
+      );
+
+      cy.contains('Allocate a worker').should('exist');
+      cy.contains('Mike Gallagher').should('exist');
+      cy.contains('Total allocations').should('exist');
+      cy.contains('Select an allocation date').should('exist');
+      cy.contains('This is for').should('exist');
+    });
   });
 });
