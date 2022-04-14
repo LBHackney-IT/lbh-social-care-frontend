092babb28253baa7b9e2fe6b0eb9a736ce430f12
Jack Musajo <jack.musajo@madetech.com>
2022-04-14 14:27:41 +0100

    fix(SCT-1822): More allocations UI fixes (#927)

--- components/TeamPage/TeamWorkerList/TeamWorkerList.tsx
+++ components/TeamPage/TeamWorkerList/TeamWorkerList.tsx
@@ -57,7 +57,7 @@ const TeamMemberAllocations = ({ user }: TeamMemberProps) => {
                     new Date(),
                     { addSuffix: true }
                   )}
-                  {' )'}
+                  {')'}
                 </td>
               </tr>
             ))}
