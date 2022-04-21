4a3031294635aee2fc16979b545af12ec3ee720c
Alexandra Herbert <Alexandra.Herbert@Madetech.com>
2022-04-21 13:08:28 +0100

    Adding more tests to check link to resident view is generated correctly

--- components/Steps/PersonLinkConfirmation.spec.tsx
+++ components/Steps/PersonLinkConfirmation.spec.tsx
@@ -1,11 +1,13 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import DetailConfirmation from './PersonLinkConfirmation';
+import Router from 'next/router';
 
 jest.mock('next/router', () => ({
   useRouter: () => ({
     query: { id: 'foo123456bar' },
   }),
+  push: jest.fn(),
 }));
 
 describe('Detail Confirmation component', () => {
@@ -17,5 +19,8 @@ describe('Detail Confirmation component', () => {
     const { asFragment, getByText } = render(<DetailConfirmation {...props} />);
     expect(asFragment()).toMatchSnapshot();
     expect(getByText(props.successMessage)).toBeInTheDocument();
+    fireEvent.click(screen.getByText('View person'));
+
+    expect(Router.push).toHaveBeenCalledWith('/residents/foo123456bar');
   });
 });
--- components/Steps/ReviewWarningNoteConfirmation.spec.tsx
+++ components/Steps/ReviewWarningNoteConfirmation.spec.tsx
@@ -1,6 +1,11 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { mockedResident } from 'factories/residents';
 import ReviewWarningNoteConfirmation from './ReviewWarningNoteConfirmation';
+import Router from 'next/router';
+
+jest.mock('next/router', () => ({
+  push: jest.fn(),
+}));
 
 describe('Review Warning Note Confirmation', () => {
   const props = {
@@ -35,5 +40,22 @@ describe('Review Warning Note Confirmation', () => {
       getByText('This Warning Note has been closed for Foo Bar')
     ).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('it should redirect to the resident view on confirm', () => {
+    const props = {
+      formData: {
+        person: mockedResident,
+        reviewDecision: 'No',
+      },
+    };
+
+    const { asFragment, getByText } = render(
+      <ReviewWarningNoteConfirmation {...props} />
+    );
+
+    fireEvent.click(screen.getByText('View person'));
+
+    expect(Router.push).toHaveBeenCalledWith('/residents/1');
   });
 });
