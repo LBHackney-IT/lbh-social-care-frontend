36c5223f784dde7b90aa1b5f08f4597d88879c22
Miles Alford <43789512+Miles-Alford@users.noreply.github.com>
2022-05-03 12:40:58 +0100

    feat: Add address dropdown to resident details (#939)
    * Inital failing test and displaying multiple results from postcode search
    
    Co-authored-by: Jaseera Abubacker <jpmaHub@users.noreply.github.com>
    Co-authored-by: Miles Alford <Miles-Alford@users.noreply.github.com>
    
    * Populate address and postcode with
    selected dropdown value
    Co-authored-by: Jaseera Abubacker <jpmaHub@users.noreply.github.com>
    Co-authored-by: Miles Alford <Miles-Alford@users.noreply.github.com>
    Co-authored-by: Alexandra Herbert <AlexHerbertMadeTech@users.noreply.github.com>
    
    * Tidy up code
    
    * Attempt to increase page size
    
    * Revert attempt to increase page size
    
    * Remove extra get of data and additional tests added
    
    Co-authored-by: Alexandra Herbert <AlexHerbertMadeTech@users.noreply.github.com>
    
    * Fix broken test
    
    Co-authored-by: Alexandra Herbert <AlexHerbertMadeTech@users.noreply.github.com>
    Co-authored-by: Miles Alford <Miles-Alford@users.noreply.github.com>
    
    * Adding failing test to fix after lunch
    
    * Adding failing test to fix after lunch
    
    * Fix multiple pages address test
    
    Co-authored-by: Rachel Hutchinson <rachel-hutchinson@users.noreply.github.com>
    
    * Hide UPRN
    
    * Remove clickoutside from custom address editor
    
    * Handle errors from postcode API
    
    Co-authored-by: Rachel Hutchinson <rachel-hutchinson@users.noreply.github.com>
    Co-authored-by: Jaseera Abubacker <jpmaHub@users.noreply.github.com>
    
    Co-authored-by: Rachel Hutchinson <rachelhutchinson93@gmail.com>
    Co-authored-by: Jaseera Abubacker <jpmaHub@users.noreply.github.com>
    Co-authored-by: Miles Alford <Miles-Alford@users.noreply.github.com>
    Co-authored-by: Alexandra Herbert <AlexHerbertMadeTech@users.noreply.github.com>
    Co-authored-by: Alexandra Herbert <Alexandra.Herbert@Madetech.com>
    Co-authored-by: Rachel Hutchinson <rachel-hutchinson@users.noreply.github.com>

--- components/ResidentPage/CustomAddressEditor.spec.tsx
+++ components/ResidentPage/CustomAddressEditor.spec.tsx
@@ -1,12 +1,23 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import { mockedResident } from 'factories/residents';
 import CustomAddressEditor from './CustomAddressEditor';
 import axios from 'axios';
 import userEvent from '@testing-library/user-event';
+import {
+  addressesAPI3and4WrapperFactory,
+  addressesAPIWrapperFactory,
+} from 'factories/postcode';
 
 const mockClose = jest.fn();
 
 jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 (global.fetch as jest.Mock) = jest.fn(() => ({
   status: 200,
@@ -83,8 +94,136 @@ describe('CustomAddressEditor', () => {
     userEvent.type(screen.getByLabelText('Building number or name'), '1');
     userEvent.type(screen.getByLabelText('Postcode'), 'Town St');
     await waitFor(() => fireEvent.click(screen.getByText('Find address')));
-    expect(axios.get).toBeCalledWith('/api/postcode/Town St?buildingNumber=1');
+    expect(mockedAxios.get).toBeCalledWith(
+      '/api/postcode/Town St?page=1&buildingNumber=1'
+    );
     expect(screen.getAllByLabelText('Postcode').length).toBe(2);
+  });
+
+  it('can show multiple results for a postcode search', async () => {
+    const mocked_results = addressesAPIWrapperFactory.build();
+    mockedAxios.get.mockResolvedValue({
+      data: mocked_results,
+    });
+
+    render(
+      <CustomAddressEditor
+        name="address"
+        label="Address"
+        onClose={mockClose}
+        resident={{
+          ...mockedResident,
+          address: undefined,
+        }}
+      />
+    );
+
+    userEvent.type(screen.getByLabelText('Building number or name'), '1');
+    userEvent.type(screen.getByLabelText('Postcode'), 'Town St');
+    await waitFor(() => fireEvent.click(screen.getByText('Find address')));
+    expect(mockedAxios.get).toBeCalledWith(
+      '/api/postcode/Town St?page=1&buildingNumber=1'
+    );
+
+    const addressDropDown = screen.getByRole('combobox');
+    expect(addressDropDown).not.toBeNull();
+    expect(addressDropDown.childElementCount).toBe(
+      mocked_results.address.length
+    );
+
+    const expectedAddress = screen.getByText('test line1');
+    expect(expectedAddress).not.toBeNull();
+    expect(expectedAddress).toBeInTheDocument();
+  });
+
+  it('populates the address fields with a selected address', async () => {
+    const mocked_results = addressesAPIWrapperFactory.build();
+    mockedAxios.get.mockResolvedValue({
+      data: mocked_results,
+    });
+
+    render(
+      <CustomAddressEditor
+        name="address"
+        label="Address"
+        onClose={mockClose}
+        resident={{
+          ...mockedResident,
+          address: undefined,
+        }}
+      />
+    );
+
+    userEvent.type(screen.getByLabelText('Building number or name'), '1');
+    userEvent.type(screen.getByLabelText('Postcode'), 'Town St');
+    await waitFor(() => fireEvent.click(screen.getByText('Find address')));
+    expect(mockedAxios.get).toBeCalledWith(
+      '/api/postcode/Town St?page=1&buildingNumber=1'
+    );
+
+    const addressDropDown = screen.getByRole('combobox');
+    expect(addressDropDown).not.toBeNull();
+
+    await act(async () => {
+      fireEvent.click(addressDropDown);
+      fireEvent.change(addressDropDown, {
+        target: {
+          value: '{"address":"test line1 2","postcode":"A1 1AA","uprn":"1234"}',
+        },
+      });
+    });
+
+    expect(screen.getByLabelText('Address')).toHaveValue('test line1 2');
+    expect(screen.getAllByLabelText('Postcode')[1]).toHaveValue('A1 1AA');
+    expect(
+      screen.getByLabelText('Unique property reference number')
+    ).toHaveValue('1234');
+  });
+
+  it('submits a selected address correctly', async () => {
+    const mocked_results = addressesAPIWrapperFactory.build();
+    mockedAxios.get.mockResolvedValue({
+      data: mocked_results,
+    });
+
+    render(
+      <CustomAddressEditor
+        name="address"
+        label="Address"
+        onClose={mockClose}
+        resident={mockedResident}
+      />
+    );
+    userEvent.type(screen.getByLabelText('Building number or name'), '1');
+    userEvent.type(screen.getAllByLabelText('Postcode')[0], 'Town St');
+    await waitFor(() => fireEvent.click(screen.getByText('Find address')));
+    expect(mockedAxios.get).toBeCalledWith(
+      '/api/postcode/Town St?page=1&buildingNumber=1'
+    );
+
+    const addressDropDown = screen.getByRole('combobox');
+
+    await act(async () => {
+      fireEvent.click(addressDropDown);
+      fireEvent.change(addressDropDown, {
+        target: {
+          value: '{"address":"test line1 2","postcode":"A1 1AA","uprn":"1234"}',
+        },
+      });
+    });
+    await waitFor(() => fireEvent.click(screen.getByText('Save')));
+
+    expect(global.fetch).toBeCalledWith('/api/residents/1', {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        address: {
+          address: 'test line1 2',
+          postcode: 'A1 1AA',
+          uprn: 1234,
+        },
+      }),
+    });
   });
 
   it('allows manual entry of an address', () => {
@@ -129,5 +268,109 @@ describe('CustomAddressEditor', () => {
         },
       }),
     });
+  });
+
+  it('shows all addresses after a search even if the results are paginated', async () => {
+    const mocked_results_page_1 = {
+      ...addressesAPIWrapperFactory.build(),
+      page_count: 2,
+    };
+    const mocked_results_page_2 = addressesAPI3and4WrapperFactory.build();
+    mockedAxios.get
+      .mockResolvedValueOnce({ data: mocked_results_page_1 })
+      .mockResolvedValue({ data: mocked_results_page_2 });
+
+    render(
+      <CustomAddressEditor
+        name="address"
+        label="Address"
+        onClose={mockClose}
+        resident={{
+          ...mockedResident,
+          address: undefined,
+        }}
+      />
+    );
+
+    userEvent.type(screen.getByLabelText('Building number or name'), '1');
+    userEvent.type(screen.getByLabelText('Postcode'), 'Town St');
+    await waitFor(() => fireEvent.click(screen.getByText('Find address')));
+
+    expect(mockedAxios.get).toBeCalledWith(
+      '/api/postcode/Town St?page=1&buildingNumber=1'
+    );
+
+    expect(mockedAxios.get).toBeCalledWith(
+      '/api/postcode/Town St?page=2&buildingNumber=1'
+    );
+
+    expect(mockedAxios.get).not.toBeCalledWith(
+      '/api/postcode/Town St?page=3&buildingNumber=1'
+    );
+
+    const addressDropDown = screen.getByRole('combobox');
+    expect(addressDropDown).not.toBeNull();
+
+    await act(async () => {
+      fireEvent.click(addressDropDown);
+    });
+
+    expect(addressDropDown.childElementCount).toBe(4);
+  });
+
+  it('shows an error if no addresses are found from the postcode search', async () => {
+    const mock_no_results = {
+      address: [],
+      page_count: 1,
+    };
+    mockedAxios.get.mockResolvedValue({ data: mock_no_results });
+
+    render(
+      <CustomAddressEditor
+        name="address"
+        label="Address"
+        onClose={mockClose}
+        resident={{
+          ...mockedResident,
+          address: undefined,
+        }}
+      />
+    );
+
+    userEvent.type(screen.getByLabelText('Building number or name'), '1');
+    userEvent.type(screen.getByLabelText('Postcode'), 'Town St');
+    await waitFor(() => fireEvent.click(screen.getByText('Find address')));
+
+    const expectedAddress = screen.getByText(
+      'No matching addresses were found.'
+    );
+    expect(expectedAddress).not.toBeNull();
+    expect(expectedAddress).toBeInTheDocument();
+  });
+
+  it('checks postcode api errors are handled', async () => {
+    mockedAxios.get.mockRejectedValueOnce(new Error());
+
+    render(
+      <CustomAddressEditor
+        name="address"
+        label="Address"
+        onClose={mockClose}
+        resident={{
+          ...mockedResident,
+          address: undefined,
+        }}
+      />
+    );
+
+    userEvent.type(screen.getByLabelText('Building number or name'), '1');
+    userEvent.type(screen.getByLabelText('Postcode'), 'Town St');
+    await waitFor(() => fireEvent.click(screen.getByText('Find address')));
+
+    const expectedAddress = screen.getByText(
+      'There was a problem retrieving addresses, please try again.'
+    );
+    expect(expectedAddress).not.toBeNull();
+    expect(expectedAddress).toBeInTheDocument();
   });
 });
