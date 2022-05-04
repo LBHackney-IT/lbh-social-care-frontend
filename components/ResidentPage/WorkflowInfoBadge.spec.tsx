6a68e19ddc7e6783ec7e4e03b41de7482a917bb9
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-05-04 16:38:49 +0100

    Display review or reassessment when workflow these are the workflow types

--- components/ResidentPage/WorkflowInfoBadge.spec.tsx
+++ components/ResidentPage/WorkflowInfoBadge.spec.tsx
@@ -1,19 +1,47 @@
 import { render, screen } from '@testing-library/react';
 import { mockWorkflow } from 'fixtures/workflows';
 import WorkflowInfoBadge from './WorkflowInfoBadge';
+import useWorkflowIds from '../../hooks/useWorkflowIds';
+
+jest.mock('../../hooks/useWorkflowIds');
 
 describe('WorkflowInfoBadge', () => {
   it('displays an unknown text if there is no workflow information', () => {
+    (useWorkflowIds as jest.Mock).mockReturnValue({ error: new Error() });
+
     render(<WorkflowInfoBadge {...mockWorkflow.id} />);
+
     const unknownLabel = screen.getByText('Unknown');
     expect(unknownLabel).toBeVisible();
-    expect(
-      unknownLabel.className.includes('govuk-tag lbh-tag lbh-tag--grey')
-    ).toBe(true);
+    expect(unknownLabel.className).toBe('govuk-tag lbh-tag lbh-tag--grey');
   });
+
   it('does not display a label if the workflow type is assessment', () => {
+    (useWorkflowIds as jest.Mock).mockReturnValue({
+      data: { workflows: [mockWorkflow] },
+    });
     render(<WorkflowInfoBadge {...mockWorkflow.id} />);
-    const workflowLabel = screen.getByTestId('workflow-info');
+    const workflowLabel = screen.queryByTestId('workflow-info');
     expect(workflowLabel).toBeNull();
+  });
+
+  it('displays a review label if the workflow type is review', () => {
+    (useWorkflowIds as jest.Mock).mockReturnValue({
+      data: { workflows: [{ ...mockWorkflow, type: 'Review' }] },
+    });
+    render(<WorkflowInfoBadge {...mockWorkflow.id} />);
+    const reviewLabel = screen.getByText('Review');
+    expect(reviewLabel).toBeVisible();
+    expect(reviewLabel.className).toBe('govuk-tag lbh-tag');
+  });
+
+  it('displays a reassessment label if the workflow type is reassessment', () => {
+    (useWorkflowIds as jest.Mock).mockReturnValue({
+      data: { workflows: [{ ...mockWorkflow, type: 'Reassessment' }] },
+    });
+    render(<WorkflowInfoBadge {...mockWorkflow.id} />);
+    const reviewLabel = screen.getByText('Reassessment');
+    expect(reviewLabel).toBeVisible();
+    expect(reviewLabel.className).toBe('govuk-tag lbh-tag');
   });
 });
--- components/ResidentPage/WorkflowInfoBadge.tsx
+++ components/ResidentPage/WorkflowInfoBadge.tsx
@@ -1,18 +1,27 @@
 import useWorkflowIds from 'hooks/useWorkflowIds';
+import { WorkflowType } from './types';
 import s from './WorkflowInfoBadge.module.scss';
 
-export const WorkflowInfoBadge = (workflowId?: string): React.ReactElement => {
-  //Get workflow assessment type
-  const { data, error } = useWorkflowIds(workflowId, 1000);
+export const WorkflowInfoBadge = (workflowId: string): React.ReactElement => {
+  const { data, error } = useWorkflowIds(workflowId, 1);
 
   return (
     <div className={s.badge}>
-      <span
-        className="govuk-tag lbh-tag lbh-tag--grey"
-        data-testid="workflow-info"
-      >
-        Unknown
-      </span>
+      {(data?.workflows[0].type === WorkflowType.Review ||
+        data?.workflows[0].type === WorkflowType.Reassessment) && (
+        <span className="govuk-tag lbh-tag" data-testid="workflow-info">
+          {data?.workflows[0].type}
+        </span>
+      )}
+
+      {error && (
+        <span
+          className="govuk-tag lbh-tag lbh-tag--grey"
+          data-testid="workflow-info"
+        >
+          Unknown
+        </span>
+      )}
     </div>
   );
 };
