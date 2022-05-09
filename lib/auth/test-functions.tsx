8af1fb067600d190e3776c4980fdc793757e30d2
mapleleafjack <jack.musajo@madetech.com>
2022-05-09 15:57:16 +0100

    Remove unused functions

--- lib/auth/test-functions.tsx
+++ lib/auth/test-functions.tsx
@@ -1,32 +1,5 @@
-import { sign } from 'jsonwebtoken';
 import { NextApiRequest } from 'next';
 import { ParsedUrlQuery } from 'querystring';
-import { ServerResponse } from 'http';
-
-export const dateToUnix = (date: Date): number =>
-  Math.floor(date.getTime() / 1000);
-
-interface MakeTokenInput {
-  sub?: string;
-  email?: string;
-  iss?: string;
-  name?: string;
-  groups?: Array<string>;
-  iat?: Date;
-}
-
-export const makeToken = ({
-  sub = '49516349857314',
-  email = 'test@example.com',
-  iss = 'Hackney',
-  name = 'example user',
-  groups = ['test-group'],
-  iat = new Date(),
-}: MakeTokenInput): string =>
-  sign(
-    { sub, email, iss, name, groups, iat: dateToUnix(iat) },
-    process.env.HACKNEY_AUTH_TOKEN_SECRET as string
-  );
 
 export interface MakeNextApiRequestInput {
   method?:
@@ -70,23 +43,3 @@ export const makeNextApiRequest = ({
 
   return request;
 };
-
-export interface MakeGetServerSidePropsContextInput {
-  resolvedUrl?: string;
-  method?: string;
-  token?: string;
-  query?: ParsedUrlQuery;
-  referer?: string;
-  res?: ServerResponse;
-}
-
-export type HttpMethod =
-  | 'GET'
-  | 'HEAD'
-  | 'POST'
-  | 'PUT'
-  | 'DELETE'
-  | 'CONNECT'
-  | 'OPTIONS'
-  | 'TRACE'
-  | 'PATCH';
